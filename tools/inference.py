811c0706d83b59a587dcccd9190b6556de33a988
yl-1993 <yangleidev@gmail.com>
2020-08-02 23:47:47 +0800

    add inference tool and demo

--- tools/inference.py
+++ tools/inference.py
@@ -1,22 +1,102 @@
-from argparse import ArgumentParser
+import argparse
+import os
 
-from mmcls.apis import inference_model, init_model
+import mmcv
+import numpy as np
+import torch
+from mmcv.parallel import MMDataParallel, MMDistributedDataParallel
+from mmcv.runner import get_dist_info, init_dist, load_checkpoint
+
+from mmcls.apis import multi_gpu_inference, single_gpu_inference
+from mmcls.core import wrap_fp16_model
+from mmcls.datasets import build_dataloader, build_dataset
+from mmcls.models import build_classifier
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='mmcls inference model')
+    parser.add_argument('config', help='test config file path')
+    parser.add_argument('checkpoint', help='checkpoint file')
+    parser.add_argument('--out', help='output result file')
+    parser.add_argument(
+        '--gpu_collect',
+        action='store_true',
+        help='whether to use gpu to collect results')
+    parser.add_argument('--tmpdir', help='tmp dir for writing some results')
+    parser.add_argument(
+        '--launcher',
+        choices=['none', 'pytorch', 'slurm', 'mpi'],
+        default='none',
+        help='job launcher')
+    parser.add_argument('--local_rank', type=int, default=0)
+    args = parser.parse_args()
+    if 'LOCAL_RANK' not in os.environ:
+        os.environ['LOCAL_RANK'] = str(args.local_rank)
+    return args
 
 
 def main():
-    parser = ArgumentParser()
-    parser.add_argument('img', help='Image file')
-    parser.add_argument('config', help='Config file')
-    parser.add_argument('checkpoint', help='Checkpoint file')
-    parser.add_argument(
-        '--device', default='cuda:0', help='Device used for inference')
-    args = parser.parse_args()
+    args = parse_args()
 
-    # build the model from a config file and a checkpoint file
-    model = init_model(args.config, args.checkpoint, device=args.device)
-    # test a single image
-    result = inference_model(model, args.img)
-    print(result)
+    cfg = mmcv.Config.fromfile(args.config)
+    # set cudnn_benchmark
+    if cfg.get('cudnn_benchmark', False):
+        torch.backends.cudnn.benchmark = True
+    cfg.model.pretrained = None
+    cfg.data.test.test_mode = True
+
+    # init distributed env first, since logger depends on the dist info.
+    if args.launcher == 'none':
+        distributed = False
+    else:
+        distributed = True
+        init_dist(args.launcher, **cfg.dist_params)
+
+    # build the dataloader
+    dataset = build_dataset(cfg.data.test)
+    data_loader = build_dataloader(
+        dataset,
+        samples_per_gpu=cfg.data.samples_per_gpu,
+        workers_per_gpu=cfg.data.workers_per_gpu,
+        dist=distributed,
+        shuffle=False,
+        round_up=False)
+
+    # build the model and load checkpoint
+    model = build_classifier(cfg.model)
+    fp16_cfg = cfg.get('fp16', None)
+    if fp16_cfg is not None:
+        wrap_fp16_model(model)
+    _ = load_checkpoint(model, args.checkpoint, map_location='cpu')
+
+    if not distributed:
+        model = MMDataParallel(model, device_ids=[0])
+        outputs = single_gpu_inference(model, data_loader)
+    else:
+        model = MMDistributedDataParallel(
+            model.cuda(),
+            device_ids=[torch.cuda.current_device()],
+            broadcast_buffers=False)
+        outputs = multi_gpu_inference(model, data_loader, args.tmpdir,
+                                 args.gpu_collect)
+
+    rank, _ = get_dist_info()
+    if rank == 0:
+        nums = []
+        results = {}
+        for output in outputs:
+            nums.append(output['num_samples'].item())
+            for topk, v in output['accuracy'].items():
+                if topk not in results:
+                    results[topk] = []
+                results[topk].append(v.item())
+        assert sum(nums) == len(dataset)
+        for topk, accs in results.items():
+            avg_acc = np.average(accs, weights=nums)
+            print(f'\n{topk} accuracy: {avg_acc:.2f}')
+    if args.out and rank == 0:
+        print(f'\nwriting results to {args.out}')
+        mmcv.dump(outputs, args.out)
 
 
 if __name__ == '__main__':
