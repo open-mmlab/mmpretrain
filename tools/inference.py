7efde02e99d7a470cddc79f73530148bd83ad75e
yl-1993 <yangleidev@gmail.com>
2020-08-03 12:06:52 +0800

    fix linting

--- tools/inference.py
+++ tools/inference.py
@@ -78,7 +78,7 @@ def main():
             device_ids=[torch.cuda.current_device()],
             broadcast_buffers=False)
         outputs = multi_gpu_inference(model, data_loader, args.tmpdir,
-                                 args.gpu_collect)
+                                      args.gpu_collect)
 
     rank, _ = get_dist_info()
     if rank == 0:
