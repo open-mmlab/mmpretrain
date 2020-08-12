24a075659a6aa0d8e31a019c4d8a01002dca37e5
lixiaojie <lixiaojie@sensetime.com>
2020-08-13 01:12:51 +0800

    update shufflenet config

---
+++ configs/_base_/schedules/imagenet_bs1024_linearlr_bn_nowd.py
@@ -0,0 +1,17 @@
+# optimizer
+optimizer = dict(
+    type='SGD',
+    lr=0.5,
+    momentum=0.9,
+    weight_decay=0.00004,
+    paramwise_cfg=dict(norm_decay_mult=0))
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(
+    policy='poly',
+    min_lr=0,
+    by_epoch=False,
+    warmup='constant',
+    warmup_iters=5000,
+)
+total_epochs = 300
