18ac0f0fa37fc702acf26d9bf67494d80f392b19
Xiaojie Li <xiaojieli@buaa.edu.cn>
2020-08-13 10:46:39 +0800

    Update shufflenet config (#31)
    * update shufflenet config
    
    * remove old config
    
    Co-authored-by: lixiaojie <lixiaojie@sensetime.com>

--- configs/_base_/models/shufflenet_v2_1x.py
+++ configs/_base_/models/shufflenet_v2_1x.py
@@ -7,4 +7,6 @@ model = dict(
         type='LinearClsHead',
         num_classes=1000,
         in_channels=1024,
-        loss=dict(type='CrossEntropyLoss', loss_weight=1.0)))
+        loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+        topk=(1, 5),
+    ))
--- configs/_base_/schedules/imagenet_bs1024_linearlr_bn_nowd.py
+++ configs/_base_/schedules/imagenet_bs1024_linearlr_bn_nowd.py
@@ -1,5 +1,10 @@
 # optimizer
-optimizer = dict(type='SGD', lr=0.5, momentum=0.9, weight_decay=0.00004)
+optimizer = dict(
+    type='SGD',
+    lr=0.5,
+    momentum=0.9,
+    weight_decay=0.00004,
+    paramwise_cfg=dict(norm_decay_mult=0))
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(
