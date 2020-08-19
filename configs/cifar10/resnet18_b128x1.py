3af9c91394dc898699ed543e3dc6eb6cb55e7e64
yl-1993 <yangleidev@gmail.com>
2020-08-19 15:46:41 +0800

    fix linting

--- configs/cifar10/resnet18_b128x1.py
+++ configs/cifar10/resnet18_b128x1.py
@@ -1,4 +1,5 @@
 _base_ = [
-    '../_base_/models/resnet18_cifar.py', '../_base_/datasets/cifar10_bs128.py',
+    '../_base_/models/resnet18_cifar.py',
+    '../_base_/datasets/cifar10_bs128.py',
     '../_base_/schedules/cifar10_bs128.py', '../_base_/default_runtime.py'
 ]
