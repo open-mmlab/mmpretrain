03bc4909bbd6c854b172e3e86fb8d5421e0f73bf
Jiarui XU <xvjiarui0826@gmail.com>
2020-09-21 22:23:29 +0800

    Add S-200,S269 _base_

---
+++ configs/_base_/models/resnest200.py
@@ -0,0 +1,18 @@
+# model settings
+model = dict(
+    type='ImageClassifier',
+    backbone=dict(
+        type='ResNeSt',
+        depth=200,
+        num_stages=4,
+        stem_channels=128,
+        out_indices=(3, ),
+        style='pytorch'),
+    neck=dict(type='GlobalAveragePooling'),
+    head=dict(
+        type='LinearClsHead',
+        num_classes=1000,
+        in_channels=2048,
+        loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+        topk=(1, 5),
+    ))
