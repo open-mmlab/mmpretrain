854af1b2fccc71a1115e4cbd7c8bf581182d45b5
yl-1993 <yangleidev@gmail.com>
2020-09-28 20:36:34 +0800

    construct data dict to replace LoadImage

--- mmcls/apis/inference.py
+++ mmcls/apis/inference.py
@@ -46,31 +46,6 @@ def init_model(config, checkpoint=None, device='cuda:0'):
     return model
 
 
-class LoadImage(object):
-    """A simple pipeline to load image."""
-
-    def __call__(self, results):
-        """Call function to load images into results.
-
-        Args:
-            results (dict): A result dict contains the file name
-                of the image to be read.
-
-        Returns:
-            dict: ``results`` will be returned containing loaded image.
-        """
-        if isinstance(results['img'], str):
-            results['filename'] = results['img']
-        else:
-            results['filename'] = None
-        img = mmcv.imread(results['img'])
-        results['img'] = img
-        results['img_fields'] = ['img']
-        results['img_shape'] = img.shape
-        results['ori_shape'] = img.shape
-        return results
-
-
 def inference_model(model, img):
     """Inference image(s) with the classifier.
 
@@ -85,10 +60,9 @@ def inference_model(model, img):
     cfg = model.cfg
     device = next(model.parameters()).device  # model device
     # build the data pipeline
-    test_pipeline = [LoadImage()] + cfg.data.test.pipeline[1:]
-    test_pipeline = Compose(test_pipeline)
+    test_pipeline = Compose(cfg.data.test.pipeline[1:])
     # prepare data
-    data = dict(img=img)
+    data = dict(img_info=dict(filename=img))
     data = test_pipeline(data)
     data = collate([data], samples_per_gpu=1)
     if next(model.parameters()).is_cuda:
