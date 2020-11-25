9e5670ff452d97d245284be993c4b7ff85ac9a2f
lixinran <lixr423@outlook.com>
2020-11-25 11:23:20 +0800

    Revise according to comments

--- mmcls/models/losses/eval_metrics.py
+++ mmcls/models/losses/eval_metrics.py
@@ -2,7 +2,14 @@ import numpy as np
 import torch
 
 
-def calculate_confusion_matrix_torch(pred, target):
+def calculate_confusion_matrix(pred, target):
+    if isinstance(pred, np.ndarray) and isinstance(target, np.ndarray):
+        pred = torch.from_numpy(pred)
+        target = torch.from_numpy(target)
+    elif not (isinstance(pred, torch.Tensor)
+              and isinstance(target, torch.Tensor)):
+        raise TypeError('pred and target should both be'
+                        'torch.Tensor or np.ndarray')
     _, pred_label = pred.topk(1, dim=1)
     num_classes = pred.size(1)
     pred_label = pred_label.view(-1)
@@ -11,18 +18,6 @@ def calculate_confusion_matrix_torch(pred, target):
     confusion_matrix = torch.zeros(num_classes, num_classes)
     with torch.no_grad():
         confusion_matrix[target_label.long(), pred_label.long()] += 1
-    return confusion_matrix
-
-
-def calculate_confusion_matrix_numpy(pred, target):
-    pred_label = pred.argsort(axis=1)[:, -1]
-    num_classes = pred.shape[1]
-    pred_label = pred_label.reshape(-1)
-    target_label = target.reshape(-1)
-    assert len(pred_label) == len(target_label)
-    confusion_matrix = torch.zeros(num_classes, num_classes)
-    with torch.no_grad():
-        confusion_matrix[target_label, pred_label] += 1
     return confusion_matrix
 
 
@@ -36,13 +31,7 @@ def precision(pred, target):
     Returns:
         float: The function will return a single float as precision.
     """
-    if isinstance(pred, torch.Tensor) and isinstance(target, torch.Tensor):
-        confusion_matrix = calculate_confusion_matrix_torch(pred, target)
-    elif isinstance(pred, np.ndarray) and isinstance(target, np.ndarray):
-        confusion_matrix = calculate_confusion_matrix_numpy(pred, target)
-    else:
-        raise TypeError('pred and target should both be'
-                        'torch.Tensor or np.ndarray')
+    confusion_matrix = calculate_confusion_matrix(pred, target)
     with torch.no_grad():
         res = confusion_matrix.diag() / torch.clamp(
             confusion_matrix.sum(1), min=1)
@@ -60,14 +49,7 @@ def recall(pred, target):
     Returns:
         float: The function will return a single float as recall.
     """
-    if isinstance(pred, torch.Tensor) and isinstance(target, torch.Tensor):
-        confusion_matrix = calculate_confusion_matrix_torch(pred, target)
-    elif isinstance(pred, np.ndarray) and isinstance(target, np.ndarray):
-        confusion_matrix = calculate_confusion_matrix_numpy(pred, target)
-    else:
-        raise TypeError('pred and target should both be'
-                        'torch.Tensor or np.ndarray')
-
+    confusion_matrix = calculate_confusion_matrix(pred, target)
     with torch.no_grad():
         res = confusion_matrix.diag() / torch.clamp(
             confusion_matrix.sum(0), min=1)
@@ -85,14 +67,7 @@ def f1_score(pred, target):
     Returns:
         float: The function will return a single float as F1 score.
     """
-    if isinstance(pred, torch.Tensor) and isinstance(target, torch.Tensor):
-        confusion_matrix = calculate_confusion_matrix_torch(pred, target)
-    elif isinstance(pred, np.ndarray) and isinstance(target, np.ndarray):
-        confusion_matrix = calculate_confusion_matrix_numpy(pred, target)
-    else:
-        raise TypeError('pred and target should both be'
-                        'torch.Tensor or np.ndarray')
-
+    confusion_matrix = calculate_confusion_matrix(pred, target)
     with torch.no_grad():
         precision = confusion_matrix.diag() / torch.clamp(
             confusion_matrix.sum(1), min=1)
