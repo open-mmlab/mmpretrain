6417a8192f93ea91b7eb234df9ef7e8a6a89eb26
lixinran <lixr423@outlook.com>
2020-12-24 14:11:43 +0800

    add mean_ap

--- mmcls/core/evaluation/__init__.py
+++ mmcls/core/evaluation/__init__.py
@@ -1,3 +1,4 @@
 from .eval_hooks import DistEvalHook, EvalHook
+from .mean_ap import average_precision, mAP
 
-__all__ = ['DistEvalHook', 'EvalHook']
+__all__ = ['DistEvalHook', 'EvalHook', 'average_precision', 'mAP']
---
+++ mmcls/core/evaluation/mean_ap.py
@@ -0,0 +1,61 @@
+import numpy as np
+import torch
+
+
+def average_precision(pred, target):
+    """ Calculate the average precision for a single class
+
+    Args:
+        pred (np.ndarray): The model prediction.
+        target (np.ndarray): The target of each prediction.
+
+    Returns:
+        float: a single float as average precision value.
+    """
+    eps = np.finfo(np.float32).eps
+
+    # sort examples
+    sort_inds = np.argsort(-pred)
+    sort_target = target[sort_inds]
+
+    # count true positive examples
+    p_inds = sort_target == 1
+    tp = np.cumsum(p_inds)
+    total_p = tp[-1]
+
+    # count not difficult examples
+    pn_inds = sort_target != 0
+    pn = np.cumsum(pn_inds)
+
+    tp[np.logical_not(p_inds)] = 0
+    precision = tp / (pn + eps)
+    ap = np.sum(precision) / (total_p + eps)
+    return ap
+
+
+def mAP(pred, target):
+    """ Calculate the mean average precision with respect of classes
+
+    Args:
+        pred (torch.Tensor | np.ndarray): The model prediction.
+        target (torch.Tensor | np.ndarray): The target of each prediction, in
+            which -1 stands for negative examples, 0 stands for difficult
+            examples and 1 stand for positive examples.
+
+    Returns:
+        float: A single float as mAP value.
+    """
+    if isinstance(pred, torch.Tensor) and isinstance(target, torch.Tensor):
+        pred = pred.numpy()
+        target = target.numpy()
+    elif not (isinstance(pred, np.ndarray) and isinstance(target, np.ndarray)):
+        raise TypeError('pred and target should both be torch.Tensor or'
+                        'np.ndarray')
+
+    assert pred.shape == target.shape
+    num_labels = pred.shape[1]
+    ap = np.zeros(num_labels)
+    for k in range(num_labels):
+        ap[k] = average_precision(pred[:, k], target[:, k])
+    mean_ap = ap.mean() * 100.0
+    return mean_ap
