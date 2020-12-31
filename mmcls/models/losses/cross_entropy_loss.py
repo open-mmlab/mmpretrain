b0c188d125ee54ec8b927585bb18dd23ff207fb4
lixinran <lixr423@outlook.com>
2020-12-31 10:31:41 +0800

    add bce loss for multilabel task

--- mmcls/models/losses/cross_entropy_loss.py
+++ mmcls/models/losses/cross_entropy_loss.py
@@ -18,15 +18,63 @@ def cross_entropy(pred, label, weight=None, reduction='mean', avg_factor=None):
     return loss
 
 
+def binary_cross_entropy(pred,
+                         label,
+                         weight=None,
+                         reduction='mean',
+                         avg_factor=None):
+    """Calculate the binary CrossEntropy loss with logits.
+
+    Args:
+        pred (torch.Tensor): The prediction with shape (N, C), where C is the
+            number of classes.
+        label (torch.Tensor): The learning label with shape (N, C), where C is
+            the number of classes.
+        weight (torch.Tensor, optional): Weight of loss. Defaults to None.
+        reduction (str, optional): The method used to reduce the loss.
+            Options are "none", "mean" and "sum". Defaults to 'mean'.
+        avg_factor (int, optional): Average factor that is used to average
+            the loss. Defaults to None.
+
+    Returns:
+        torch.Tensor: The calculated loss
+    """
+    # would they be any different?
+    assert pred.dim() == label.dim()
+
+    loss = F.binary_cross_entropy_with_logits(pred, label, reduction='none')
+
+    # apply weights and do the reduction
+    if weight is not None:
+        weight = weight.float()
+    loss = weight_reduce_loss(
+        loss, weight=weight, reduction=reduction, avg_factor=avg_factor)
+    return loss
+
+
 @LOSSES.register_module()
 class CrossEntropyLoss(nn.Module):
 
-    def __init__(self, reduction='mean', loss_weight=1.0):
+    def __init__(self, use_sigmoid=False, reduction='mean', loss_weight=1.0):
+        """Cross entropy loss
+
+        Args:
+            use_sigmoid (bool, optional): Whether the prediction uses sigmoid
+                of softmax. Defaults to False.
+            reduction (str, optional): The method used to reduce the loss.
+            Options are "none", "mean" and "sum". Defaults to 'mean'.
+            loss_weight (float, optional):  Weight of the loss.
+                Defaults to 1.0.
+        """
         super(CrossEntropyLoss, self).__init__()
+        self.use_sigmoid = use_sigmoid
         self.reduction = reduction
         self.loss_weight = loss_weight
 
-        self.cls_criterion = cross_entropy
+        if self.use_sigmoid:
+            self.cls_criterion = binary_cross_entropy
+        else:
+            self.cls_criterion = cross_entropy
 
     def forward(self,
                 cls_score,
---
+++ tests/test_losses.py
@@ -0,0 +1,34 @@
+import torch
+
+from mmcls.models import build_loss
+
+
+def test_cross_entropy_loss():
+
+    # test ce_loss
+    cls_score = torch.Tensor([[100, -100]])
+    label = torch.Tensor([1]).long()
+    weight = torch.tensor(0.5)
+
+    loss_cfg = dict(type='CrossEntropyLoss', reduction='mean', loss_weight=1.0)
+    loss = build_loss(loss_cfg)
+    assert torch.allclose(loss(cls_score, label), torch.tensor(200.))
+    # test ce_loss with weight
+    assert torch.allclose(
+        loss(cls_score, label, weight=weight), torch.tensor(100.))
+
+    # test bce_loss
+    cls_score = torch.Tensor([[100, -100], [100, -100]])
+    label = torch.Tensor([[1, 0], [0, 1]])
+    weight = torch.Tensor([0.5, 0.5])
+
+    loss_cfg = dict(
+        type='CrossEntropyLoss',
+        use_sigmoid=True,
+        reduction='mean',
+        loss_weight=1.0)
+    loss = build_loss(loss_cfg)
+    assert torch.allclose(loss(cls_score, label), torch.tensor(50.))
+    # test ce_loss with weight
+    assert torch.allclose(
+        loss(cls_score, label, weight=weight), torch.tensor(25.))
