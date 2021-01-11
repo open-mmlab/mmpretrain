194ab7efda231e069c1ba5d2af16dcf22a0569f1
LXXXXR <73265258+LXXXXR@users.noreply.github.com>
2021-01-11 11:05:24 +0800

    [Feature] Add bce loss for multilabel task (#130)
    * add bce loss for multilabel task
    
    * minor change
    
    * apply class wise sum
    
    * fix docstring
    
    * do not apply sum over classes and fix docstring
    
    * fix docstring
    
    * fix weight shape
    
    * fix weight shape
    
    * fix docstring
    
    * fix linting issue
    
    Co-authored-by: Y. Xiong <xiongyuxy@gmail.com>

--- mmcls/models/losses/__init__.py
+++ mmcls/models/losses/__init__.py
@@ -1,12 +1,14 @@
 from .accuracy import Accuracy, accuracy
-from .cross_entropy_loss import CrossEntropyLoss, cross_entropy
+from .cross_entropy_loss import (CrossEntropyLoss, binary_cross_entropy,
+                                 cross_entropy)
 from .eval_metrics import f1_score, precision, recall
 from .focal_loss import FocalLoss, sigmoid_focal_loss
 from .label_smooth_loss import LabelSmoothLoss, label_smooth
 from .utils import reduce_loss, weight_reduce_loss, weighted_loss
 
 __all__ = [
-    'accuracy', 'Accuracy', 'cross_entropy', 'CrossEntropyLoss', 'reduce_loss',
-    'weight_reduce_loss', 'label_smooth', 'LabelSmoothLoss', 'weighted_loss',
-    'precision', 'recall', 'f1_score', 'FocalLoss', 'sigmoid_focal_loss'
+    'accuracy', 'Accuracy', 'cross_entropy', 'binary_cross_entropy',
+    'CrossEntropyLoss', 'reduce_loss', 'weight_reduce_loss', 'label_smooth',
+    'LabelSmoothLoss', 'weighted_loss', 'precision', 'recall', 'f1_score',
+    'FocalLoss', 'sigmoid_focal_loss'
 ]
--- tests/test_losses.py
+++ tests/test_losses.py
@@ -3,6 +3,37 @@ import torch
 from mmcls.models import build_loss
 
 
+def test_cross_entropy_loss():
+
+    # test ce_loss
+    cls_score = torch.Tensor([[100, -100]])
+    label = torch.Tensor([1]).long()
+    weight = torch.tensor(0.5)
+
+    loss_cfg = dict(type='CrossEntropyLoss', reduction='mean', loss_weight=1.0)
+    loss = build_loss(loss_cfg)
+    assert torch.allclose(loss(cls_score, label), torch.tensor(200.))
+    # test ce_loss with weight
+    assert torch.allclose(
+        loss(cls_score, label, weight=weight), torch.tensor(100.))
+
+    # test bce_loss
+    cls_score = torch.Tensor([[100, -100], [100, -100]])
+    label = torch.Tensor([[1, 0], [0, 1]])
+    weight = torch.Tensor([0.5, 0.5])
+
+    loss_cfg = dict(
+        type='CrossEntropyLoss',
+        use_sigmoid=True,
+        reduction='mean',
+        loss_weight=1.0)
+    loss = build_loss(loss_cfg)
+    assert torch.allclose(loss(cls_score, label), torch.tensor(50.))
+    # test ce_loss with weight
+    assert torch.allclose(
+        loss(cls_score, label, weight=weight), torch.tensor(25.))
+
+
 def test_focal_loss():
     # test focal_loss
     cls_score = torch.Tensor([[5, -5, 0], [5, -5, 0]])
