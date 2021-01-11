6916f33d5653ea7a3bd997363528bcf017b24a02
LXXXXR <73265258+LXXXXR@users.noreply.github.com>
2021-01-11 11:22:22 +0800

    [Feature] Add asymmetric loss for multilabel task (#132)
    * add asymmetric loss
    
    * minor change
    
    * fix docstring
    
    * do not apply sum over classes and fix docstring
    
    * fix docstring
    
    * fix weight shape
    
    * fix weight shape
    
    * add reference
    
    * fix linkting issue
    
    Co-authored-by: Y. Xiong <xiongyuxy@gmail.com>

--- mmcls/models/losses/__init__.py
+++ mmcls/models/losses/__init__.py
@@ -1,4 +1,5 @@
 from .accuracy import Accuracy, accuracy
+from .asymmetric_loss import AsymmetricLoss, asymmetric_loss
 from .cross_entropy_loss import (CrossEntropyLoss, binary_cross_entropy,
                                  cross_entropy)
 from .eval_metrics import f1_score, precision, recall
@@ -7,8 +8,8 @@ from .label_smooth_loss import LabelSmoothLoss, label_smooth
 from .utils import reduce_loss, weight_reduce_loss, weighted_loss
 
 __all__ = [
-    'accuracy', 'Accuracy', 'cross_entropy', 'binary_cross_entropy',
-    'CrossEntropyLoss', 'reduce_loss', 'weight_reduce_loss', 'label_smooth',
-    'LabelSmoothLoss', 'weighted_loss', 'precision', 'recall', 'f1_score',
-    'FocalLoss', 'sigmoid_focal_loss'
+    'accuracy', 'Accuracy', 'asymmetric_loss', 'AsymmetricLoss',
+    'cross_entropy', 'binary_cross_entropy', 'CrossEntropyLoss', 'reduce_loss',
+    'weight_reduce_loss', 'label_smooth', 'LabelSmoothLoss', 'weighted_loss',
+    'precision', 'recall', 'f1_score', 'FocalLoss', 'sigmoid_focal_loss'
 ]
--- tests/test_losses.py
+++ tests/test_losses.py
@@ -3,6 +3,38 @@ import torch
 from mmcls.models import build_loss
 
 
+def test_asymmetric_loss():
+    # test asymmetric_loss
+    cls_score = torch.Tensor([[5, -5, 0], [5, -5, 0]])
+    label = torch.Tensor([[1, 0, 1], [0, 1, 0]])
+    weight = torch.tensor([0.5, 0.5])
+
+    loss_cfg = dict(
+        type='AsymmetricLoss',
+        gamma_pos=1.0,
+        gamma_neg=4.0,
+        clip=0.05,
+        reduction='mean',
+        loss_weight=1.0)
+    loss = build_loss(loss_cfg)
+    assert torch.allclose(loss(cls_score, label), torch.tensor(3.80845 / 3))
+
+    # test asymmetric_loss with weight
+    assert torch.allclose(
+        loss(cls_score, label, weight=weight), torch.tensor(3.80845 / 6))
+
+    # test asymmetric_loss without clip
+    loss_cfg = dict(
+        type='AsymmetricLoss',
+        gamma_pos=1.0,
+        gamma_neg=4.0,
+        clip=None,
+        reduction='mean',
+        loss_weight=1.0)
+    loss = build_loss(loss_cfg)
+    assert torch.allclose(loss(cls_score, label), torch.tensor(5.1186 / 3))
+
+
 def test_cross_entropy_loss():
 
     # test ce_loss
