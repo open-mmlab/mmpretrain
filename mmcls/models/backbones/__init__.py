6d3c68b41cf7b476fcc15289f5443413bc8d021e
Yidi Shao <ydshao@smail.nju.edu.cn>
2021-02-04 10:21:55 +0800

    Change model's name from 'MobileNetv3' to 'MobileNetV3'

--- mmcls/models/backbones/__init__.py
+++ mmcls/models/backbones/__init__.py
@@ -1,7 +1,7 @@
 from .alexnet import AlexNet
 from .lenet import LeNet5
 from .mobilenet_v2 import MobileNetV2
-from .mobilenet_v3 import MobileNetv3
+from .mobilenet_v3 import MobileNetV3
 from .regnet import RegNet
 from .resnest import ResNeSt
 from .resnet import ResNet, ResNetV1d
@@ -16,5 +16,5 @@ from .vgg import VGG
 __all__ = [
     'LeNet5', 'AlexNet', 'VGG', 'RegNet', 'ResNet', 'ResNeXt', 'ResNetV1d',
     'ResNeSt', 'ResNet_CIFAR', 'SEResNet', 'SEResNeXt', 'ShuffleNetV1',
-    'ShuffleNetV2', 'MobileNetV2', 'MobileNetv3'
+    'ShuffleNetV2', 'MobileNetV2', 'MobileNetV3'
 ]
