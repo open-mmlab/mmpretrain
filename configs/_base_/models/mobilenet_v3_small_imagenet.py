21880c12433b56f073d0cadc2c3293075cee14ad
Yidi Shao <ydshao@smail.nju.edu.cn>
2021-02-04 19:32:52 +0800

    Modify configs for MobileNetV3 on CIFAR10.
    And add MobileNetV3 configs for imagenet

---
+++ configs/_base_/models/mobilenet_v3_small_imagenet.py
@@ -0,0 +1,12 @@
+# model settings
+model = dict(
+    type='ImageClassifier',
+    backbone=dict(type='MobileNetV3', arch='small'),
+    neck=dict(type='GlobalAveragePooling'),
+    head=dict(
+        type='ConvClsHead',
+        num_classes=1000,
+        in_channels=576,
+        mid_channels=1280,
+        loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+        topk=(1, 5)))
