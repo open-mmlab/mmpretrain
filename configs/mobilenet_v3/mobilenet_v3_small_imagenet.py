de25c4f1b38b7f076e5878feb4c46dfbaf86bfb1
Yidi Shao <ydshao@smail.nju.edu.cn>
2021-02-07 14:19:12 +0800

    Add MobileNetV3 configs for imagenet

---
+++ configs/mobilenet_v3/mobilenet_v3_small_imagenet.py
@@ -0,0 +1,6 @@
+_base_ = [
+    '../_base_/models/mobilenet_v3_small_imagenet.py',
+    '../_base_/datasets/imagenet_bs32_pil_resize.py',
+    '../_base_/schedules/imagenet_bs256_epochstep.py',
+    '../_base_/default_runtime.py'
+]
