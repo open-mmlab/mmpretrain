e64cfa467e810136989296bc27a0f965e4165a68
ftbabi <36023009+ftbabi@users.noreply.github.com>
2021-02-09 10:38:26 +0800

    Clean configs

--- configs/_base_/models/resnet50_cifar_mixup.py
+++ configs/_base_/models/resnet50_cifar_mixup.py
@@ -1,22 +1,3 @@
-# # model settings
-# model = dict(
-#     type='ImageClassifier',
-#     backbone=dict(type='MobileNetV2', widen_factor=1.0),
-#     neck=dict(type='GlobalAveragePooling'),
-#     head=dict(
-#         type='LinearClsHead',
-#         num_classes=10,
-#         in_channels=1280,
-#         loss=dict(type='CrossEntropyLoss', use_soft=True, loss_weight=1.0),
-#         topk=(1, 5),
-#     ),
-#     train_cfg=dict(
-#         mixup=dict(
-#             alpha=0.2,
-#             num_classes=10
-#         ))
-#     )
-
 # model settings
 model = dict(
     type='ImageClassifier',
@@ -33,24 +14,3 @@ model = dict(
         in_channels=2048,
         loss=dict(type='CrossEntropyLoss', loss_weight=1.0, use_soft=True)),
     train_cfg=dict(mixup=dict(alpha=1.0, num_classes=10)))
-
-# # model settings
-# model = dict(
-#     type='ImageClassifier',
-#     backbone=dict(
-#         type='ResNet_CIFAR',
-#         depth=18,
-#         num_stages=4,
-#         out_indices=(3, ),
-#         style='pytorch'),
-#     neck=dict(type='GlobalAveragePooling'),
-#     head=dict(
-#         type='MultiLabelLinearClsHead',
-#         num_classes=10,
-#         in_channels=512,
-#         loss=dict(type='CrossEntropyLoss', loss_weight=1.0, use_soft=True)),
-#     train_cfg=dict(
-#         mixup=dict(
-#             alpha=0.2,
-#             num_classes=10
-#         )))
