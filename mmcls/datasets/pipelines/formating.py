7cf41283a28782f3ba9fca37d35cee9f20d70801
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2021-02-24 12:05:53 +0100

    Add ori_filename meta_key

--- mmcls/datasets/pipelines/formating.py
+++ mmcls/datasets/pipelines/formating.py
@@ -127,8 +127,9 @@ class Collect(object):
 
     def __init__(self,
                  keys,
-                 meta_keys=('filename', 'ori_shape', 'img_shape', 'flip',
-                            'flip_direction', 'img_norm_cfg')):
+                 meta_keys=('filename', 'ori_filename', 'ori_shape',
+                            'img_shape', 'flip', 'flip_direction',
+                            'img_norm_cfg')):
         self.keys = keys
         self.meta_keys = meta_keys
 
