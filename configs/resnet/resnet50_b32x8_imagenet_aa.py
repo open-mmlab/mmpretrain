bee2fb74259fa35efc3ead97fd7f47f2a66ebdcf
lixinran <lixr423@outlook.com>
2021-03-16 10:10:42 +0800

    add configs

---
+++ configs/resnet/resnet50_b32x8_imagenet_aa.py
@@ -0,0 +1,19 @@
+_base_ = [
+    '../_base_/models/resnet50.py', '../_base_/datasets/imagenet_bs32.py',
+    '../_base_/schedules/imagenet_bs256.py', '../_base_/default_runtime.py'
+]
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='RandomResizedCrop', size=224),
+    dict(type='RandomFlip', flip_prob=0.5, direction='horizontal'),
+    dict(type='AutoAugment', policies='ImageNetPolicy'),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='ImageToTensor', keys=['img']),
+    dict(type='ToTensor', keys=['gt_label']),
+    dict(type='Collect', keys=['img', 'gt_label'])
+]
+
+data = dict(train=dict(pipeline=train_pipeline))
