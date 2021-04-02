aeaeac915b5d5118c1894593655c245738d30f10
曹巍瀚 <SENSETIME\caoweihan@cn0214003110l.domain.sensetime.com>
2021-04-02 15:58:29 +0800

    add cutmix

--- mmcls/models/utils/__init__.py
+++ mmcls/models/utils/__init__.py
@@ -2,9 +2,10 @@ from .channel_shuffle import channel_shuffle
 from .inverted_residual import InvertedResidual
 from .make_divisible import make_divisible
 from .mixup import BatchMixupLayer
+from .cutmix import BatchCutMixLayer
 from .se_layer import SELayer
 
 __all__ = [
     'channel_shuffle', 'make_divisible', 'InvertedResidual', 'BatchMixupLayer',
-    'SELayer'
+    'BatchCutMixLayer', 'SELayer'
 ]
