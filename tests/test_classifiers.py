1d9f466bd88239bc7399643f959aa023150c528b
曹巍瀚 <SENSETIME\caoweihan@cn0214003110l.domain.sensetime.com>
2021-04-02 17:40:10 +0800

    Test cutmix in ImageClassifier

--- tests/test_classifiers.py
+++ tests/test_classifiers.py
@@ -20,12 +20,36 @@ def test_image_classifier():
             in_channels=2048,
             loss=dict(type='CrossEntropyLoss', loss_weight=1.0,
                       use_soft=True)),
-        # train_cfg=dict(mixup=dict(alpha=1.0, num_classes=10)))
-        train_cfg=dict(cutmix=dict(alpha=1.0, num_classes=10, cutmix_prob=1.0)))
+        train_cfg=dict(mixup=dict(alpha=1.0, num_classes=10)))
     img_classifier = ImageClassifier(**model_cfg)
     img_classifier.init_weights()
     imgs = torch.randn(16, 3, 32, 32)
     label = torch.randint(0, 10, (16, ))
+
+    losses = img_classifier.forward_train(imgs, label)
+    assert losses['loss'].item() > 0
+
+def test_image_classifier_with_cutmix():
+    # Test cutmix in ImageClassifier
+    model_cfg = dict(
+        backbone=dict(
+            type='ResNet_CIFAR',
+            depth=50,
+            num_stages=4,
+            out_indices=(3,),
+            style='pytorch'),
+        neck=dict(type='GlobalAveragePooling'),
+        head=dict(
+            type='MultiLabelLinearClsHead',
+            num_classes=10,
+            in_channels=2048,
+            loss=dict(type='CrossEntropyLoss', loss_weight=1.0,
+                      use_soft=True)),
+        train_cfg=dict(cutmix=dict(alpha=1.0, num_classes=10, cutmix_prob=1.0)))
+    img_classifier = ImageClassifier(**model_cfg)
+    img_classifier.init_weights()
+    imgs = torch.randn(16, 3, 32, 32)
+    label = torch.randint(0, 10, (16,))
 
     losses = img_classifier.forward_train(imgs, label)
     assert losses['loss'].item() > 0
