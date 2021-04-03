ec8b951884f750e264447865a470222103d5b6ad
HIT-cwh <2892770585@qq.com>
2021-04-03 14:17:43 +0800

    add vit_b_p16_224_finetune_imagenet

---
+++ configs/vision_transformer/vit_base_patch16_224_finetune_imagenet.py
@@ -0,0 +1,10 @@
+# Refer to pytorch-image-models
+_base_ = [
+    '../_base_/models/vit_base_patch16_224_finetune.py',
+    '../_base_/datasets/imagenet_bs32_pil_resize.py',
+    '../_base_/schedules/imagenet_bs256_epochstep.py',
+    '../_base_/default_runtime.py'
+]
+
+img_norm_cfg = dict(
+    mean=[127.5, 127.5, 127.5], std=[127.5, 127.5, 127.5], to_rgb=True)
