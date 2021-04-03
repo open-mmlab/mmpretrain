37673614bf463df2847bb3beb73cbf1083da6058
HIT-cwh <2892770585@qq.com>
2021-04-03 14:18:44 +0800

    add vit_b_p16_224_pretrain_imagenet

---
+++ configs/vision_transformer/vit_base_patch16_224_pretrain_imagenet.py
@@ -0,0 +1,8 @@
+# Refer to ClassyVision. In this implementation, label smoothing is not used.
+_base_ = [
+    '../_base_/models/vit_base_patch16_224_pretrain.py',
+    '../_base_/datasets/imagenet_bs32_pil_resize.py',
+    '../_base_/schedules/imagenet_bs4096.py', '../_base_/default_runtime.py'
+]
+
+# 64*(8*8)=4096
