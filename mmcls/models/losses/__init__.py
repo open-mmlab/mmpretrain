a353ac81bfc1fdd81628841e797b42f81b8e79c0
HIT-cwh <2892770585@qq.com>
2021-04-08 15:48:17 +0800

    remove the old version of LabelSmoothLoss

--- mmcls/models/losses/__init__.py
+++ mmcls/models/losses/__init__.py
@@ -3,7 +3,6 @@ from .asymmetric_loss import AsymmetricLoss, asymmetric_loss
 from .cross_entropy_loss import (CrossEntropyLoss, binary_cross_entropy,
                                  cross_entropy)
 from .focal_loss import FocalLoss, sigmoid_focal_loss
-# from .label_smooth_loss import LabelSmoothLoss, label_smooth
 from .label_smooth_loss import LabelSmoothLoss
 from .utils import (convert_to_one_hot, reduce_loss, weight_reduce_loss,
                     weighted_loss)
