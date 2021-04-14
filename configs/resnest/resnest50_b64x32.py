48a6e4e3afcf2e06f47e130a9460f0b88d2da65c
lixinran <lixr423@outlook.com>
2021-04-14 20:01:33 +0800

    fix config

--- configs/resnest/resnest50_b64x32.py
+++ configs/resnest/resnest50_b64x32.py
@@ -1,13 +1,198 @@
 _base_ = [
-    '../_base_/models/resnest50_train.py',
     '../_base_/datasets/imagenet_bs64_ra.py', '../_base_/default_runtime.py'
 ]
+
+# dataset settings
+dataset_type = 'ImageNet'
+img_lighting_cfg = dict(
+    eigval=[55.4625, 4.7940, 1.1475],
+    eigvec=[[-144.7125, 183.3960, 102.2295], [-148.1040, -1.1475, -207.5700],
+            [-148.8180, -177.1740, 107.1765]],
+    alphastd=0.1,
+    to_rgb=True)
+policies = [
+    dict(type='AutoContrast', prob=0.5),
+    dict(type='Equalize', prob=0.5),
+    dict(type='Invert', prob=0.5),
+    dict(
+        type='Rotate',
+        magnitude_key='angle',
+        magnitude_range=(0, 30),
+        pad_val=0,
+        prob=0.5,
+        random_negative_prob=0.5,
+        interpolation='bicubic'),
+    dict(
+        type='Posterize',
+        magnitude_key='bits',
+        magnitude_range=(0, 4),
+        prob=0.5),
+    dict(
+        type='Solarize',
+        magnitude_key='thr',
+        magnitude_range=(0, 256),
+        prob=0.5),
+    dict(
+        type='SolarizeAdd',
+        magnitude_key='magnitude',
+        magnitude_range=(0, 110),
+        thr=128,
+        prob=0.5),
+    dict(
+        type='ColorTransform',
+        magnitude_key='magnitude',
+        magnitude_range=(-0.9, 0.9),
+        prob=0.5,
+        random_negative_prob=0.),
+    dict(
+        type='Contrast',
+        magnitude_key='magnitude',
+        magnitude_range=(-0.9, 0.9),
+        prob=0.5,
+        random_negative_prob=0.),
+    dict(
+        type='Brightness',
+        magnitude_key='magnitude',
+        magnitude_range=(-0.9, 0.9),
+        prob=0.5,
+        random_negative_prob=0.),
+    dict(
+        type='Sharpness',
+        magnitude_key='magnitude',
+        magnitude_range=(-0.9, 0.9),
+        prob=0.5,
+        random_negative_prob=0.),
+    dict(
+        type='Shear',
+        magnitude_key='magnitude',
+        magnitude_range=(0, 0.3),
+        pad_val=0,
+        prob=0.5,
+        direction='horizontal',
+        random_negative_prob=0.5),
+    dict(
+        type='Shear',
+        magnitude_key='magnitude',
+        magnitude_range=(0, 0.3),
+        pad_val=0,
+        prob=0.5,
+        direction='vertical',
+        random_negative_prob=0.5),
+    dict(
+        type='Cutout',
+        magnitude_key='shape',
+        magnitude_range=(1, 41),
+        pad_val=0,
+        prob=0.5),
+    dict(
+        type='Translate',
+        magnitude_key='magnitude',
+        magnitude_range=(0, 0.3),
+        pad_val=0,
+        prob=0.5,
+        direction='horizontal',
+        random_negative_prob=0.5,
+        interpolation='bicubic'),
+    dict(
+        type='Translate',
+        magnitude_key='magnitude',
+        magnitude_range=(0, 0.3),
+        pad_val=0,
+        prob=0.5,
+        direction='vertical',
+        random_negative_prob=0.5,
+        interpolation='bicubic')
+]
+train_pipeline = [
+    dict(
+        type='LoadImageFromFile',
+        file_client_args=dict(
+            backend='memcached',
+            server_list_cfg=  # noqa
+            '/mnt/lustre/share/memcached_client/server_list.conf',  # noqa
+            client_cfg='/mnt/lustre/share/memcached_client/client.conf')),
+    dict(
+        type='RandAugment',
+        policies=policies,
+        num_policies=2,
+        magnitude_level=12),
+    dict(type='RandomResizedCrop', size=224),
+    dict(type='RandomFlip', flip_prob=0.5, direction='horizontal'),
+    dict(type='ColorJitter', brightness=0.4, contrast=0.4, saturation=0.4),
+    dict(type='Lighting', **img_lighting_cfg),
+    dict(
+        type='Normalize',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        to_rgb=False),
+    dict(type='ImageToTensor', keys=['img']),
+    dict(type='ToTensor', keys=['gt_label']),
+    dict(type='Collect', keys=['img', 'gt_label'])
+]
+test_pipeline = [
+    dict(
+        type='LoadImageFromFile',
+        file_client_args=dict(
+            backend='memcached',
+            server_list_cfg=  # noqa
+            '/mnt/lustre/share/memcached_client/server_list.conf',  # noqa
+            client_cfg='/mnt/lustre/share/memcached_client/client.conf')),
+    dict(type='Resize', size=(256, -1)),
+    dict(type='CenterCrop', crop_size=224),
+    dict(
+        type='Normalize',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        to_rgb=True),
+    dict(type='ImageToTensor', keys=['img']),
+    dict(type='Collect', keys=['img'])
+]
+data = dict(
+    samples_per_gpu=64,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        data_prefix='data/imagenet/train',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        data_prefix='data/imagenet/val',
+        ann_file='data/imagenet/meta/val.txt',
+        pipeline=test_pipeline),
+    test=dict(
+        # replace `data/val` with `data/test` for standard test
+        type=dataset_type,
+        data_prefix='data/imagenet/val',
+        ann_file='data/imagenet/meta/val.txt',
+        pipeline=test_pipeline))
+evaluation = dict(interval=1, metric='accuracy')
+
+# model settings
+model = dict(
+    type='ImageClassifier',
+    backbone=dict(
+        type='ResNeSt',
+        depth=50,
+        num_stages=4,
+        out_indices=(3, ),
+        style='pytorch'),
+    neck=dict(type='GlobalAveragePooling'),
+    head=dict(
+        type='MultiLabelLinearClsHead',
+        num_classes=1000,
+        in_channels=2048,
+        loss=dict(
+            type='LabelSmoothLoss',
+            loss_weight=1.0,
+            label_smooth_val=0.1,
+            reduction='mean')))
+train_cfg = dict(mixup=dict(alpha=0.2, num_classes=1000))
 
 # batch_size = 4096 8x8gpus
 # optimizer
 optimizer = dict(
     type='SGD',
-    lr=1.6,
+    lr=1.6 / 4,
     momentum=0.9,
     weight_decay=0.0001,
     paramwise_cfg=dict(bias_decay_mult=0., norm_decay_mult=0.))
@@ -21,3 +206,10 @@ lr_config = dict(
     warmup_ratio=0.001,
     warmup_by_epoch=True)
 runner = dict(type='EpochBasedRunner', max_epochs=270)
+
+log_config = dict(
+    interval=10,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        # dict(type='TensorboardLoggerHook')
+    ])
