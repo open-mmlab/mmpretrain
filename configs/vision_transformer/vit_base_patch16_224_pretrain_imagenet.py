6d78351c44639a7d86f13baeee1377cdbb2e4884
HIT-cwh <2892770585@qq.com>
2021-04-15 15:16:23 +0800

    rename imagenet_bs4096.py to imagenet_bs2048_AdamW.py

--- configs/vision_transformer/vit_base_patch16_224_pretrain_imagenet.py
+++ configs/vision_transformer/vit_base_patch16_224_pretrain_imagenet.py
@@ -1,5 +1,6 @@
 _base_ = [
     '../_base_/models/vit_base_patch16_224_pretrain.py',
     '../_base_/datasets/imagenet_bs32_pil_resize.py',
-    '../_base_/schedules/imagenet_bs4096.py', '../_base_/default_runtime.py'
+    '../_base_/schedules/imagenet_bs2048_AdamW.py',
+    '../_base_/default_runtime.py'
 ]
