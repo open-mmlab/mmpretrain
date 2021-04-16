aaf909e491db53dcc0734ac57491a860a6e3f692
QingChuanWS <bingshan45@163.com>
2021-04-16 15:38:04 +0800

    update code and doc

--- tools/ort_test.py
+++ tools/ort_test.py
@@ -3,105 +3,62 @@ import warnings
 
 import mmcv
 import numpy as np
-import onnx
-import onnxruntime as rt
+import onnxruntime as ort
 from mmcv import DictAction
-from torch import nn
+from mmcv.parallel import MMDataParallel
 
 from mmcls.apis import single_gpu_test
 from mmcls.datasets import build_dataloader, build_dataset
+from mmcls.models.classifiers import BaseClassifier
 
 
-class ORTModel(nn.Module):
-    """Warp onnxruntime model and verify model based on dataset.
+class ONNXRuntimeClassifier(BaseClassifier):
+    """Wrapper for classifier's inference with ONNXRuntime."""
 
-    Args:
-        onnx_file: the onnx file path.
-        batch_infer: whether to support batch inference for the model.
-    """
-
-    def __init__(self, onnx_file, batch_infer=True):
-        super(ORTModel, self).__init__()
-        self.onnx_file = onnx_file
-        self.batch_infer = batch_infer
-        self.onnx_model = onnx.load(onnx_file)
-        onnx.checker.check_model(self.onnx_model)
-
+    def __init__(self, onnx_file, class_names, device_id):
+        super(ONNXRuntimeClassifier, self).__init__()
         # check if the input number is coorect.
-        input_all = [node.name for node in self.onnx_model.graph.input]
-        input_initializer = [
-            node.name for node in self.onnx_model.graph.initializer
-        ]
-        self.net_feed_input = list(set(input_all) - set(input_initializer))
-        assert (len(self.net_feed_input) == 1)
+        sess = ort.InferenceSession(onnx_file)
 
-        # init onnxruntime inference seession
-        self.sess = rt.InferenceSession(self.onnx_file)
+        self.sess = sess
+        self.CLASSES = class_names
+        self.device_id = device_id
+        self.io_binding = sess.io_binding()
+        self.output_names = [_.name for _ in sess.get_outputs()]
 
-    def forward(self, *input, **data):
-        """Run onnxruntime inference.
-        Args:
-            data(dict): a dict containing images data and image meta data.
+    def simple_test(self, img, img_metas, **kwargs):
+        raise NotImplementedError('This method is not implemented.')
 
-        Return:
-            output(List[np.array]): a list of np.array, the dimension of
-                np.array is the number of CLASSES.
-        """
-        retsults = []
-        if self.batch_infer:
-            result = self.sess.run(
-                None,
-                {self.net_feed_input[0]: data['img'].detach().numpy()})[0]
-            batch_size = data['img'].size(0)
-            for i in range(batch_size):
-                retsults.append(result[i])
-        else:
-            batch_size = data['img'].size(0)
-            for i in range(batch_size):
-                result = self.sess.run(
-                    None, {
-                        self.net_feed_input[0]:
-                        data['img'][i].unsqueeze(0).detach().numpy()
-                    })[0]
-                retsults.append(result[i])
-        return retsults
+    def extract_feat(self, imgs):
+        raise NotImplementedError('This method is not implemented.')
 
+    def forward_train(self, imgs, **kwargs):
+        raise NotImplementedError('This method is not implemented.')
 
-def ort_test(config,
-             onnx_model,
-             output_file='results.pkl',
-             batch_infer=True,
-             metrics=None,
-             metric_options=None):
-    cfg = mmcv.Config.fromfile(config)
-    # build dataset and dataloader
-    dataset = build_dataset(cfg.data.test)
-    data_loader = build_dataloader(
-        dataset,
-        samples_per_gpu=cfg.data.samples_per_gpu,
-        workers_per_gpu=cfg.data.workers_per_gpu,
-        shuffle=False,
-        round_up=False)
-    # build onnxruntime model and run inference.
-    model = ORTModel(onnx_model, batch_infer)
-    outputs = single_gpu_test(model, data_loader)
+    def forward_test(self, imgs, img_metas, **kwargs):
+        assert ort.get_device() == 'GPU', \
+            ('The dataset accuracy verification defaultly uses GPU'
+                'for inference. Please install the onnxruntime-gpu model.')
+        input_data = imgs
+        batch_size = imgs.shape[0]
+        # set io binding for inputs/outputs
+        self.io_binding.bind_input(
+            name='input',
+            device_type='cuda',
+            device_id=self.device_id,
+            element_type=np.float32,
+            shape=list(imgs.shape),
+            buffer_ptr=input_data.data_ptr())
+        for name in self.output_names:
+            self.io_binding.bind_output(name)
+        # run session to get outputs
+        self.sess.run_with_iobinding(self.io_binding)
+        result = self.io_binding.copy_outputs_to_cpu()[0]
 
-    if metrics:
-        results = dataset.evaluate(outputs, metrics, metric_options)
-        for k, v in results.items():
-            print(f'\n{k} : {v:.2f}')
-    else:
-        warnings.warn('Evaluation metrics are not specified.')
-        scores = np.vstack(outputs)
-        pred_score = np.max(scores, axis=1)
-        pred_label = np.argmax(scores, axis=1)
-        pred_class = [dataset.CLASSES[lb] for lb in pred_label]
-        results = {
-            'pred_score': pred_score,
-            'pred_label': pred_label,
-            'pred_class': pred_class
-        }
-    mmcv.dump(results, output_file)
+        results = []
+        for i in range(batch_size):
+            results.append(result[i])
+        return results
 
 
 def parse_args():
@@ -109,12 +66,14 @@ def parse_args():
         description='Use Dataset to Verify Model Accuracy.')
     parser.add_argument('config', help='test config file path')
     parser.add_argument('model', help='filename of the input ONNX model')
-    parser.add_argument('--output-file', type=str, default='results.pkl')
     parser.add_argument(
-        '--batch-infer',
-        action='store_false',
-        help='Whether to support batch inference for the model. \
-            Defaults to True.')
+        '--out', type=str, help='output result file in pickle format')
+    parser.add_argument(
+        '--cfg-options',
+        nargs='+',
+        action=DictAction,
+        help='override some settings in the used config, the key-value pair '
+        'in xxx=yyy format will be merged into config file.')
     parser.add_argument(
         '--metrics',
         type=str,
@@ -131,17 +90,65 @@ def parse_args():
         help='custom options for evaluation, the key-value pair in xxx=yyy '
         'format will be parsed as a dict metric_options for dataset.evaluate()'
         ' function.')
+    parser.add_argument('--show', action='store_true', help='show results')
+    parser.add_argument(
+        '--show-dir', help='directory where painted images will be saved')
     args = parser.parse_args()
     return args
 
 
-if __name__ == '__main__':
+def main():
     args = parse_args()
 
-    ort_test(
-        args.config,
-        args.model,
-        output_file=args.output_file,
-        metrics=args.metrics,
-        metric_options=args.metric_options,
-        batch_infer=args.batch_infer)
+    if args.out is not None and not args.out.endswith(('.pkl', '.pickle')):
+        raise ValueError('The output file must be a pkl file.')
+
+    cfg = mmcv.Config.fromfile(args.config)
+    if args.cfg_options is not None:
+        cfg.merge_from_dict(args.cfg_options)
+
+    # build dataset and dataloader
+    dataset = build_dataset(cfg.data.test)
+    data_loader = build_dataloader(
+        dataset,
+        samples_per_gpu=cfg.data.samples_per_gpu,
+        workers_per_gpu=cfg.data.workers_per_gpu,
+        shuffle=False,
+        round_up=False)
+
+    # build onnxruntime model and run inference.
+    model = ONNXRuntimeClassifier(
+        args.model, class_names=dataset.CLASSES, device_id=0)
+
+    model = MMDataParallel(model, device_ids=[0])
+    outputs = single_gpu_test(model, data_loader, args.show, args.show_dir)
+
+    if args.metrics:
+        results = dataset.evaluate(outputs, args.metrics, args.metric_options)
+
+        for k, v in results.items():
+            print(f'\n{k} : {v:.2f}')
+    else:
+        warnings.warn('Evaluation metrics are not specified.')
+        scores = np.vstack(outputs)
+        pred_score = np.max(scores, axis=1)
+        pred_label = np.argmax(scores, axis=1)
+        pred_class = [dataset.CLASSES[lb] for lb in pred_label]
+        results = {
+            'pred_score': pred_score,
+            'pred_label': pred_label,
+            'pred_class': pred_class
+        }
+        if not args.out:
+            print('\nthe predicted result for the first element is '
+                  f'pred_score = {pred_score[0]:.2f}, '
+                  f'pred_label = {pred_label[0]} '
+                  f'and pred_class = {pred_class[0]}. '
+                  'Specify --out to save all results to files.')
+    if args.out:
+        print(f'\nwriting results to {args.out}')
+        mmcv.dump(results, args.out)
+
+
+if __name__ == '__main__':
+    main()
