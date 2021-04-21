81e6a3e41662166ae72d00ff16cb4e780e8def69
HIT-cwh <2892770585@qq.com>
2021-04-21 22:13:37 +0800

    inherit from mmcv BaseModule

--- mmcls/models/backbones/base_backbone.py
+++ mmcls/models/backbones/base_backbone.py
@@ -1,19 +1,19 @@
 import logging
 from abc import ABCMeta, abstractmethod
 
-import torch.nn as nn
 from mmcv.runner import load_checkpoint
+from mmcv.runner.base_module import BaseModule
 
 
-class BaseBackbone(nn.Module, metaclass=ABCMeta):
+class BaseBackbone(BaseModule, metaclass=ABCMeta):
     """Base backbone.
 
     This class defines the basic functions of a backbone. Any backbone that
     inherits this class should at least define its own `forward` function.
     """
 
-    def __init__(self):
-        super(BaseBackbone, self).__init__()
+    def __init__(self, init_cfg):
+        super(BaseBackbone, self).__init__(init_cfg)
 
     def init_weights(self, pretrained=None):
         """Init backbone weights.
@@ -24,6 +24,7 @@ class BaseBackbone(nn.Module, metaclass=ABCMeta):
                 checkpoint. If pretrained is None, then it follows default
                 initializer or customized initializer in subclasses.
         """
+        super(BaseBackbone, self).init_weights()
         if isinstance(pretrained, str):
             logger = logging.getLogger()
             load_checkpoint(self, pretrained, strict=False, logger=logger)
