28b8a467ac842b9011cf2ea0f2b8cd8466da8cd0
HIT-cwh <2892770585@qq.com>
2021-04-22 23:00:41 +0800

    sync with model

--- configs/_base_/models/vit_base_patch16_384_finetune.py
+++ configs/_base_/models/vit_base_patch16_384_finetune.py
@@ -3,14 +3,38 @@ model = dict(
     type='ImageClassifier',
     backbone=dict(
         type='VisionTransformer',
-        num_layers=12,
         embed_dim=768,
-        num_heads=12,
-        img_size=384,
+        img_size=224,
         patch_size=16,
         in_channels=3,
-        feedforward_channels=3072,
-        drop_rate=0.1),
+        drop_rate=0.,
+        hybrid_backbone=None,
+        norm_cfg=dict(type='LN'),
+        encoder=dict(
+            type='TransformerEncoder',
+            num_layers=12,
+            transformerlayers=dict(
+                type='VitTransformerEncoderLayer',
+                attn_cfgs=[
+                    dict(
+                        type='MultiheadAttention',
+                        embed_dims=768,
+                        num_heads=12,
+                        dropout=0.1)
+                ],
+                feedforward_channels=3072,
+                ffn_dropout=0.1,
+                operation_order=('norm', 'self_attn', 'norm', 'ffn')),
+            init_cfg=[
+                dict(type='Xavier', layer='Linear', distribution='normal')
+            ]),
+        init_cfg=[
+            dict(
+                type='Kaiming',
+                layer='Conv2d',
+                mode='fan_in',
+                nonlinearity='linear')
+        ]),
     neck=None,
     head=dict(
         type='VisionTransformerClsHead',
