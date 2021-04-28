c5b38ac458d14d479baefa5bb1bfb93f4c0a517c
HIT-cwh <2892770585@qq.com>
2021-04-28 15:01:27 +0800

    replace paras names with *args and **kwargs

--- mmcls/models/backbones/vision_transformer.py
+++ mmcls/models/backbones/vision_transformer.py
@@ -18,47 +18,21 @@ class VitTransformerEncoderLayer(BaseTransformerLayer):
     """Implements encoder layer in Vit transformer.
 
     Args:
-        attn_cfgs (list[`mmcv.ConfigDict`] | list[dict] | dict )):
-            Configs for self_attention or cross_attention, the order
-            should be consistent with it in `operation_order`. If it is
-            a dict, it would be expand to the number of attention in
-            `operation_order`.
-        feedforward_channels (int): The hidden dimension for FFNs.
         ffn_dropout (float): Probability of an element to be zeroed
             in ffn. Default 0.0.
-        operation_order (tuple[str]): The execution order of operation
-            in transformer. Such as ('self_attn', 'norm', 'ffn', 'norm').
-            Default：None
         act_cfg (dict): The activation config for FFNs.
-        norm_cfg (dict): Config dict for normalization layer.
-        ffn_num_fcs (int): The number of fully-connected layers in FFNs.
-            Default：2.
     """
 
     def __init__(self,
-                 attn_cfgs,
-                 feedforward_channels,
+                 *args,
                  ffn_dropout=0.0,
-                 operation_order=None,
                  act_cfg=dict(type='GELU'),
-                 norm_cfg=dict(type='LN'),
-                 ffn_num_fcs=2,
                  **kwargs):
         super(VitTransformerEncoderLayer, self).__init__(
-            attn_cfgs=attn_cfgs,
-            feedforward_channels=feedforward_channels,
-            operation_order=operation_order,
-            ffn_dropout=ffn_dropout,
-            act_cfg=act_cfg,
-            norm_cfg=norm_cfg,
-            ffn_num_fcs=ffn_num_fcs,
-            **kwargs)
+            *args, ffn_dropout=ffn_dropout, act_cfg=act_cfg, **kwargs)
         assert len(self.operation_order) == 4
         assert set(self.operation_order) == set(['self_attn', 'norm', 'ffn'])
 
-    # TODO: wait for the new version of mmcv, the current mmcv does not
-    #  support initializing a layer with weights according to xavier_normal_
-    #  and bias according to normal_
     def init_weights(self):
         super(VitTransformerEncoderLayer, self).init_weights()
         for ffn in self.ffns:
