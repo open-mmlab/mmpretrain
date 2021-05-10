13754366ee88ce51aa475c1357cc4f416d968d67
mzr1996 <mzr1996@163.com>
2021-05-10 01:03:21 -0400

    Change 'mode' attribute in GAP to 'dim', and add docstring

--- tests/test_neck.py
+++ tests/test_neck.py
@@ -7,7 +7,7 @@ from mmcls.models.necks import GlobalAveragePooling
 def test_gap_neck():
 
     # test 1d gap_neck
-    neck = GlobalAveragePooling(mode='1d')
+    neck = GlobalAveragePooling(dim=1)
     # batch_size, num_features, feature_size
     fake_input = torch.rand(1, 16, 24)
 
@@ -16,7 +16,7 @@ def test_gap_neck():
     assert output.shape == (1, 16)
 
     # test 1d gap_neck
-    neck = GlobalAveragePooling(mode='2d')
+    neck = GlobalAveragePooling(dim=2)
     # batch_size, num_features, feature_size(2)
     fake_input = torch.rand(1, 16, 24, 24)
 
@@ -25,7 +25,7 @@ def test_gap_neck():
     assert output.shape == (1, 16)
 
     # test 1d gap_neck
-    neck = GlobalAveragePooling(mode='3d')
+    neck = GlobalAveragePooling(dim=3)
     # batch_size, num_features, feature_size(3)
     fake_input = torch.rand(1, 16, 24, 24, 5)
 
@@ -33,6 +33,6 @@ def test_gap_neck():
     # batch_size, num_features
     assert output.shape == (1, 16)
 
-    with pytest.raises(NotImplementedError):
-        # mode must in ['1d', '2d', '3d']
-        GlobalAveragePooling(mode='other')
+    with pytest.raises(AssertionError):
+        # dim must in [1, 2, 3]
+        GlobalAveragePooling(dim='other')
