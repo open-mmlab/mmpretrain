7c2009844ab204fe84fd287e559c64a126eb93da
HIT-cwh <2892770585@qq.com>
2021-05-14 11:24:14 +0800

    add mixup_prob to BatchMixupLayer

--- mmcls/models/utils/mixup.py
+++ mmcls/models/utils/mixup.py
@@ -1,8 +1,8 @@
 from abc import ABCMeta, abstractmethod
 
+import numpy as np
 import torch
 import torch.nn.functional as F
-from torch.distributions.beta import Beta
 
 
 class BaseMixupLayer(object, metaclass=ABCMeta):
@@ -22,29 +22,34 @@ class BatchMixupLayer(BaseMixupLayer):
     Args:
         alpha (float): Parameters for Beta distribution.
         num_classes (int): The number of classes.
+        mixup_prob (float): MixUp probability. It should be in range [0, 1]
     """
 
-    def __init__(self, alpha, num_classes):
+    def __init__(self, alpha, num_classes, mixup_prob):
         super(BatchMixupLayer, self).__init__()
 
-        assert isinstance(alpha, float)
+        assert isinstance(alpha, float) and alpha > 0
         assert isinstance(num_classes, int)
+        assert isinstance(mixup_prob, float) and 0.0 <= mixup_prob <= 1.0
 
         self.alpha = alpha
         self.num_classes = num_classes
-
-        self.Beta = Beta(self.alpha, self.alpha)
+        self.mixup_prob = mixup_prob
 
     def mixup(self, img, gt_label):
-        lam = self.Beta.sample()
-        batch_size = img.size(0)
-        index = torch.randperm(batch_size)
         one_hot_gt_label = F.one_hot(gt_label, num_classes=self.num_classes)
+        r = np.random.rand(1)
+        if r < self.mixup_prob:
+            lam = np.random.beta(self.alpha, self.alpha)
+            batch_size = img.size(0)
+            index = torch.randperm(batch_size)
 
-        mixed_img = lam * img + (1 - lam) * img[index, :]
-        mixed_gt_label = lam * one_hot_gt_label + (
-            1 - lam) * one_hot_gt_label[index, :]
-        return mixed_img, mixed_gt_label
+            mixed_img = lam * img + (1 - lam) * img[index, :]
+            mixed_gt_label = lam * one_hot_gt_label + (
+                1 - lam) * one_hot_gt_label[index, :]
+            return mixed_img, mixed_gt_label
+        else:
+            return img, one_hot_gt_label
 
     def __call__(self, img, gt_label):
         return self.mixup(img, gt_label)
