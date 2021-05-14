3fbe45c77ee2bd65b6b025db73a49455bef4d051
HIT-cwh <2892770585@qq.com>
2021-05-14 12:00:36 +0800

    test classifier with cutmixup

--- tests/test_classifiers.py
+++ tests/test_classifiers.py
@@ -20,7 +20,7 @@ def test_image_classifier():
             in_channels=2048,
             loss=dict(type='CrossEntropyLoss', loss_weight=1.0,
                       use_soft=True)),
-        train_cfg=dict(mixup=dict(alpha=1.0, num_classes=10)))
+        train_cfg=dict(cutmixup=dict(mixup_alpha=1.0, num_classes=10)))
     img_classifier = ImageClassifier(**model_cfg)
     img_classifier.init_weights()
     imgs = torch.randn(16, 3, 32, 32)
@@ -47,8 +47,7 @@ def test_image_classifier_with_cutmix():
             in_channels=2048,
             loss=dict(type='CrossEntropyLoss', loss_weight=1.0,
                       use_soft=True)),
-        train_cfg=dict(
-            cutmix=dict(alpha=1.0, num_classes=10, cutmix_prob=1.0)))
+        train_cfg=dict(cutmixup=dict(cutmix_alpha=1.0, num_classes=10)))
     img_classifier = ImageClassifier(**model_cfg)
     img_classifier.init_weights()
     imgs = torch.randn(16, 3, 32, 32)
@@ -74,7 +73,7 @@ def test_image_classifier_with_label_smooth_loss():
             num_classes=10,
             in_channels=2048,
             loss=dict(type='LabelSmoothLoss', label_smooth_val=0.1)),
-        train_cfg=dict(mixup=dict(alpha=1.0, num_classes=10)))
+        train_cfg=dict(cutmixup=dict(mixup_alpha=1.0, num_classes=10)))
     img_classifier = ImageClassifier(**model_cfg)
     img_classifier.init_weights()
     imgs = torch.randn(16, 3, 32, 32)
@@ -107,11 +106,11 @@ def test_image_classifier_vit():
             loss=dict(type='CrossEntropyLoss', loss_weight=1.0, use_soft=True),
             topk=(1, 5),
         ),
-        train_cfg=dict(mixup=dict(alpha=0.2, num_classes=1000)))
+        train_cfg=dict(cutmixup=dict(mixup_alpha=0.2)))
     img_classifier = ImageClassifier(**model_cfg)
     img_classifier.init_weights()
-    imgs = torch.randn(16, 3, 224, 224)
-    label = torch.randint(0, 1000, (16, ))
+    imgs = torch.randn(2, 3, 224, 224)
+    label = torch.randint(0, 1000, (2, ))
 
     losses = img_classifier.forward_train(imgs, label)
     assert losses['loss'].item() > 0
