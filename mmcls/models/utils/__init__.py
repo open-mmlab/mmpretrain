5433893199bd0493ea59b14bb466c0bbf4b2ec9b
HIT-cwh <2892770585@qq.com>
2021-05-14 11:26:32 +0800

    add cutmixup to __init__

--- mmcls/models/utils/__init__.py
+++ mmcls/models/utils/__init__.py
@@ -1,5 +1,6 @@
 from .channel_shuffle import channel_shuffle
 from .cutmix import BatchCutMixLayer
+from .cutmixup import CutMixUp
 from .helpers import to_2tuple, to_3tuple, to_4tuple, to_ntuple
 from .inverted_residual import InvertedResidual
 from .make_divisible import make_divisible
@@ -9,5 +10,5 @@ from .se_layer import SELayer
 __all__ = [
     'channel_shuffle', 'make_divisible', 'InvertedResidual', 'BatchMixupLayer',
     'BatchCutMixLayer', 'SELayer', 'to_ntuple', 'to_2tuple', 'to_3tuple',
-    'to_4tuple'
+    'to_4tuple', 'CutMixUp'
 ]
