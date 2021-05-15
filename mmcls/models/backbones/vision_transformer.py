5f170a4906c14ad4e2d2663fd1e03f8c00420d42
HIT-cwh <2892770585@qq.com>
2021-05-15 21:58:17 +0800

    delete permute

--- mmcls/models/backbones/vision_transformer.py
+++ mmcls/models/backbones/vision_transformer.py
@@ -286,13 +286,6 @@ class VisionTransformer(BaseBackbone):
         x = x + self.pos_embed
         x = self.drop_after_pos(x)
 
-        # Reason for permute:
-        # as the shape of input x is [batch_size, num_query, embed_dim],
-        # but the one from nn.MultiheadAttention is
-        # [num_query, batch_size, embed_dim]
-        x = x.permute(1, 0, 2)
         x = self.encoder(query=x, key=None, value=None)
-        # Convert the shape back to [batch_size, num_query, embed_dim]
-        x = x.permute(1, 0, 2)
 
         return x[:, 0]
