2dcdc2d241d4a9c4f6f510fb93dbe8e4c736112f
HIT-cwh <2892770585@qq.com>
2021-05-22 16:39:35 +0800

    test cutmixup

--- tests/test_classifiers.py
+++ tests/test_classifiers.py
@@ -57,6 +57,82 @@ def test_image_classifier_with_cutmix():
     assert losses['loss'].item() > 0
 
 
+def test_image_classifier_with_cutmixup():
+
+    imgs = torch.randn(16, 3, 32, 32)
+    label = torch.randint(0, 10, (16, ))
+
+    # Test cutmix and mixup in ImageClassifier
+    model_cfg = dict(
+        backbone=dict(
+            type='ResNet_CIFAR',
+            depth=50,
+            num_stages=4,
+            out_indices=(3, ),
+            style='pytorch'),
+        neck=dict(type='GlobalAveragePooling'),
+        head=dict(
+            type='MultiLabelLinearClsHead',
+            num_classes=10,
+            in_channels=2048,
+            loss=dict(type='CrossEntropyLoss', loss_weight=1.0,
+                      use_soft=True)),
+        train_cfg=dict(
+            cutmixup=dict(cutmix_alpha=1.0, mixup_alpha=0.2, num_classes=10)))
+    img_classifier = ImageClassifier(**model_cfg)
+    img_classifier.init_weights()
+
+    losses = img_classifier.forward_train(imgs, label)
+    assert losses['loss'].item() > 0
+
+    # Test cutmix in ImageClassifier
+    model_cfg['train_cfg'] = dict(cutmix=dict(alpha=1.0, num_classes=10))
+    img_classifier = ImageClassifier(**model_cfg)
+    img_classifier.init_weights()
+
+    losses = img_classifier.forward_train(imgs, label)
+    assert losses['loss'].item() > 0
+
+    # Test cutmix with cutmix_minmax in ImageClassifier
+    model_cfg['train_cfg'] = dict(
+        cutmix=dict(alpha=1.0, num_classes=10, cutmix_minmax=[0.2, 0.8]))
+    img_classifier = ImageClassifier(**model_cfg)
+    img_classifier.init_weights()
+
+    losses = img_classifier.forward_train(imgs, label)
+    assert losses['loss'].item() > 0
+
+    # Test mixup in ImageClassifier
+    model_cfg['train_cfg'] = dict(mixup=dict(alpha=1.0, num_classes=10))
+    img_classifier = ImageClassifier(**model_cfg)
+    img_classifier.init_weights()
+
+    losses = img_classifier.forward_train(imgs, label)
+    assert losses['loss'].item() > 0
+
+    # Test not using cutmix and mixup in ImageClassifier
+    model_cfg = dict(
+        backbone=dict(
+            type='ResNet_CIFAR',
+            depth=50,
+            num_stages=4,
+            out_indices=(3, ),
+            style='pytorch'),
+        neck=dict(type='GlobalAveragePooling'),
+        head=dict(
+            type='LinearClsHead',
+            num_classes=10,
+            in_channels=2048,
+            loss=dict(type='CrossEntropyLoss', loss_weight=1.0)))
+    img_classifier = ImageClassifier(**model_cfg)
+    img_classifier.init_weights()
+    imgs = torch.randn(16, 3, 32, 32)
+    label = torch.randint(0, 10, (16, ))
+
+    losses = img_classifier.forward_train(imgs, label)
+    assert losses['loss'].item() > 0
+
+
 def test_image_classifier_with_label_smooth_loss():
 
     # Test mixup in ImageClassifier
