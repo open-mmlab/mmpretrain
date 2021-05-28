626658bce552a53b31b8bc8a4e8a8b50738f5a32
lixinran <lixr423@outlook.com>
2021-05-28 17:15:19 +0800

    fix bug

--- configs/efficientnet/efficientnet_b2_b32x8_imagenet.py
+++ configs/efficientnet/efficientnet_b2_b32x8_imagenet.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/efficientnet_b0.py',
+    '../_base_/models/efficientnet_b2.py',
     '../_base_/datasets/imagenet_bs32.py',
     '../_base_/schedules/imagenet_bs256.py',
     '../_base_/default_runtime.py',
