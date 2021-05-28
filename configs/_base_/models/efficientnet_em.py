53845c1d97571737259aa6bc3c122efdb4428bed
lixinran <lixr423@outlook.com>
2021-05-28 16:52:02 +0800

    add more configs

---
+++ configs/_base_/models/efficientnet_em.py
@@ -0,0 +1,16 @@
+# model settings
+model = dict(
+    type='ImageClassifier',
+    backbone=dict(
+        type='EfficientNet',
+        arch='em',
+        conv_cfg=dict(type='Conv2dAdaptivePadding'),
+        norm_cfg=dict(type='BN', eps=1e-3)),
+    neck=dict(type='GlobalAveragePooling'),
+    head=dict(
+        type='LinearClsHead',
+        num_classes=1000,
+        in_channels=1280,
+        loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+        topk=(1, 5),
+    ))
