e634a1f48bd017c7be7e1e0b0df6e2f87c911b10
HIT-cwh <2892770585@qq.com>
2021-05-30 14:30:35 +0800

    delete cutmixup.py

--- mmcls/models/utils/__init__.py
+++ mmcls/models/utils/__init__.py
@@ -1,7 +1,6 @@
 from .augment.cutmix import BatchCutMixLayer
 from .augment.mixup import BatchMixupLayer
 from .channel_shuffle import channel_shuffle
-from .cutmixup import CutMixUp
 from .helpers import to_2tuple, to_3tuple, to_4tuple, to_ntuple
 from .inverted_residual import InvertedResidual
 from .make_divisible import make_divisible
@@ -10,5 +9,5 @@ from .se_layer import SELayer
 __all__ = [
     'channel_shuffle', 'make_divisible', 'InvertedResidual', 'BatchMixupLayer',
     'BatchCutMixLayer', 'SELayer', 'to_ntuple', 'to_2tuple', 'to_3tuple',
-    'to_4tuple', 'CutMixUp'
+    'to_4tuple'
 ]
