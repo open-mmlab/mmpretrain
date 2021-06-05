fd6163176f1797ecd33833f6f72bb66642bc9358
HIT-cwh <2892770585@qq.com>
2021-06-05 13:01:23 +0800

    replace xxx with self.xxx

--- mmcls/models/utils/augment/cutmix.py
+++ mmcls/models/utils/augment/cutmix.py
@@ -11,13 +11,14 @@ class BaseCutMixLayer(object, metaclass=ABCMeta):
     """Base class for CutMixLayer.
 
     Args:
-        alpha (float): Parameters for Beta distribution. Positive(>0).
-        num_classes (int): The number of classes.
+        alpha (float): Parameters for Beta distribution. Positive(>0)
+        num_classes (int): The number of classes
         prob (float): MixUp probability. It should be in range [0, 1].
             Default to 1.0
-        cutmix_minmax (List[float], optional): cutmix min/max image ratio,
-            cutmix is active and uses this vs alpha if not None.
-        correct_lam (bool): apply lambda correction when cutmix bbox
+        cutmix_minmax (List[float], optional): cutmix min/max image ratio.
+            (as percent of image size). When cutmix_minmax is not None, we
+            generate cutmix bounding-box using cutmix_minmax instead of alpha
+        correct_lam (bool): Whether to apply lambda correction when cutmix bbox
             clipped by image borders. Default to True
     """
 
@@ -39,26 +40,28 @@ class BaseCutMixLayer(object, metaclass=ABCMeta):
         self.cutmix_minmax = cutmix_minmax
         self.correct_lam = correct_lam
 
-    def rand_bbox_minmax(self, img_shape, minmax, count=None):
-        """Min-Max CutMix bounding-box Inspired by Darknet cutmix impl,
-        generates a random rectangular bbox based on min/max percent values
-        applied to each dimension of the input image.
+    def rand_bbox_minmax(self, img_shape, count=None):
+        """Min-Max CutMix bounding-box Inspired by Darknet cutmix
+        implementation. It generates a random rectangular bbox based on min/max
+        percent values applied to each dimension of the input image.
 
         Typical defaults for minmax are usually in the  .2-.3 for min and
         .8-.9 range for max.
 
         Args:
             img_shape (tuple): Image shape as tuple
-            minmax (tuple or list): Min and max bbox ratios
-                (as percent of image size)
             count (int, optional): Number of bbox to generate. Default to None
         """
-        assert len(minmax) == 2
+        assert len(self.cutmix_minmax) == 2
         img_h, img_w = img_shape[-2:]
         cut_h = np.random.randint(
-            int(img_h * minmax[0]), int(img_h * minmax[1]), size=count)
+            int(img_h * self.cutmix_minmax[0]),
+            int(img_h * self.cutmix_minmax[1]),
+            size=count)
         cut_w = np.random.randint(
-            int(img_w * minmax[0]), int(img_w * minmax[1]), size=count)
+            int(img_w * self.cutmix_minmax[0]),
+            int(img_w * self.cutmix_minmax[1]),
+            size=count)
         yl = np.random.randint(0, img_h - cut_h, size=count)
         xl = np.random.randint(0, img_w - cut_w, size=count)
         yu = yl + cut_h
@@ -66,9 +69,9 @@ class BaseCutMixLayer(object, metaclass=ABCMeta):
         return yl, yu, xl, xu
 
     def rand_bbox(self, img_shape, lam, margin=0., count=None):
-        """Standard CutMix bounding-box Generates a random square bbox based on
-        lambda value. This impl includes support for enforcing a border margin
-        as percent of bbox dimensions.
+        """Standard CutMix bounding-box that generates a random square bbox
+        based on lambda value. This implementation includes support for
+        enforcing a border margin as percent of bbox dimensions.
 
         Args:
             img_shape (tuple): Image shape as tuple
@@ -89,19 +92,19 @@ class BaseCutMixLayer(object, metaclass=ABCMeta):
         xh = np.clip(cx + cut_w // 2, 0, img_w)
         return yl, yh, xl, xh
 
-    def cutmix_bbox_and_lam(self,
-                            img_shape,
-                            lam,
-                            ratio_minmax=None,
-                            correct_lam=True,
-                            count=None):
-        """Generate bbox and apply lambda correction."""
-        if ratio_minmax is not None:
-            yl, yu, xl, xu = self.rand_bbox_minmax(
-                img_shape, ratio_minmax, count=count)
+    def cutmix_bbox_and_lam(self, img_shape, lam, count=None):
+        """Generate bbox and apply lambda correction.
+
+        Args:
+            img_shape (tuple): Image shape as tuple
+            lam (float): Cutmix lambda value
+            count (int, optional): Number of bbox to generate. Default to None
+        """
+        if self.cutmix_minmax is not None:
+            yl, yu, xl, xu = self.rand_bbox_minmax(img_shape, count=count)
         else:
             yl, yu, xl, xu = self.rand_bbox(img_shape, lam, count=count)
-        if correct_lam or ratio_minmax is not None:
+        if self.correct_lam or self.cutmix_minmax is not None:
             bbox_area = (yu - yl) * (xu - xl)
             lam = 1. - bbox_area / float(img_shape[-2] * img_shape[-1])
         return (yl, yu, xl, xu), lam
@@ -120,24 +123,17 @@ class BatchCutMixLayer(BaseCutMixLayer):
 
     def cutmix(self, img, gt_label):
         one_hot_gt_label = F.one_hot(gt_label, num_classes=self.num_classes)
-        # r = np.random.rand(1)
-        # if r < self.cutmix_prob:
         lam = np.random.beta(self.alpha, self.alpha)
         batch_size = img.size(0)
         index = torch.randperm(batch_size)
 
-        (bby1, bby2, bbx1, bbx2), lam = self.cutmix_bbox_and_lam(
-            img.shape,
-            lam,
-            ratio_minmax=self.cutmix_minmax,
-            correct_lam=self.correct_lam)
+        (bby1, bby2, bbx1,
+         bbx2), lam = self.cutmix_bbox_and_lam(img.shape, lam)
         img[:, :, bby1:bby2, bbx1:bbx2] = \
             img[index, :, bby1:bby2, bbx1:bbx2]
         mixed_gt_label = lam * one_hot_gt_label + (
             1 - lam) * one_hot_gt_label[index, :]
         return img, mixed_gt_label
-        # else:
-        #     return img, one_hot_gt_label
 
     def __call__(self, img, gt_label):
         return self.cutmix(img, gt_label)
