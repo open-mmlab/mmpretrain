3d0c7eddfae03ac8551706e5ec9053a6f807a671
HIT-cwh <2892770585@qq.com>
2021-06-14 23:41:05 +0800

    set custom_hooks_config to cfg.get(custom_hooks, None)

--- mmcls/apis/train.py
+++ mmcls/apis/train.py
@@ -127,10 +127,13 @@ def train_model(model,
         optimizer_config = cfg.optimizer_config
 
     # register hooks
-    runner.register_training_hooks(cfg.lr_config, optimizer_config,
-                                   cfg.checkpoint_config, cfg.log_config,
-                                   cfg.get('momentum_config', None),
-                                   cfg.get('custom_hooks', None))
+    runner.register_training_hooks(
+        cfg.lr_config,
+        optimizer_config,
+        cfg.checkpoint_config,
+        cfg.log_config,
+        cfg.get('momentum_config', None),
+        custom_hooks_config=cfg.get('custom_hooks', None))
     if distributed:
         runner.register_hook(DistSamplerSeedHook())
 
