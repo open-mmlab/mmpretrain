f3da6589999f254ba00f9a5e37151c8acc11eb15
mzr1996 <mzr1996@163.com>
2021-06-28 19:45:45 +0800

    Remove useless comment in config

--- configs/swin_transformer/swin_tiny_224_imagenet.py
+++ configs/swin_transformer/swin_tiny_224_imagenet.py
@@ -1,21 +1,3 @@
-# Inspiration from
-# https://github.com/microsoft/Swin-Transformer/blob/main/config.py
-
-# -[x] EPOCHS = 300
-# -[x] WARMUP_EPOCHS = 20
-# -[x] WEIGHT_DECAY = 0.05
-# -[x] BASE_LR = 5e-4
-# -[x] WARMUP_LR = 5e-7, ratio=1e-3
-# -[x] MIN_LR = 5e-6, ratio=1e-2
-# -[x] CLIP_GRAD = 5.0
-
-# -[x] LR_SCHEDULER.NAME = 'cosine'
-
-# -[x] OPTIMIZER.NAME = 'adamw'
-# -[x] EPS = 1e-8
-# -[x] BETAS = (0.9, 0.999)
-# -[x] MOMENTUM = 0.9
-
 _base_ = [
     '../_base_/models/swin_transformer/tiny_224.py',
     '../_base_/datasets/imagenet_bs128_swin_224.py',
