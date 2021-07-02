d8e2e9097f6bf386a1be4fb95939ab8d0b649078
HIT-cwh <2892770585@qq.com>
2021-07-02 15:07:05 +0800

    add tnt backbone

--- mmcls/models/backbones/__init__.py
+++ mmcls/models/backbones/__init__.py
@@ -12,6 +12,7 @@ from .seresnext import SEResNeXt
 from .shufflenet_v1 import ShuffleNetV1
 from .shufflenet_v2 import ShuffleNetV2
 from .swin_transformer import SwinTransformer
+from .tnt import TNT
 from .vgg import VGG
 from .vision_transformer import VisionTransformer
 
@@ -19,5 +20,5 @@ __all__ = [
     'LeNet5', 'AlexNet', 'VGG', 'RegNet', 'ResNet', 'ResNeXt', 'ResNetV1d',
     'ResNeSt', 'ResNet_CIFAR', 'SEResNet', 'SEResNeXt', 'ShuffleNetV1',
     'ShuffleNetV2', 'MobileNetV2', 'MobileNetV3', 'VisionTransformer',
-    'SwinTransformer'
+    'SwinTransformer', 'TNT'
 ]
