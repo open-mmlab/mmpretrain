9775f656658501e2415abe464bf1af711f4e6098
HIT-cwh <2892770585@qq.com>
2021-07-02 22:18:03 +0800

    set RandomResizedCrop backend=pillow

--- configs/vision_transformer/vit_base_patch16_224_bs4096_pretrain_imagenet.py
+++ configs/vision_transformer/vit_base_patch16_224_bs4096_pretrain_imagenet.py
@@ -129,7 +129,7 @@ img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='RandomResizedCrop', size=224),
+    dict(type='RandomResizedCrop', size=224, backend='pillow'),
     dict(type='RandomFlip', flip_prob=0.5, direction='horizontal'),
     dict(type='AutoAugment', policies=policies),
     dict(type='Normalize', **img_norm_cfg),
