bff47e8fae4451c482500827f255df3a23e501b7
mzr1996 <mzr1996@163.com>
2021-07-09 13:18:47 +0800

    Use pin_memory by default

--- mmcls/datasets/builder.py
+++ mmcls/datasets/builder.py
@@ -49,7 +49,7 @@ def build_dataloader(dataset,
                      shuffle=True,
                      round_up=True,
                      seed=None,
-                     pin_memory=False,
+                     pin_memory=True,
                      persistent_workers=True,
                      **kwargs):
     """Build PyTorch DataLoader.
