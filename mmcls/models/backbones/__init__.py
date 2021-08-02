6e57a6d5920aeff11dc903c9908cd421a99697ef
mzr1996 <mzr1996@163.com>
2021-08-02 17:31:18 +0800

    Fix lint

--- mmcls/models/backbones/__init__.py
+++ mmcls/models/backbones/__init__.py
@@ -15,8 +15,8 @@ from .shufflenet_v1 import ShuffleNetV1
 from .shufflenet_v2 import ShuffleNetV2
 from .swin_transformer import SwinTransformer
 from .timm_backbone import TIMMBackbone
-from .tnt import TNT
 from .t2t_vit import T2T_ViT
+from .tnt import TNT
 from .vgg import VGG
 from .vision_transformer import VisionTransformer
 
