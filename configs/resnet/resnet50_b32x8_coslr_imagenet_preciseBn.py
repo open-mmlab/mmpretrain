63fc73b5e9499a6b5c137eee7bda0a07a188a5fe
Yu Zhaohui <1105212286@qq.com>
2021-08-06 11:10:16 +0800

    add precise-bn config

---
+++ configs/resnet/resnet50_b32x8_coslr_imagenet_preciseBn.py
@@ -0,0 +1,8 @@
+_base_ = [
+    '../_base_/models/resnet50.py', '../_base_/datasets/imagenet_bs32.py',
+    '../_base_/schedules/imagenet_bs256_coslr.py',
+    '../_base_/default_runtime.py'
+]
+
+# precise bn
+precise_bn = dict(num_iters=200, interval=1)
