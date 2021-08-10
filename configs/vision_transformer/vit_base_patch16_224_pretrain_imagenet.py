b98a477b5e13a65aa31d362f9bc7951f92b9e6ea
mzr1996 <mzr1996@163.com>
2021-08-10 12:09:07 +0800

    Use base variable to simplify auto_aug setting

--- configs/vision_transformer/vit_base_patch16_224_pretrain_imagenet.py
+++ configs/vision_transformer/vit_base_patch16_224_pretrain_imagenet.py
@@ -1,103 +1,9 @@
 _base_ = [
     '../_base_/models/vit_base_patch16_224_pretrain.py',
     '../_base_/datasets/imagenet_bs64_pil_resize.py',
+    '../_base_/datasets/pipeline/auto_aug.py',
     '../_base_/schedules/imagenet_bs4096_AdamW.py',
     '../_base_/default_runtime.py'
-]
-
-policies = [
-    [
-        dict(type='Posterize', bits=4, prob=0.4),
-        dict(type='Rotate', angle=30., prob=0.6)
-    ],
-    [
-        dict(type='Solarize', thr=256 / 9 * 4, prob=0.6),
-        dict(type='AutoContrast', prob=0.5)
-    ],
-    [dict(type='Equalize', prob=0.8),
-     dict(type='Equalize', prob=0.6)],
-    [
-        dict(type='Posterize', bits=5, prob=0.6),
-        dict(type='Posterize', bits=5, prob=0.6)
-    ],
-    [
-        dict(type='Equalize', prob=0.4),
-        dict(type='Solarize', thr=256 / 9 * 5, prob=0.2)
-    ],
-    [
-        dict(type='Equalize', prob=0.4),
-        dict(type='Rotate', angle=30 / 9 * 8, prob=0.8)
-    ],
-    [
-        dict(type='Solarize', thr=256 / 9 * 6, prob=0.6),
-        dict(type='Equalize', prob=0.6)
-    ],
-    [dict(type='Posterize', bits=6, prob=0.8),
-     dict(type='Equalize', prob=1.)],
-    [
-        dict(type='Rotate', angle=10., prob=0.2),
-        dict(type='Solarize', thr=256 / 9, prob=0.6)
-    ],
-    [
-        dict(type='Equalize', prob=0.6),
-        dict(type='Posterize', bits=5, prob=0.6)
-    ],
-    [
-        dict(type='Rotate', angle=30 / 9 * 8, prob=0.8),
-        dict(type='ColorTransform', magnitude=0., prob=0.4)
-    ],
-    [
-        dict(type='Rotate', angle=30., prob=0.4),
-        dict(type='Equalize', prob=0.6)
-    ],
-    [dict(type='Equalize', prob=0.0),
-     dict(type='Equalize', prob=0.8)],
-    [dict(type='Invert', prob=0.6),
-     dict(type='Equalize', prob=1.)],
-    [
-        dict(type='ColorTransform', magnitude=0.4, prob=0.6),
-        dict(type='Contrast', magnitude=0.8, prob=1.)
-    ],
-    [
-        dict(type='Rotate', angle=30 / 9 * 8, prob=0.8),
-        dict(type='ColorTransform', magnitude=0.2, prob=1.)
-    ],
-    [
-        dict(type='ColorTransform', magnitude=0.8, prob=0.8),
-        dict(type='Solarize', thr=256 / 9 * 2, prob=0.8)
-    ],
-    [
-        dict(type='Sharpness', magnitude=0.7, prob=0.4),
-        dict(type='Invert', prob=0.6)
-    ],
-    [
-        dict(
-            type='Shear',
-            magnitude=0.3 / 9 * 5,
-            prob=0.6,
-            direction='horizontal'),
-        dict(type='Equalize', prob=1.)
-    ],
-    [
-        dict(type='ColorTransform', magnitude=0., prob=0.4),
-        dict(type='Equalize', prob=0.6)
-    ],
-    [
-        dict(type='Equalize', prob=0.4),
-        dict(type='Solarize', thr=256 / 9 * 5, prob=0.2)
-    ],
-    [
-        dict(type='Solarize', thr=256 / 9 * 4, prob=0.6),
-        dict(type='AutoContrast', prob=0.6)
-    ],
-    [dict(type='Invert', prob=0.6),
-     dict(type='Equalize', prob=1.)],
-    [
-        dict(type='ColorTransform', magnitude=0.4, prob=0.6),
-        dict(type='Contrast', magnitude=0.8, prob=1.)
-    ],
-    [dict(type='Equalize', prob=0.8),
-     dict(type='Equalize', prob=0.6)],
 ]
 
 dataset_type = 'ImageNet'
@@ -107,7 +13,7 @@ train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='RandomResizedCrop', size=224),
     dict(type='RandomFlip', flip_prob=0.5, direction='horizontal'),
-    dict(type='AutoAugment', policies=policies),
+    dict(type='AutoAugment', policies={{_base_.policy_imagenet}}),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='ImageToTensor', keys=['img']),
     dict(type='ToTensor', keys=['gt_label']),
