da93cac3d0d1938300f68287dd69187f68fa3ae2
mzr1996 <mzr1996@163.com>
2021-08-11 16:40:25 +0800

    Update parse_version_info function.

--- mmcls/__init__.py
+++ mmcls/__init__.py
@@ -1,27 +1,14 @@
 import mmcv
 
-from .version import __version__
-
-
-def digit_version(version_str):
-    digit_version = []
-    for x in version_str.split('.'):
-        if x.isdigit():
-            digit_version.append(int(x))
-        elif x.find('rc') != -1:
-            patch_version = x.split('rc')
-            digit_version.append(int(patch_version[0]) - 1)
-            digit_version.append(int(patch_version[1]))
-    return digit_version
-
+from .version import __version__, parse_version_info
 
 mmcv_minimum_version = '1.3.8'
 mmcv_maximum_version = '1.5.0'
-mmcv_version = digit_version(mmcv.__version__)
+mmcv_version = parse_version_info(mmcv.__version__)
 
 
-assert (mmcv_version >= digit_version(mmcv_minimum_version)
-        and mmcv_version <= digit_version(mmcv_maximum_version)), \
+assert (mmcv_version >= parse_version_info(mmcv_minimum_version)
+        and mmcv_version <= parse_version_info(mmcv_maximum_version)), \
     f'MMCV=={mmcv.__version__} is used but incompatible. ' \
     f'Please install mmcv>={mmcv_minimum_version}, <={mmcv_maximum_version}.'
 
