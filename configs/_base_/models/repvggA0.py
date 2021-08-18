e40cec1082791f18659f9f0bb73ae6b3180426e1
zhangrui-wolf <zhangrui_wolf@163.com>
2021-08-18 11:39:48 +0800

    Add the repvggA0 primitive config

---
+++ configs/_base_/models/repvggA0.py
@@ -0,0 +1,17 @@
+custom_imports = dict(imports=['RepVGG.repvgg'], allow_failed_imports=False)
+
+model = dict(
+    type='ImageClassifier',
+    backbone=dict(
+        type='RepVGG',
+        arch='A0',
+        out_indices=(3, ),
+    ),
+    neck=dict(type='GlobalAveragePooling'),
+    head=dict(
+        type='LinearClsHead',
+        num_classes=1000,
+        in_channels=1280,
+        loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+        topk=(1, 5),
+    ))
