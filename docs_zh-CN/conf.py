c524cc1ad1f00d772b565c0537307f63691cf736
mzr1996 <mzr1996@163.com>
2021-09-22 21:38:30 +0800

    Fix lint

--- docs_zh-CN/conf.py
+++ docs_zh-CN/conf.py
@@ -203,7 +203,8 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [(master_doc, 'mmcls', 'MMClassification Documentation', [author], 1)]
+man_pages = [(master_doc, 'mmcls', 'MMClassification Documentation', [author],
+              1)]
 
 # -- Options for Texinfo output ----------------------------------------------
 
