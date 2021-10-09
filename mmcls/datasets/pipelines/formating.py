53df6646b5c3563d2b2ae41c6155aa53e563e19e
mzr1996 <mzr1996@163.com>
2021-10-09 19:10:31 +0800

    Add formating.py to keep compatibility.

---
+++ mmcls/datasets/pipelines/formating.py
@@ -0,0 +1,9 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+# flake8: noqa
+import warnings
+
+from .formatting import *
+
+warnings.warn('DeprecationWarning: mmcls.datasets.pipelines.formating will be '
+              'deprecated in 2021, please replace it with '
+              'mmcls.datasets.pipelines.formatting.')
