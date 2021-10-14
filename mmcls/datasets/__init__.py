4012063c27f15ab8744f6945d513e4084bcaedf3
cgarriga <cgarriga@redpoints.com>
2021-10-14 12:05:50 +0200

    Add logos dataset and models

--- mmcls/datasets/__init__.py
+++ mmcls/datasets/__init__.py
@@ -2,10 +2,10 @@
 from .base_dataset import BaseDataset
 from .builder import DATASETS, PIPELINES, build_dataloader, build_dataset
 from .cifar import CIFAR10, CIFAR100
-from .detection_conversion import (OpenBrandDataset, XMLDataset)
 from .dataset_wrappers import (ClassBalancedDataset, ConcatDataset,
                                RepeatDataset)
 from .imagenet import ImageNet
+from .logos import LogoDataset
 from .mnist import MNIST, FashionMNIST
 from .multi_label import MultiLabelDataset
 from .samplers import DistributedSampler
@@ -15,6 +15,5 @@ __all__ = [
     'BaseDataset', 'ImageNet', 'CIFAR10', 'CIFAR100', 'MNIST', 'FashionMNIST',
     'VOC', 'MultiLabelDataset', 'build_dataloader', 'build_dataset', 'Compose',
     'DistributedSampler', 'ConcatDataset', 'RepeatDataset',
-    'ClassBalancedDataset', 'DATASETS', 'PIPELINES', 'OpenBrandDataset',
-    'XMLDataset'
+    'ClassBalancedDataset', 'DATASETS', 'PIPELINES', 'LogoDataset'
 ]
