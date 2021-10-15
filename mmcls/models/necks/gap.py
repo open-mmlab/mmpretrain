2c309f686c76ee59037b03cc5c14a0b956968f60
mzr1996 <mzr1996@163.com>
2021-10-15 17:40:57 +0800

    Revert GAP modification.

--- mmcls/models/necks/gap.py
+++ mmcls/models/necks/gap.py
@@ -1,13 +1,12 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import torch
 import torch.nn as nn
-from mmcv.runner.base_module import BaseModule
 
 from ..builder import NECKS
 
 
 @NECKS.register_module()
-class GlobalAveragePooling(BaseModule):
+class GlobalAveragePooling(nn.Module):
     """Global Average Pooling neck.
 
     Note that we use `view` to remove extra channel after pooling. We do not
@@ -19,8 +18,8 @@ class GlobalAveragePooling(BaseModule):
             Default: 2
     """
 
-    def __init__(self, dim=2, init_cfg=None):
-        super(GlobalAveragePooling, self).__init__(init_cfg=init_cfg)
+    def __init__(self, dim=2):
+        super(GlobalAveragePooling, self).__init__()
         assert dim in [1, 2, 3], 'GlobalAveragePooling dim only support ' \
             f'{1, 2, 3}, get {dim} instead.'
         if dim == 1:
