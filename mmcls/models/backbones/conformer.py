0e96d635f72772d99c46b726cda714b9f6713242
pengzhiliang <1083127130@qq.com>
2021-10-27 00:44:01 +0800

    reuse the TransformerEncoderLayer in the vision_transformer.py

--- mmcls/models/backbones/conformer.py
+++ mmcls/models/backbones/conformer.py
@@ -10,105 +10,7 @@ from mmcv.runner import load_checkpoint
 from mmcls.utils import get_root_logger
 from ..builder import BACKBONES
 from .base_backbone import BaseBackbone
-
-
-class Mlp(nn.Module):
-
-    def __init__(self,
-                 in_features,
-                 hidden_features=None,
-                 out_features=None,
-                 act_layer=nn.GELU,
-                 drop=0.):
-        super().__init__()
-        out_features = out_features or in_features
-        hidden_features = hidden_features or in_features
-        self.fc1 = nn.Linear(in_features, hidden_features)
-        self.act = act_layer()
-        self.fc2 = nn.Linear(hidden_features, out_features)
-        self.drop = nn.Dropout(drop)
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.act(x)
-        x = self.drop(x)
-        x = self.fc2(x)
-        x = self.drop(x)
-        return x
-
-
-class Attention(nn.Module):
-
-    def __init__(self,
-                 dim,
-                 num_heads=8,
-                 qkv_bias=False,
-                 qk_scale=None,
-                 attn_drop=0.,
-                 proj_drop=0.):
-        super().__init__()
-        self.num_heads = num_heads
-        head_dim = dim // num_heads
-        self.scale = qk_scale or head_dim**-0.5
-
-        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
-        self.attn_drop = nn.Dropout(attn_drop)
-        self.proj = nn.Linear(dim, dim)
-        self.proj_drop = nn.Dropout(proj_drop)
-
-    def forward(self, x):
-        B, N, C = x.shape
-        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads,
-                                  C // self.num_heads).permute(2, 0, 3, 1, 4)
-        q, k, v = qkv[0], qkv[1], qkv[
-            2]  # make torchscript happy (cannot use tensor as tuple)
-
-        attn = (q @ k.transpose(-2, -1)) * self.scale
-        attn = attn.softmax(dim=-1)
-        attn = self.attn_drop(attn)
-
-        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
-        x = self.proj(x)
-        x = self.proj_drop(x)
-        return x
-
-
-class Block(nn.Module):
-
-    def __init__(self,
-                 dim,
-                 num_heads,
-                 mlp_ratio=4.,
-                 qkv_bias=False,
-                 qk_scale=None,
-                 drop=0.,
-                 attn_drop=0.,
-                 drop_path=0.,
-                 act_layer=nn.GELU,
-                 norm_layer=partial(nn.LayerNorm, eps=1e-6)):
-        super().__init__()
-        self.norm1 = norm_layer(dim)
-        self.attn = Attention(
-            dim,
-            num_heads=num_heads,
-            qkv_bias=qkv_bias,
-            qk_scale=qk_scale,
-            attn_drop=attn_drop,
-            proj_drop=drop)
-        self.drop_path = DropPath(
-            drop_path) if drop_path > 0. else nn.Identity()
-        self.norm2 = norm_layer(dim)
-        mlp_hidden_dim = int(dim * mlp_ratio)
-        self.mlp = Mlp(
-            in_features=dim,
-            hidden_features=mlp_hidden_dim,
-            act_layer=act_layer,
-            drop=drop)
-
-    def forward(self, x):
-        x = x + self.drop_path(self.attn(self.norm1(x)))
-        x = x + self.drop_path(self.mlp(self.norm2(x)))
-        return x
+from .vision_transformer import TransformerEncoderLayer
 
 
 class ConvBlock(nn.Module):
@@ -122,7 +24,7 @@ class ConvBlock(nn.Module):
                  groups=1,
                  norm_layer=partial(nn.BatchNorm2d, eps=1e-6),
                  drop_block=None,
-                 drop_path=None):
+                 drop_path=0.):
         super(ConvBlock, self).__init__()
 
         expansion = 4
@@ -171,7 +73,8 @@ class ConvBlock(nn.Module):
 
         self.res_conv = res_conv
         self.drop_block = drop_block
-        self.drop_path = drop_path
+        self.drop_path = DropPath(
+            drop_path) if drop_path > 0. else nn.Identity()
 
     def zero_init_last_bn(self):
         nn.init.zeros_(self.bn3.weight)
@@ -295,7 +198,6 @@ class ConvTransBlock(nn.Module):
                  num_heads=12,
                  mlp_ratio=4.,
                  qkv_bias=False,
-                 qk_scale=None,
                  cls_token=True,
                  drop_rate=0.,
                  attn_drop_rate=0.,
@@ -319,10 +221,14 @@ class ConvTransBlock(nn.Module):
                 outplanes=outplanes,
                 stride=2,
                 res_conv=True,
-                groups=groups)
+                groups=groups,
+                drop_path=drop_path_rate)
         else:
             self.fusion_block = ConvBlock(
-                inplanes=outplanes, outplanes=outplanes, groups=groups)
+                inplanes=outplanes,
+                outplanes=outplanes,
+                groups=groups,
+                drop_path=drop_path_rate)
 
         if num_med_block > 0:
             raise NotImplementedError
@@ -339,15 +245,15 @@ class ConvTransBlock(nn.Module):
             up_stride=dw_stride,
             cls_token=cls_token)
 
-        self.trans_block = Block(
-            dim=embed_dim,
+        self.trans_block = TransformerEncoderLayer(
+            embed_dims=embed_dim,
             num_heads=num_heads,
-            mlp_ratio=mlp_ratio,
+            feedforward_channels=int(embed_dim * mlp_ratio),
+            drop_rate=drop_rate,
+            drop_path_rate=drop_path_rate,
+            attn_drop_rate=attn_drop_rate,
             qkv_bias=qkv_bias,
-            qk_scale=qk_scale,
-            drop=drop_rate,
-            attn_drop=attn_drop_rate,
-            drop_path=drop_path_rate)
+            norm_cfg=dict(type='LN', eps=1e-6))
 
         self.dw_stride = dw_stride
         self.embed_dim = embed_dim
@@ -406,7 +312,6 @@ class Conformer(BaseBackbone):
                  base_channel=64,
                  mlp_ratio=4.,
                  qkv_bias=True,
-                 qk_scale=None,
                  cls_token=True,
                  drop_path_rate=0.,
                  norm_eval=True,
@@ -470,14 +375,14 @@ class Conformer(BaseBackbone):
             kernel_size=trans_dw_stride,
             stride=trans_dw_stride,
             padding=0)
-        self.trans_1 = Block(
-            dim=self.embed_dims,
+
+        self.trans_1 = TransformerEncoderLayer(
+            embed_dims=self.embed_dims,
             num_heads=self.num_heads,
-            mlp_ratio=mlp_ratio,
+            feedforward_channels=int(self.embed_dims * mlp_ratio),
+            drop_path_rate=self.trans_dpr[0],
             qkv_bias=qkv_bias,
-            qk_scale=qk_scale,
-            drop_path=self.trans_dpr[0],
-        )
+            norm_cfg=dict(type='LN', eps=1e-6))
 
         # 2~4 stage
         init_stage = 2
@@ -495,7 +400,6 @@ class Conformer(BaseBackbone):
                     num_heads=self.num_heads,
                     mlp_ratio=mlp_ratio,
                     qkv_bias=qkv_bias,
-                    qk_scale=qk_scale,
                     drop_path_rate=self.trans_dpr[i - 1],
                     cls_token=self.cls_token_flag))
 
@@ -520,7 +424,6 @@ class Conformer(BaseBackbone):
                     num_heads=self.num_heads,
                     mlp_ratio=mlp_ratio,
                     qkv_bias=qkv_bias,
-                    qk_scale=qk_scale,
                     drop_path_rate=self.trans_dpr[i - 1],
                     cls_token=self.cls_token_flag))
 
@@ -546,7 +449,6 @@ class Conformer(BaseBackbone):
                     num_heads=self.num_heads,
                     mlp_ratio=mlp_ratio,
                     qkv_bias=qkv_bias,
-                    qk_scale=qk_scale,
                     drop_path_rate=self.trans_dpr[i - 1],
                     cls_token=self.cls_token_flag,
                     last_fusion=last_fusion))
