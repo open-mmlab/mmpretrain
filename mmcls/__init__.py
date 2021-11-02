546277f81537f768a8eaa9e7250b1da3e7533b30
mzr1996 <mzr1996@163.com>
2021-11-02 16:56:49 +0800

    Update mmcv dependency version

--- mmcls/__init__.py
+++ mmcls/__init__.py
@@ -47,7 +47,7 @@ def digit_version(version_str: str, length: int = 4):
     return tuple(release)
 
 
-mmcv_minimum_version = '1.3.8'
+mmcv_minimum_version = '1.3.16'
 mmcv_maximum_version = '1.5.0'
 mmcv_version = digit_version(mmcv.__version__)
 
