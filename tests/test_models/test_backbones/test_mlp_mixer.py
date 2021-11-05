38709b93e5d9a88ebe165883de3a802241802038
0x4f5da2 <chen1319276472@foxmail.com>
2021-11-05 20:45:05 +0800

    add unit test for mlp mixer

---
+++ tests/test_models/test_backbones/test_mlp_mixer.py
@@ -0,0 +1,28 @@
+import torch
+from torch.nn.modules.batchnorm import _BatchNorm
+
+from mmcls.models.backbones import MlpMixer
+
+
+def check_norm_state(modules, train_state):
+    """Check if norm layer is in correct train state."""
+    for mod in modules:
+        if isinstance(mod, _BatchNorm):
+            if mod.training != train_state:
+                return False
+    return True
+
+
+def test_mlp_mixer_backbone():
+    cfg_ori = dict(
+        patch_size=16,
+        num_blocks=12,
+        embed_dims=768,
+    )
+    model = MlpMixer(**cfg_ori)
+    model.train()
+    assert check_norm_state(model.modules(), True)
+    imgs = torch.randn(3, 3, 224, 224)
+    out = model(imgs)
+    assert isinstance(out, tuple) and len(out) == 1
+    assert out[0].size(0) == 3 and out[0].size(1) == 768
