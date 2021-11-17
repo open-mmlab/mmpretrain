820ce42316a1408cd3192c92d25d533d4150bc38
mzr1996 <mzr1996@163.com>
2021-11-17 16:43:01 +0800

    Fix some files

--- configs/mobilenet_v3/mobilenet_v3_small_cifar.py
+++ configs/mobilenet_v3/mobilenet_v3_small_cifar.py
@@ -1,6 +1,6 @@
-_base_ = 'mobilenet-v3-small_8xb16_cifar.py'
+_base_ = 'mobilenet-v3-small_8xb16_cifar10.py'
 
 _deprecation_ = dict(
-    expected='mobilenet-v3-small_8xb16_cifar.py',
+    expected='mobilenet-v3-small_8xb16_cifar10.py',
     reference='https://github.com/open-mmlab/mmclassification/pull/508',
 )
