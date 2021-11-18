52eee339f4d0b79026653b2d92b9f7a6a5b149f5
mzr1996 <mzr1996@163.com>
2021-11-18 10:41:15 +0800

    Remove fp16 metafile and README.

--- configs/fp16/resnet50_b32x8_fp16_dynamic_imagenet.py
+++ configs/fp16/resnet50_b32x8_fp16_dynamic_imagenet.py
@@ -1,6 +1,6 @@
-_base_ = 'resnet50_8xb32-fp16-dynamic_in1k.py'
+_base_ = '../resnet/resnet50_8xb32-fp16-dynamic_in1k.py'
 
 _deprecation_ = dict(
-    expected='resnet50_8xb32-fp16-dynamic_in1k.py',
+    expected='../resnet/resnet50_8xb32-fp16-dynamic_in1k.py',
     reference='https://github.com/open-mmlab/mmclassification/pull/508',
 )
--- configs/resnet/resnet50_8xb32-fp16-dynamic_in1k.py
+++ configs/resnet/resnet50_8xb32-fp16-dynamic_in1k.py
@@ -1,4 +1,4 @@
-_base_ = ['../resnet/resnet50_8xb32_in1k.py']
+_base_ = ['./resnet50_8xb32_in1k.py']
 
 # fp16 settings
 fp16 = dict(loss_scale='dynamic')
