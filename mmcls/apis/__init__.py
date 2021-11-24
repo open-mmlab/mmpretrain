9ee0e42fb663dc6b87f1fd2f8759ee39f14752a8
mzr1996 <mzr1996@163.com>
2021-11-24 17:09:09 +0800

    [Enchance] Set a random seed when the user does not set a seed.

--- mmcls/apis/__init__.py
+++ mmcls/apis/__init__.py
@@ -1,9 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .inference import inference_model, init_model, show_result_pyplot
 from .test import multi_gpu_test, single_gpu_test
-from .train import set_random_seed, train_model
+from .train import init_random_seed, set_random_seed, train_model
 
 __all__ = [
     'set_random_seed', 'train_model', 'init_model', 'inference_model',
-    'multi_gpu_test', 'single_gpu_test', 'show_result_pyplot'
+    'multi_gpu_test', 'single_gpu_test', 'show_result_pyplot',
+    'init_random_seed'
 ]
