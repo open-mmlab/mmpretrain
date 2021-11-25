95394adc1cf99fb9262c372700c1d5b696181523
mzr1996 <mzr1996@163.com>
2021-11-25 16:58:57 +0800

    Update ViT training setting

--- configs/_base_/datasets/imagenet_bs64_pil_resize_autoaug.py
+++ configs/_base_/datasets/imagenet_bs64_pil_resize_autoaug.py
@@ -8,7 +8,11 @@ img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='RandomResizedCrop', size=224, backend='pillow'),
+    dict(
+        type='RandomResizedCrop',
+        size=224,
+        backend='pillow',
+        interpolation='bicubic'),
     dict(type='RandomFlip', flip_prob=0.5, direction='horizontal'),
     dict(type='AutoAugment', policies={{_base_.policy_imagenet}}),
     dict(type='Normalize', **img_norm_cfg),
@@ -18,7 +22,11 @@ train_pipeline = [
 ]
 test_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='Resize', size=(256, -1), backend='pillow'),
+    dict(
+        type='Resize',
+        size=(256, -1),
+        backend='pillow',
+        interpolation='bicubic'),
     dict(type='CenterCrop', crop_size=224),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='ImageToTensor', keys=['img']),
