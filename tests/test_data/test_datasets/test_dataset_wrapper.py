3d2b1fb415ab940035ce56ef24c4b0a486782433
Ezra-Yu <1105212286@qq.com>
2021-11-25 14:13:40 +0800

    update unit tests

--- tests/test_data/test_datasets/test_dataset_wrapper.py
+++ tests/test_data/test_datasets/test_dataset_wrapper.py
@@ -49,8 +49,8 @@ def test_concat_dataset(construct_dataset):
     concat_dataset = ConcatDataset([dataset_a, dataset_b])
     assert concat_dataset[5] == 5
     assert concat_dataset[25] == 15
-    assert np.array_equal(concat_dataset.get_cat_ids(5), cat_ids_list_a[5])
-    assert np.array_equal(concat_dataset.get_cat_ids(25), cat_ids_list_b[15])
+    assert concat_dataset.get_cat_ids(5) == cat_ids_list_a[5]
+    assert concat_dataset.get_cat_ids(25) == cat_ids_list_b[15]
     assert len(concat_dataset) == len(dataset_a) + len(dataset_b)
     assert concat_dataset.CLASSES == BaseDataset.CLASSES
 
@@ -65,9 +65,9 @@ def test_repeat_dataset(construct_dataset):
     assert repeat_dataset[5] == 5
     assert repeat_dataset[15] == 5
     assert repeat_dataset[27] == 7
-    assert np.array_equal(repeat_dataset.get_cat_ids(5), cat_ids_list[5])
-    assert np.array_equal(repeat_dataset.get_cat_ids(15), cat_ids_list[5])
-    assert np.array_equal(repeat_dataset.get_cat_ids(27), cat_ids_list[7])
+    assert repeat_dataset.get_cat_ids(5) == cat_ids_list[5]
+    assert repeat_dataset.get_cat_ids(15) == cat_ids_list[5]
+    assert repeat_dataset.get_cat_ids(27) == cat_ids_list[7]
     assert len(repeat_dataset) == 10 * len(dataset)
     assert repeat_dataset.CLASSES == BaseDataset.CLASSES
 
