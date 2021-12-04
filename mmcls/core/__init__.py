62f9ece87175a4319a4db97933e919da626061dc
0x4f5da2 <chen1319276472@foxmail.com>
2021-12-04 19:38:51 +0800

    update lr

--- mmcls/core/__init__.py
+++ mmcls/core/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .evaluation import *  # noqa: F401, F403
 from .fp16 import *  # noqa: F401, F403
+from .hooks import *  # noqa: F401, F403
 from .optim import *  # noqa: F401, F403
 from .sampler import *  # noqa: F401, F403
 from .utils import *  # noqa: F401, F403
---
+++ mmcls/core/hooks/__init__.py
@@ -0,0 +1,3 @@
+from .lr_updater import CosineAnnealingCooldownLrUpdaterHook
+
+__all__ = ['CosineAnnealingCooldownLrUpdaterHook']
