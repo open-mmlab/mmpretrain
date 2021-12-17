643fb192cdad25a2faf0237059ef5bf97c65b5c8
Ma Zerun <mzr1996@163.com>
2021-12-17 15:55:02 +0800

    [Enhance] Enhance feature extraction function. (#593)
    * Fix MobileNet V3 configs
    
    * Refactor to support more powerful feature extraction.
    
    * Add unit tests
    
    * Fix unit test
    
    * Imporve according to comments
    
    * Update checkpoints path
    
    * Fix unit tests
    
    * Add docstring of `simple_test`
    
    * Add docstring of `extract_feat`
    
    * Update model zoo

--- configs/_base_/models/mobilenet_v3_small_imagenet.py
+++ configs/_base_/models/mobilenet_v3_small_imagenet.py
@@ -11,4 +11,6 @@ model = dict(
         dropout_rate=0.2,
         act_cfg=dict(type='HSwish'),
         loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+        init_cfg=dict(
+            type='Normal', layer='Linear', mean=0., std=0.01, bias=0.),
         topk=(1, 5)))
--- mmcls/models/heads/multi_label_linear_head.py
+++ mmcls/models/heads/multi_label_linear_head.py
@@ -1,6 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 from ..builder import HEADS
 from .multi_label_head import MultiLabelClsHead
@@ -39,21 +39,47 @@ class MultiLabelLinearClsHead(MultiLabelClsHead):
 
         self.fc = nn.Linear(self.in_channels, self.num_classes)
 
-    def forward_train(self, x, gt_label, **kwargs):
+    def pre_logits(self, x):
         if isinstance(x, tuple):
             x = x[-1]
+        return x
+
+    def forward_train(self, x, gt_label, **kwargs):
+        x = self.pre_logits(x)
         gt_label = gt_label.type_as(x)
         cls_score = self.fc(x)
         losses = self.loss(cls_score, gt_label, **kwargs)
         return losses
 
-    def simple_test(self, x):
-        """Test without augmentation."""
-        if isinstance(x, tuple):
-            x = x[-1]
-        cls_score = self.fc(x)
-        if isinstance(cls_score, list):
-            cls_score = sum(cls_score) / float(len(cls_score))
-        pred = F.sigmoid(cls_score) if cls_score is not None else None
+    def simple_test(self, x, sigmoid=True, post_process=True):
+        """Inference without augmentation.
 
-        return self.post_process(pred)
+        Args:
+            x (tuple[Tensor]): The input features.
+                Multi-stage inputs are acceptable but only the last stage will
+                be used to classify. The shape of every item should be
+                ``(num_samples, in_channels)``.
+            sigmoid (bool): Whether to sigmoid the classification score.
+            post_process (bool): Whether to do post processing the
+                inference results. It will convert the output to a list.
+
+        Returns:
+            Tensor | list: The inference results.
+
+                - If no post processing, the output is a tensor with shape
+                  ``(num_samples, num_classes)``.
+                - If post processing, the output is a multi-dimentional list of
+                  float and the dimensions are ``(num_samples, num_classes)``.
+        """
+        x = self.pre_logits(x)
+        cls_score = self.fc(x)
+
+        if sigmoid:
+            pred = torch.sigmoid(cls_score) if cls_score is not None else None
+        else:
+            pred = cls_score
+
+        if post_process:
+            return self.post_process(pred)
+        else:
+            return pred
