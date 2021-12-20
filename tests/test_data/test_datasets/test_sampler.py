e2dae0516f7625a52348fb3e62acb9aca5c78def
0x4f5da2 <chen1319276472@foxmail.com>
2021-12-20 21:04:42 +0800

    update unit test

--- tests/test_data/test_datasets/test_sampler.py
+++ tests/test_data/test_datasets/test_sampler.py
@@ -6,7 +6,7 @@ import numpy as np
 import pytest
 
 from mmcls.apis.train import set_default_sampler_cfg
-from mmcls.datasets import BaseDataset, RepeatAugSampler
+from mmcls.datasets import BaseDataset, RepeatAugSampler, build_sampler
 
 
 @patch.multiple(BaseDataset, __abstractmethods__=set())
@@ -19,6 +19,16 @@ def construct_toy_single_label_dataset(length):
     dataset.data_infos.__len__.return_value = length
     dataset.get_cat_ids = MagicMock(side_effect=lambda idx: cat_ids_list[idx])
     return dataset, cat_ids_list
+
+
+@patch('mmcls.datasets.samplers.repeat_aug.dist')
+def test_sampler_builder(mock_torch_dist):
+    assert build_sampler(None) is None
+    mock_torch_dist.is_available.side_effect = lambda: True
+    mock_torch_dist.get_world_size.side_effect = lambda: 1
+    mock_torch_dist.get_rank.side_effect = lambda: 0
+    dataset = construct_toy_single_label_dataset(1000)[0]
+    build_sampler(dict(type='RepeatAugSampler', dataset=dataset))
 
 
 def test_default_cfg_setter():
@@ -39,10 +49,12 @@ def test_default_cfg_setter():
         set_default_sampler_cfg(cfg, distributed=False)
 
 
-def test_rep_aug_fail():
+@patch('mmcls.datasets.samplers.repeat_aug.dist')
+def test_rep_aug_fail(mock_torch_dist):
+    mock_torch_dist.is_available.side_effect = lambda: False
     dataset = construct_toy_single_label_dataset(1000)[0]
     with pytest.raises(Exception):
-        RepeatAugSampler(dataset)
+        build_sampler(dict(type='RepeatAugSampler', dataset=dataset))
     with pytest.raises(Exception):
         RepeatAugSampler(dataset, num_replicas=1)
 
@@ -53,7 +65,8 @@ def test_rep_aug(mock_torch_dist):
     mock_torch_dist.is_available.side_effect = lambda: True
     mock_torch_dist.get_world_size.side_effect = lambda: 1
     mock_torch_dist.get_rank.side_effect = lambda: 0
-    ra = RepeatAugSampler(dataset, selected_round=1)
+    ra = RepeatAugSampler(dataset, selected_round=0, shuffle=False)
+    ra.set_epoch(0)
     assert len(ra) == 1000
     ra = RepeatAugSampler(dataset)
     assert len(ra) == 768
