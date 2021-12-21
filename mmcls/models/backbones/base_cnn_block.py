761d956aae7bbae749e102235909a794c96bb028
Ezra-Yu <1105212286@qq.com>
2021-12-21 21:42:46 +0800

    fix lintt

--- mmcls/models/backbones/base_cnn_block.py
+++ mmcls/models/backbones/base_cnn_block.py
@@ -1,11 +1,12 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from typing import List, Dict, Any, Optional
-
-from mmcv.cnn import build_activation_layer, build_plugin_layer
 from abc import ABCMeta, abstractmethod
+from typing import Any, Dict, List, Optional
+
+from mmcv.cnn import build_plugin_layer
 from mmcv.runner import BaseModule
 
-PLUGIN_DICT=Dict[str, Optional[Any]]
+PLUGIN_DICT = Dict[str, Optional[Any]]
+
 
 class PluginsBaseCNNBlock(BaseModule, metaclass=ABCMeta):
     """Baseblock for CNN Neural Networks backbone with Plugins;
@@ -18,8 +19,8 @@ class PluginsBaseCNNBlock(BaseModule, metaclass=ABCMeta):
     """
 
     def __init__(self,
-                 plugins_allowed_position:List[str]=[],
-                 plugins:Optional[List[PLUGIN_DICT]]=None,
+                 plugins_allowed_position: List[str] = [],
+                 plugins: Optional[List[PLUGIN_DICT]] = None,
                  init_cfg=None):
         super(PluginsBaseCNNBlock, self).__init__(init_cfg)
 
@@ -29,14 +30,16 @@ class PluginsBaseCNNBlock(BaseModule, metaclass=ABCMeta):
 
         assert plugins is None or isinstance(plugins, list)
         if self.with_plugins:
-            assert all(p['position'] in self.allowed_position for p in plugins), \
-                f"'position' must be in {','.join(self.allowed_position)}."
+            assert all(
+                p['position'] in self.allowed_position for p in plugins
+            ), f"'position' must be in {','.join(self.allowed_position)}."
 
-        self.plugin_dict = {p:list() for p in self.allowed_position}
+        self.plugin_dict = {p: list() for p in self.allowed_position}
         self.position2inchannels_map = dict()
 
     def make_block_plugins(self, plugins):
         """make plugins for block.
+
         Args:
             plugins (list[dict]): List of plugins cfg to build.
         Returns:
@@ -59,15 +62,15 @@ class PluginsBaseCNNBlock(BaseModule, metaclass=ABCMeta):
 
     @abstractmethod
     def get_position2inchannels_map(self):
-        """get the input channels in different place"""
+        """get the input channels in different place."""
         raise NotImplementedError
-
 
     def build_plugins(self):
         if not self.with_plugins:
-            return 
+            return
         self.position2inchannels_map = self.get_position2inchannels_map()
-        assert set(self.position2inchannels_map.keys()) == set(self.allowed_position)
+        assert set(self.position2inchannels_map.keys()) == set(
+            self.allowed_position)
 
         for position_name in self.allowed_position:
             position_plugin_cfgs = [
@@ -77,7 +80,7 @@ class PluginsBaseCNNBlock(BaseModule, metaclass=ABCMeta):
 
             plugin_layer_names = self.make_block_plugins(position_plugin_cfgs)
             self.plugin_dict[position_name] = plugin_layer_names
-        
+
     def forward_plugin(self, x, position):
         """Forward function for plugins."""
         out = x
@@ -96,13 +99,10 @@ class BaseBottleNeck(PluginsBaseCNNBlock):
         init_cfg (dict or list[dict], optional): Initialization config dict.
             Default: None
     """
-    def __init__(self,
-            in_channels,
-            out_channels,
-            plugins=None,
-            init_cfg=None):
+
+    def __init__(self, in_channels, out_channels, plugins=None, init_cfg=None):
         super(BaseBottleNeck, self).__init__(
-            plugins_allowed_position=[f"after_conv{i}" for i in range(1, 4)], 
+            plugins_allowed_position=[f'after_conv{i}' for i in range(1, 4)],
             plugins=plugins,
             init_cfg=init_cfg)
 
@@ -114,11 +114,11 @@ class BaseBottleNeck(PluginsBaseCNNBlock):
         raise NotImplementedError
 
     def get_position2inchannels_map(self):
-        """get the input channels in different place"""
+        """get the input channels in different place."""
         return dict(
             after_conv1=self.mid_channels,
             after_conv2=self.mid_channels,
-            after_conv3=self.out_channels)  
+            after_conv3=self.out_channels)
 
 
 class BaseBasicBlock(PluginsBaseCNNBlock):
@@ -130,14 +130,11 @@ class BaseBasicBlock(PluginsBaseCNNBlock):
         init_cfg (dict or list[dict], optional): Initialization config dict.
             Default: None
     """
-    def __init__(self,
-                in_channels,
-                out_channels,
-                plugins=None,
-                init_cfg=None):
+
+    def __init__(self, in_channels, out_channels, plugins=None, init_cfg=None):
         super(BaseBasicBlock, self).__init__(
-            plugins_allowed_position=["after_conv1", "after_conv2"], 
-            plugins=plugins, 
+            plugins_allowed_position=['after_conv1', 'after_conv2'],
+            plugins=plugins,
             init_cfg=init_cfg)
 
         self.in_channels = in_channels
@@ -148,10 +145,9 @@ class BaseBasicBlock(PluginsBaseCNNBlock):
         raise NotImplementedError
 
     def get_position2inchannels_map(self):
-        """get the input channels in different place"""
+        """get the input channels in different place."""
         return dict(
-            after_conv1=self.mid_channels,
-            after_conv2=self.out_channels)
+            after_conv1=self.mid_channels, after_conv2=self.out_channels)
 
 
 class BaseVGGBlock(PluginsBaseCNNBlock):
@@ -163,18 +159,15 @@ class BaseVGGBlock(PluginsBaseCNNBlock):
         init_cfg (dict or list[dict], optional): Initialization config dict.
             Default: None
     """
-    def __init__(self,
-                in_channels,
-                out_channels,
-                plugins=None,
-                init_cfg=None):
+
+    def __init__(self, in_channels, out_channels, plugins=None, init_cfg=None):
         super(BaseVGGBlock, self).__init__(
-            plugins_allowed_position=["after_conv1"],
-            plugins=plugins, 
+            plugins_allowed_position=['after_conv1'],
+            plugins=plugins,
             init_cfg=init_cfg)
         self.in_channels = in_channels
         self.out_channels = out_channels
 
     def get_position2inchannels_map(self):
-        """get the input channels in different place"""
+        """get the input channels in different place."""
         return dict(after_conv1=self.out_channels)
