125befd4c4b73e88ff4334e15fc3434f4ba5e69b
Ezra-Yu <1105212286@qq.com>
2021-12-24 15:46:05 +0800

    Update resnet50_8xb32-coslr-preciseBN_in1k.py

--- configs/resnet/resnet50_8xb32-coslr-preciseBN_in1k.py
+++ configs/resnet/resnet50_8xb32-coslr-preciseBN_in1k.py
@@ -1,7 +1,7 @@
 _base_ = 'resnet50_8xb32-coslr_in1k.py'
 
 # Precise BN hook will update the bn stats, so this hook should be executed
-# before CheckpointHook, which has priority of 'ABOVE_NORMAL'. So set the
+# before CheckpointHook, which has priority of 'NORMAL'. So set the
 # priority of PreciseBNHook to 'ABOVE_NORMAL' here.
 custom_hooks = [
     dict(
