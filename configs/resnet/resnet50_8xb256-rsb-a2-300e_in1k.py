cdf569a805fff6ba6aebab980a6769f84006c55c
mzr1996 <mzr1996@163.com>
2021-12-31 11:12:21 +0800

    Fix ResNet rsb config

--- configs/resnet/resnet50_8xb256-rsb-a2-300e_in1k.py
+++ configs/resnet/resnet50_8xb256-rsb-a2-300e_in1k.py
@@ -11,12 +11,7 @@ model = dict(
         norm_cfg=dict(type='SyncBN', requires_grad=True),
         drop_path_rate=0.05,
     ),
-    head=dict(
-        loss=dict(
-            type='LabelSmoothLoss',
-            label_smooth_val=0.1,
-            mode='original',
-        )),
+    head=dict(loss=dict(use_sigmoid=True)),
     train_cfg=dict(augments=[
         dict(type='BatchMixup', alpha=0.1, num_classes=1000, prob=0.5),
         dict(type='BatchCutMix', alpha=1.0, num_classes=1000, prob=0.5)
--- configs/resnet/resnet50_8xb256-rsb-a3-100e_in1k.py
+++ configs/resnet/resnet50_8xb256-rsb-a3-100e_in1k.py
@@ -8,12 +8,7 @@ _base_ = [
 # Model settings
 model = dict(
     backbone=dict(norm_cfg=dict(type='SyncBN', requires_grad=True)),
-    head=dict(
-        loss=dict(
-            type='LabelSmoothLoss',
-            label_smooth_val=0.1,
-            mode='original',
-        )),
+    head=dict(loss=dict(use_sigmoid=True)),
     train_cfg=dict(augments=[
         dict(type='BatchMixup', alpha=0.1, num_classes=1000, prob=0.5),
         dict(type='BatchCutMix', alpha=1.0, num_classes=1000, prob=0.5)
