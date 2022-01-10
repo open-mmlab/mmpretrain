c9a994672df0218c8ac2e2b5702a6fb49786dfce
mzr1996 <mzr1996@163.com>
2022-01-10 14:54:04 +0800

    Add HRNet configs

---
+++ configs/hrnet/hrnet-w18.py
@@ -0,0 +1,6 @@
+_base_ = [
+    '../_base_/models/hrnet/hrnet-w18.py',
+    '../_base_/datasets/imagenet_bs64_baseline.py',
+    '../_base_/schedules/imagenet_bs512_baseline.py',
+    '../_base_/default_runtime.py'
+]
