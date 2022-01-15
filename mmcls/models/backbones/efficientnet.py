a09b549b636465fc5aabd986295ac6a14ddbb906
0x4f5da2 <chen1319276472@foxmail.com>
2022-01-15 22:08:45 +0800

    reorganize code

--- mmcls/models/backbones/efficientnet.py
+++ mmcls/models/backbones/efficientnet.py
@@ -6,12 +6,105 @@ import torch.nn as nn
 import torch.utils.checkpoint as cp
 from mmcv.cnn import ConvModule, constant_init, kaiming_init
 from mmcv.cnn.bricks.drop import drop_path
-from mmcv.runner import load_checkpoint
+from mmcv.runner import Sequential, load_checkpoint
 from torch.nn.modules.batchnorm import _BatchNorm
 
 from mmcls.models.backbones.base_backbone import BaseBackbone
-from mmcls.models.utils import InvertedResidual, make_divisible
+from mmcls.models.utils import InvertedResidual, SELayer, make_divisible
 from ..builder import BACKBONES
+
+
+class EdgeResidual(nn.Module):
+    """Edge Residual Block
+    Args:
+        in_channels (int): The input channels of this Module.
+        out_channels (int): The output channels of this Module.
+        mid_channels (int): The input channels of the depthwise convolution.
+        kernel_size (int): The kernel size of the depthwise convolution.
+            Default: 3.
+        stride (int): The stride of the depthwise convolution. Default: 1.
+        se_cfg (dict): Config dict for se layer. Default: None, which means no
+            se layer.
+        with_residual (bool): Use residual connection. Default: True.
+        conv_cfg (dict): Config dict for convolution layer. Default: None,
+            which means using conv2d.
+        norm_cfg (dict): Config dict for normalization layer.
+            Default: dict(type='BN').
+        act_cfg (dict): Config dict for activation layer.
+            Default: dict(type='ReLU').
+        with_cp (bool): Use checkpoint or not. Using checkpoint will save some
+            memory while slowing down the training speed. Default: False.
+    Returns:
+        Tensor: The output tensor.
+    """
+
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 mid_channels,
+                 kernel_size=3,
+                 stride=1,
+                 se_cfg=None,
+                 with_residual=True,
+                 conv_cfg=None,
+                 norm_cfg=dict(type='BN'),
+                 act_cfg=dict(type='ReLU'),
+                 with_cp=False):
+        super(EdgeResidual, self).__init__()
+        assert stride in [1, 2]
+        self.with_cp = with_cp
+        self.with_se = se_cfg is not None
+        self.with_residual = (
+            stride == 1 and in_channels == out_channels and with_residual)
+
+        if self.with_se:
+            assert isinstance(se_cfg, dict)
+
+        self.conv1 = ConvModule(
+            in_channels=in_channels,
+            out_channels=mid_channels,
+            kernel_size=kernel_size,
+            stride=1,
+            padding=kernel_size // 2,
+            conv_cfg=conv_cfg,
+            norm_cfg=norm_cfg,
+            act_cfg=act_cfg)
+
+        if self.with_se:
+            self.se = SELayer(**se_cfg)
+
+        self.conv2 = ConvModule(
+            in_channels=mid_channels,
+            out_channels=out_channels,
+            kernel_size=1,
+            stride=stride,
+            padding=0,
+            conv_cfg=conv_cfg,
+            norm_cfg=norm_cfg,
+            act_cfg=None)
+
+    def forward(self, x):
+
+        def _inner_forward(x):
+            out = x
+            out = self.conv1(out)
+
+            if self.with_se:
+                out = self.se(out)
+
+            out = self.conv2(out)
+
+            if self.with_residual:
+                return x + out
+            else:
+                return out
+
+        if self.with_cp and x.requires_grad:
+            out = cp.checkpoint(_inner_forward, x)
+        else:
+            out = _inner_forward(x)
+
+        return out
 
 
 class MBConv(InvertedResidual):
@@ -300,8 +393,7 @@ class EfficientNet(BaseBackbone):
                         act_cfg=act_cfg,
                         with_cp=self.with_cp))
                 self.in_channels = out_channels
-            # self.layers.append(nn.Sequential(*layer))
-            self.layers.extend(layer)
+            self.layers.append(Sequential(*layer))
 
     def _freeze_stages(self):
         for i in range(self.frozen_stages):
@@ -324,10 +416,13 @@ class EfficientNet(BaseBackbone):
             raise TypeError('pretrained must be a str or None')
 
     def forward(self, x):
+        outs = []
         for i, layer in enumerate(self.layers):
             x = layer(x)
+            if i in self.out_indices:
+                outs.append(x)
 
-        return (x, )
+        return tuple(outs)
 
     def train(self, mode=True):
         super(EfficientNet, self).train(mode)
@@ -336,11 +431,3 @@ class EfficientNet(BaseBackbone):
             for m in self.modules():
                 if isinstance(m, nn.BatchNorm2d):
                     m.eval()
-
-
-if __name__ == '__main__':
-    m = EfficientNet()
-    import ipdb
-
-    ipdb.set_trace()
-    print(m)
--- tools/convert_models/tensorflow2pytorch.py
+++ tools/convert_models/tensorflow2pytorch.py
@@ -3,11 +3,13 @@ import os
 
 import numpy as np
 import torch
+from mmcv.runner import Sequential
 from tensorflow.python.training import py_checkpoint_reader
 
+from mmcls.models.backbones.efficientnet import EfficientNet
 
-def tr(v):
-    # tensorflow weights to pytorch weights
+
+def tf2pth(v):
     if v.ndim == 4:
         return np.ascontiguousarray(v.transpose(3, 2, 0, 1))
     elif v.ndim == 2:
@@ -16,35 +18,50 @@ def tr(v):
 
 
 def read_ckpt(ckpt):
-    # https://github.com/tensorflow/tensorflow/issues/1823
     reader = py_checkpoint_reader.NewCheckpointReader(ckpt)
     weights = {
-        n: reader.get_tensor(n)
+        n: torch.as_tensor(tf2pth(reader.get_tensor(n)))
         for (n, _) in reader.get_variable_to_shape_map().items()
     }
-    pyweights = {k: torch.as_tensor(tr(v)) for (k, v) in weights.items()}
-    return pyweights
+    return weights
 
 
 def map_key(weight):
     m = dict()
     has_expand_conv = set()
     max_idx = 0
+    name = None
     for k, v in weight.items():
         seg = k.split('/')
+        if len(seg) == 1:
+            continue
+        name = seg[0][13:]
         if seg[2] == 'tpu_batch_normalization_2':
             has_expand_conv.add(seg[1])
         if seg[1].startswith('blocks_'):
             idx = int(seg[1][7:]) + 1
             max_idx = max(max_idx, idx)
+
+    model = EfficientNet(name)
+    idx2key = []
+    for idx, module in enumerate(model.layers):
+        if isinstance(module, Sequential):
+            for j in range(len(module)):
+                idx2key.append('{}.{}'.format(idx, j))
+        else:
+            idx2key.append('{}'.format(idx))
+
     for k, v in weight.items():
+
         if 'Exponential' in k:
             continue
         seg = k.split('/')
+        if len(seg) == 1:
+            continue
         if seg[2] == 'depthwise_conv2d':
             v = v.transpose(1, 0)
         if seg[1] == 'stem':
-            prefix = 'backbone.layers.0'
+            prefix = 'backbone.layers.{}'.format(idx2key[0])
             mapping = {
                 'conv2d/kernel': 'conv.weight',
                 'tpu_batch_normalization/beta': 'bn.bias',
@@ -57,75 +74,87 @@ def map_key(weight):
 
         elif seg[1].startswith('blocks_'):
             idx = int(seg[1][7:]) + 1
-            prefix = '.'.join(['backbone', 'layers', str(idx)])
+            prefix = '.'.join(['backbone', 'layers', idx2key[idx]])
+
+            base_mapping = {
+                'depthwise_conv2d/depthwise_kernel':
+                'depthwise_conv.conv.weight',
+                'se/conv2d/kernel': 'se.conv1.conv.weight',
+                'se/conv2d/bias': 'se.conv1.conv.bias',
+                'se/conv2d_1/kernel': 'se.conv2.conv.weight',
+                'se/conv2d_1/bias': 'se.conv2.conv.bias'
+            }
 
             if seg[1] not in has_expand_conv:
                 mapping = {
-                    'depthwise_conv2d/depthwise_kernel':
-                    'depthwise_conv.conv.weight',
-                    'conv2d/kernel': 'linear_conv.conv.weight',
-                    'tpu_batch_normalization/beta': 'depthwise_conv.bn.bias',
+                    'conv2d/kernel':
+                    'linear_conv.conv.weight',
+                    'tpu_batch_normalization/beta':
+                    'depthwise_conv.bn.bias',
                     'tpu_batch_normalization/gamma':
                     'depthwise_conv.bn.weight',
                     'tpu_batch_normalization/moving_mean':
                     'depthwise_conv.bn.running_mean',
                     'tpu_batch_normalization/moving_variance':
                     'depthwise_conv.bn.running_var',
-                    'tpu_batch_normalization_1/beta': 'linear_conv.bn.bias',
-                    'tpu_batch_normalization_1/gamma': 'linear_conv.bn.weight',
+                    'tpu_batch_normalization_1/beta':
+                    'linear_conv.bn.bias',
+                    'tpu_batch_normalization_1/gamma':
+                    'linear_conv.bn.weight',
                     'tpu_batch_normalization_1/moving_mean':
                     'linear_conv.bn.running_mean',
                     'tpu_batch_normalization_1/moving_variance':
                     'linear_conv.bn.running_var',
-                    'se/conv2d/kernel': 'se.conv1.conv.weight',
-                    'se/conv2d/bias': 'se.conv1.conv.bias',
-                    'se/conv2d_1/kernel': 'se.conv2.conv.weight',
-                    'se/conv2d_1/bias': 'se.conv2.conv.bias'
                 }
             else:
                 mapping = {
                     'depthwise_conv2d/depthwise_kernel':
                     'depthwise_conv.conv.weight',
-                    'conv2d/kernel': 'expand_conv.conv.weight',
-                    'conv2d_1/kernel': 'linear_conv.conv.weight',
-                    'tpu_batch_normalization/beta': 'expand_conv.bn.bias',
-                    'tpu_batch_normalization/gamma': 'expand_conv.bn.weight',
+                    'conv2d/kernel':
+                    'expand_conv.conv.weight',
+                    'conv2d_1/kernel':
+                    'linear_conv.conv.weight',
+                    'tpu_batch_normalization/beta':
+                    'expand_conv.bn.bias',
+                    'tpu_batch_normalization/gamma':
+                    'expand_conv.bn.weight',
                     'tpu_batch_normalization/moving_mean':
                     'expand_conv.bn.running_mean',
                     'tpu_batch_normalization/moving_variance':
                     'expand_conv.bn.running_var',
-                    'tpu_batch_normalization_1/beta': 'depthwise_conv.bn.bias',
+                    'tpu_batch_normalization_1/beta':
+                    'depthwise_conv.bn.bias',
                     'tpu_batch_normalization_1/gamma':
                     'depthwise_conv.bn.weight',
                     'tpu_batch_normalization_1/moving_mean':
                     'depthwise_conv.bn.running_mean',
                     'tpu_batch_normalization_1/moving_variance':
                     'depthwise_conv.bn.running_var',
-                    'tpu_batch_normalization_2/beta': 'linear_conv.bn.bias',
-                    'tpu_batch_normalization_2/gamma': 'linear_conv.bn.weight',
+                    'tpu_batch_normalization_2/beta':
+                    'linear_conv.bn.bias',
+                    'tpu_batch_normalization_2/gamma':
+                    'linear_conv.bn.weight',
                     'tpu_batch_normalization_2/moving_mean':
                     'linear_conv.bn.running_mean',
                     'tpu_batch_normalization_2/moving_variance':
                     'linear_conv.bn.running_var',
-                    'se/conv2d/kernel': 'se.conv1.conv.weight',
-                    'se/conv2d/bias': 'se.conv1.conv.bias',
-                    'se/conv2d_1/kernel': 'se.conv2.conv.weight',
-                    'se/conv2d_1/bias': 'se.conv2.conv.bias'
                 }
+            mapping.update(base_mapping)
             suffix = mapping['/'.join(seg[2:])]
             m[prefix + '.' + suffix] = v
         elif seg[1] == 'head':
+            seq_key = idx2key[max_idx + 1]
             mapping = {
                 'conv2d/kernel':
-                'backbone.layers.{}.conv.weight'.format(max_idx + 1),
+                'backbone.layers.{}.conv.weight'.format(seq_key),
                 'tpu_batch_normalization/beta':
-                'backbone.layers.{}.bn.bias'.format(max_idx + 1),
+                'backbone.layers.{}.bn.bias'.format(seq_key),
                 'tpu_batch_normalization/gamma':
-                'backbone.layers.{}.bn.weight'.format(max_idx + 1),
+                'backbone.layers.{}.bn.weight'.format(seq_key),
                 'tpu_batch_normalization/moving_mean':
-                'backbone.layers.{}.bn.running_mean'.format(max_idx + 1),
+                'backbone.layers.{}.bn.running_mean'.format(seq_key),
                 'tpu_batch_normalization/moving_variance':
-                'backbone.layers.{}.bn.running_var'.format(max_idx + 1),
+                'backbone.layers.{}.bn.running_var'.format(seq_key),
                 'dense/kernel':
                 'head.fc.weight',
                 'dense/bias':
@@ -139,12 +168,7 @@ def map_key(weight):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('infile', type=str, help='Path to the ckpt.')
-    parser.add_argument(
-        'outfile',
-        type=str,
-        nargs='?',
-        default='',
-        help='Output file (inferred if missing).')
+    parser.add_argument('outfile', type=str, help='Output file.')
     args = parser.parse_args()
     assert args.outfile
 
