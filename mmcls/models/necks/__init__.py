5de480ea9ef254fd7a90c50d66d42eb9b5e186eb
Ma Zerun <mzr1996@163.com>
2022-01-28 10:54:14 +0800

    [Feature] Support HRNet and add pre-trained models (#660)
    * Support HRNet
    
    * Add HRNet configs
    
    * Fix a bug in backward
    
    * Add configs and update docs.
    
    * Not use bias in conv before batch norm
    
    * Defaults to use `norm_eval=False`
    
    * Add unit tests and support out_channels in HRFuseScales
    
    * Update checkpoint path
    
    * Update docstring.
    
    * Remove incorrect files
    
    * Improve according to comments

--- mmcls/models/necks/__init__.py
+++ mmcls/models/necks/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .gap import GlobalAveragePooling
+from .hr_fuse import HRFuseScales
 
-__all__ = ['GlobalAveragePooling']
+__all__ = ['GlobalAveragePooling', 'HRFuseScales']
