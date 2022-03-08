aeeae3fd749f13b0b9704fe0aa279e4c80559f6e
Ezra-Yu <1105212286@qq.com>
2022-03-08 14:11:33 +0800

    fix name style

--- configs/swin_transformer/swin-large_8xb8_cub_384px.py
+++ configs/swin_transformer/swin-large_8xb8_cub_384px.py
@@ -5,7 +5,7 @@ _base_ = [
 ]
 
 # model settings
-checkpoint = 'https://download.openmmlab.com/mmclassification/v0/swin-transformer/convert/swin-base_3rdparty_in21k-384px.pth'  # noqa
+checkpoint = 'https://download.openmmlab.com/mmclassification/v0/swin-transformer/convert/swin-large_3rdparty_in21k-384px.pth'  # noqa
 model = dict(
     type='ImageClassifier',
     backbone=dict(
@@ -22,13 +22,14 @@ paramwise_cfg = dict(
     })
 
 optimizer = dict(
+    _delete_=True,
     type='AdamW',
     lr=5e-6,
     weight_decay=0.0005,
     eps=1e-8,
     betas=(0.9, 0.999),
     paramwise_cfg=paramwise_cfg)
-optimizer_config = dict(grad_clip=dict(max_norm=5.0))
+optimizer_config = dict(grad_clip=dict(max_norm=5.0), _delete_=True)
 
 log_config = dict(interval=20)  # log every 20 intervals
 
