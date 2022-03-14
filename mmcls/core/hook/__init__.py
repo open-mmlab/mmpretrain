9cd7d29c76f6d4970209dc69bf24d785497af45b
Rist115 <to78314910@gmail.com>
2022-03-15 08:36:52 +0900

    fix filename

--- mmcls/core/hook/__init__.py
+++ mmcls/core/hook/__init__.py
@@ -2,7 +2,7 @@
 from .class_num_check_hook import ClassNumCheckHook
 from .lr_updater import CosineAnnealingCooldownLrUpdaterHook
 from .precise_bn_hook import PreciseBNHook
-from .stop_augments_hook import SwitchDataAugHook, SwitchTrainAugHook
+from .switch_augments_hook import SwitchDataAugHook, SwitchTrainAugHook
 
 __all__ = [
     'ClassNumCheckHook', 'PreciseBNHook',
