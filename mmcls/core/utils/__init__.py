995f2912ab78474d1b5c79d0fc344170fb67c15b
Hubert <42952108+yingfhu@users.noreply.github.com>
2022-03-25 15:31:13 +0800

    [Feature] Add diff seeds to diff ranks.  (#744)
    * [Feature] Add diff seeds to diff ranks.
    
    * lint

--- mmcls/core/utils/__init__.py
+++ mmcls/core/utils/__init__.py
@@ -1,5 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .dist_utils import DistOptimizerHook, allreduce_grads
+from .dist_utils import DistOptimizerHook, allreduce_grads, sync_random_seed
 from .misc import multi_apply
 
-__all__ = ['allreduce_grads', 'DistOptimizerHook', 'multi_apply']
+__all__ = [
+    'allreduce_grads', 'DistOptimizerHook', 'multi_apply', 'sync_random_seed'
+]
