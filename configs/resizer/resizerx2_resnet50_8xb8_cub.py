eeb6b04be17eab8cffe0f96b1d43ab053d9a01e6
Rist115 <to78314910@gmail.com>
2022-04-04 10:48:42 +0900

    add resizer

---
+++ configs/resizer/resizerx2_resnet50_8xb8_cub.py
@@ -0,0 +1,54 @@
+_base_ = [
+    '../_base_/models/resnet50.py', '../_base_/datasets/cub_bs8_448.py',
+    '../_base_/schedules/cub_bs64.py', '../_base_/default_runtime.py'
+]
+
+# use pre-train weight converted from https://github.com/Alibaba-MIIL/ImageNet21K # noqa
+checkpoint = 'https://download.openmmlab.com/mmclassification/v0/resnet/resnet50_3rdparty-mill_in21k_20220331-faac000b.pth'  # noqa
+
+model = dict(
+    type='ImageClassifier',
+    backbone=[
+        dict(type='LearnableResizer', output_size=(384, 384)),
+        dict(
+            type='ResNeSt',
+            depth=50,
+            num_stages=4,
+            out_indices=(3, ),
+            style='pytorch',
+            init_cfg=dict(
+                type='Pretrained', checkpoint=checkpoint, prefix='backbone'))
+    ],
+    head=dict(num_classes=200, ))
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='Resize', size=1200),
+    dict(type='RandomCrop', size=896),
+    dict(type='RandomFlip', flip_prob=0.5, direction='horizontal'),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='ImageToTensor', keys=['img']),
+    dict(type='ToTensor', keys=['gt_label']),
+    dict(type='Collect', keys=['img', 'gt_label'])
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='Resize', size=1200),
+    dict(type='CenterCrop', crop_size=896),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='ImageToTensor', keys=['img']),
+    dict(type='Collect', keys=['img'])
+]
+data = dict(
+    train=dict(pipeline=train_pipeline),
+    val=dict(pipeline=test_pipeline),
+    test=dict(pipeline=test_pipeline))
+
+optimizer = dict(lr=0.01 / 8)
+
+log_config = dict(interval=20)  # log every 20 intervals
+
+checkpoint_config = dict(
+    interval=1, max_keep_ckpts=3)  # save last three checkpoints
---
+++ tests/test_models/test_backbones/test_resizer.py
@@ -0,0 +1,18 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import torch
+from torch import Tensor
+
+from mmcls.models.backbones import LearnableResizer
+
+
+def test_learnableresizer():
+    imgs = torch.randn(16, 3, 48, 48)
+
+    preprocessors = LearnableResizer(input_size=[32, 32])
+    imgs = preprocessors(imgs)
+    assert isinstance(imgs, Tensor) and imgs.shape == (16, 3, 32, 32)
+
+    single_img = torch.randn(1, 3, 32, 32)
+    single_img = preprocessors(single_img)
+    assert isinstance(single_img,
+                      Tensor) and single_img.shape == (1, 3, 32, 32)
