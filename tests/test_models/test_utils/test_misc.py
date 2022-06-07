f7a7bd29296a2db193419ee7343f0e962bfe3e53
Philipp Allgeuer <philipp.allgeuer@uni-hamburg.de>
2022-06-07 11:57:18 +0200

    Add torch_meshgrid_ij wrapper

--- tests/test_models/test_utils/test_misc.py
+++ tests/test_models/test_utils/test_misc.py
@@ -2,6 +2,7 @@
 import pytest
 import torch
 from mmcv.utils import digit_version
+from packaging import version
 
 from mmcls.models.utils import channel_shuffle, is_tracing, make_divisible
 
@@ -57,3 +58,14 @@ def test_is_tracing():
     # test with trace
     traced_foo = torch.jit.trace(foo, (torch.rand(1), ))
     assert isinstance(traced_foo(x), torch.Tensor)
+
+
+_torch_version_meshgrid_indexing = version.parse(
+    torch.__version__) >= version.parse('1.10.0a0')
+
+
+def torch_meshgrid_ij(*tensors):
+    if _torch_version_meshgrid_indexing:
+        return torch.meshgrid(*tensors, indexing='ij')
+    else:
+        return torch.meshgrid(*tensors)  # Uses indexing='ij' by default
