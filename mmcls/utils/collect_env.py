35cba87f82c3fc81781f89829326f4f6e1835551
Ezra-Yu <1105212286@qq.com>
2022-07-14 14:05:52 +0800

    refactor version and utils

--- mmcls/utils/collect_env.py
+++ mmcls/utils/collect_env.py
@@ -1,6 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from mmcv.utils import collect_env as collect_base_env
-from mmcv.utils import get_git_hash
+from mmengine.utils import get_git_hash
 
 import mmcls
 
@@ -10,8 +10,3 @@ def collect_env():
     env_info = collect_base_env()
     env_info['MMClassification'] = mmcls.__version__ + '+' + get_git_hash()[:7]
     return env_info
-
-
-if __name__ == '__main__':
-    for name, val in collect_env().items():
-        print(f'{name}: {val}')
