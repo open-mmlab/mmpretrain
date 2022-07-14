a667b488aec2a03b9324a6d44ea2428ab42df299
yingfhu <yingfhu@gmail.com>
2022-07-14 19:21:34 +0800

    minor fix

--- configs/vision_transformer/vit-base-p16_ft-64xb64_in1k-384.py
+++ configs/vision_transformer/vit-base-p16_ft-64xb64_in1k-384.py
@@ -40,4 +40,4 @@ optim_wrapper = dict(clip_grad=dict(max_norm=1.0))
 # NOTE: `auto_scale_lr` is for automatically scaling LR,
 # USER SHOULD NOT CHANGE ITS VALUES.
 # base_batch_size = (64 GPUs) x (64 samples per GPU)
-auto_scale_lr = dict(base_batch_size=256)
+auto_scale_lr = dict(base_batch_size=4096)
