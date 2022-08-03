1edc7619de7c0f86c5a1cd9b91cfbdaaabd3d58e
mzr1996 <mzr1996@163.com>
2022-08-03 17:53:21 +0800

    Remove `init_random_seed` and `set_random_seed`.

--- mmcls/utils/__init__.py
+++ mmcls/utils/__init__.py
@@ -1,8 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .collect_env import collect_env
-from .setup_env import init_random_seed, register_all_modules, set_random_seed
+from .setup_env import register_all_modules
 
-__all__ = [
-    'collect_env', 'register_all_modules', 'init_random_seed',
-    'set_random_seed'
-]
+__all__ = ['collect_env', 'register_all_modules']
