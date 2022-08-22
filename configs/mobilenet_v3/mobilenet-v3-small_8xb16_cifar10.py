ec71d071d8b8474c3860edf8dd1449f3ac8490f4
Jiahao Wang <48375204+techmonsterwang@users.noreply.github.com>
2022-08-22 10:28:33 +0800

    [Improve] Fixed typo in `RepVGG`. (#985)
    * [Improve] Use `forward_dummy` to calculate FLOPS. (#953)
    
    * fixed
    
    Co-authored-by: Ming-Hsuan-Tu <alec.tu@acer.com>

--- configs/mobilenet_v3/mobilenet-v3-small_8xb16_cifar10.py
+++ configs/mobilenet_v3/mobilenet-v3-small_8xb16_cifar10.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/mobilenet-v3-small_8xb16_cifar.py',
+    '../_base_/models/mobilenet-v3-small_cifar.py',
     '../_base_/datasets/cifar10_bs16.py',
     '../_base_/schedules/cifar10_bs128.py', '../_base_/default_runtime.py'
 ]
