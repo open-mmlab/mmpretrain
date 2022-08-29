84fa490bbc0d55bb0d212cf4def58e738b14a955
tonysy <sy.zhangbuaa@gmail.com>
2022-08-29 20:22:09 +0800

    [Feature] Use fvcore for flops count

--- tools/analysis_tools/get_flops.py
+++ tools/analysis_tools/get_flops.py
@@ -1,8 +1,11 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import argparse
 
+import torch
+from fvcore.nn import (ActivationCountAnalysis, FlopCountAnalysis,
+                       flop_count_str, flop_count_table, parameter_count)
+from fvcore.nn.print_model_statistics import _format_size
 from mmcv import Config
-from mmcv.cnn.utils import get_model_complexity_info
 
 from mmcls.models import build_classifier
 
@@ -42,10 +45,28 @@ def main():
             'FLOPs counter is currently not currently supported with {}'.
             format(model.__class__.__name__))
 
-    flops, params = get_model_complexity_info(model, input_shape)
+    inputs = (torch.randn((1, *input_shape)), )
+    flops_ = FlopCountAnalysis(model, inputs)
+    activations_ = ActivationCountAnalysis(model, inputs)
+
+    flops = _format_size(flops_.total())
+    activations = _format_size(activations_.total())
+    params = _format_size(parameter_count(model)[''])
+
+    flop_table = flop_count_table(
+        flops=flops_,
+        activations=activations_,
+        show_param_shapes=True,
+    )
+    flop_str = flop_count_str(flops=flops_, activations=activations_)
+
+    print('\n' + flop_str)
+    print('\n' + flop_table)
+
     split_line = '=' * 30
     print(f'{split_line}\nInput shape: {input_shape}\n'
-          f'Flops: {flops}\nParams: {params}\n{split_line}')
+          f'Flops: {flops}\nParams: {params}\n'
+          f'Activation: {activations}\n{split_line}')
     print('!!!Please be cautious if you use the results in papers. '
           'You may need to check if all ops are supported and verify that the '
           'flops computation is correct.')
