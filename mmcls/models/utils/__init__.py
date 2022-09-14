f14ea4e3afa36b277358ac7431bf0075e4d56b6a
Ezra-Yu <18586273+Ezra-Yu@users.noreply.github.com>
2022-09-14 10:15:37 +0800

    update UT

--- mmcls/models/utils/__init__.py
+++ mmcls/models/utils/__init__.py
@@ -10,7 +10,7 @@ from .inverted_residual import InvertedResidual
 from .make_divisible import make_divisible
 from .position_encoding import ConditionalPositionEncoding
 from .se_layer import SELayer
-from .useful_layers import LayerScale
+from .layer_scale import LayerScale
 
 __all__ = [
     'channel_shuffle', 'make_divisible', 'InvertedResidual', 'SELayer',
