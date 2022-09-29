8d9865a9597bb5cc89d5c26e96db3b2eeb2e8320
mzr1996 <mzr1996@163.com>
2022-09-29 15:17:50 +0800

    Add unit tests

--- tests/test_models/test_classifiers.py
+++ tests/test_models/test_classifiers.py
@@ -47,7 +47,7 @@ class TestImageClassifier(TestCase):
         # test set batch augmentation from train_cfg
         cfg = {
             **self.DEFAULT_ARGS, 'train_cfg':
-            dict(augments=dict(type='Mixup', alpha=1., num_classes=10))
+            dict(augments=dict(type='Mixup', alpha=1.))
         }
         model: ImageClassifier = MODELS.build(cfg)
         self.assertIsNotNone(model.data_preprocessor.batch_augments)
