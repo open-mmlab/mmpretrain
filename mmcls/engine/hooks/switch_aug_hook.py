db1533855e2421358c30a8fd28ed7ee17115dc19
Ezra-Yu <18586273+Ezra-Yu@users.noreply.github.com>
2022-10-18 18:10:50 +0800

    fix lint

--- mmcls/engine/hooks/switch_aug_hook.py
+++ mmcls/engine/hooks/switch_aug_hook.py
@@ -2,10 +2,10 @@
 from typing import Optional, Sequence
 
 from mmcv.transforms import Compose
-from mmcls.models.utils import RandomBatchAugment
-from mmengine.model import is_model_wrapper
 from mmengine.hooks import Hook
+from mmengine.model import is_model_wrapper
 
+from mmcls.models.utils import RandomBatchAugment
 from mmcls.registry import HOOKS, MODELS
 
 DATA_BATCH = Optional[Sequence[dict]]
@@ -27,28 +27,28 @@ class SwitchTrainAugHook(Hook):
             Defaults to 'unchange', means not changing the train augments.
         loss (dict, str, optional): the new train loss.
             Defaults to 'unchange', means not changing the train loss.
-    
+
     Example:
         >>> # in config
         >>> # deinfe new_train_pipeline, new_train_augments or new_loss
         >>> custom_hooks = [
-        >>>             dict(                                                                                                                                                                                     
-        >>>                 type='SwitchDataAugHook',                                                                                                                                                             
-        >>>                 action_epoch=37,                                                                                                                                                                      
+        >>>             dict(
+        >>>                 type='SwitchDataAugHook',
+        >>>                 action_epoch=37,
         >>>                 pipeline=new_train_pipeline,
         >>>                 train_augments=new_train_augments,
         >>>                 loss=new_loss),]
         >>>
         >>> # switch data augments by epoch
         >>> switch_hook = dict(
-        >>>                 type='SwitchTrainAugHook', 
-        >>>                 pipeline=new_pipeline, 
+        >>>                 type='SwitchTrainAugHook',
+        >>>                 pipeline=new_pipeline,
         >>>                 action_epoch=5)
         >>> runner.register_custom_hooks([switch_hook])
-        >>> 
+        >>>
         >>> # switch train augments and loss by iter
         >>> switch_hook = dict(
-        >>>                 type='SwitchTrainAugHook', 
+        >>>                 type='SwitchTrainAugHook',
         >>>                 train_augments=new_train_augments,
         >>>                 loss=new_loss,
         >>>                 action_iter=5)
@@ -100,7 +100,6 @@ class SwitchTrainAugHook(Hook):
             self.train_augments = RandomBatchAugment(**train_augments)
 
         self.loss = MODELS.build(loss) if loss != 'unchange' else loss
-            
 
     def before_train(self, runner) -> None:
         """before run setting. If resume form a checkpoint, check whether is
@@ -110,8 +109,8 @@ class SwitchTrainAugHook(Hook):
             runner (Runner): The runner of the training, validation or testing
                 process.
         """
-        # if this hook is the latest switch hook obj in the previously unfinished 
-        # tasks, then do the switch process before train
+        # if this hook is the latest switch hook obj in the previously
+        # unfinished tasks, then do the switch process before train
         if runner._resume and self._is_lastest_switch_hook(runner):
             action_milestone_str = ' after resume'
             self._do_switch(runner, action_milestone_str)
@@ -133,8 +132,8 @@ class SwitchTrainAugHook(Hook):
             self._do_switch(runner, action_milestone_str)
 
     def _is_lastest_switch_hook(self, runner):
-        """a helper function to judge if this hook is the latest processed switch 
-        hooks with the same class name in a runner."""
+        """a helper function to judge if this hook is the latest processed
+        switch hooks with the same class name in a runner."""
         # collect all the switch_hook with the same class name in a list.
         switch_hook_objs = [
             hook_obj for hook_obj in runner._hooks
@@ -144,7 +143,7 @@ class SwitchTrainAugHook(Hook):
         # get the latest swict hook based on the current iter.
         dataset_length = len(runner.train_loop.dataloader)
         cur_iter = runner.train_loop.iter
-        
+
         min_gap, min_gap_idx = float('inf'), -1
         for i, switch_hook_obj in enumerate(switch_hook_objs):
             # use iter to calculate
@@ -174,7 +173,7 @@ class SwitchTrainAugHook(Hook):
         if self.pipeline != 'unchange':
             self._switch_train_loader_pipeline(runner)
             runner.logger.info(f'Switch train pipeline{action_milestone_str}.')
-            
+
         if self.loss != 'unchange':
             self._switch_train_loss(runner)
             runner.logger.info(f'Switch train loss{action_milestone_str}.')
@@ -209,8 +208,8 @@ class SwitchTrainAugHook(Hook):
         # The dataset pipeline cannot be updated when persistent_workers
         # is True, so we need to force the dataloader's multi-process
         # restart. This is a very hacky approach.
-        if hasattr(train_loader, 'persistent_workers') \
-            and train_loader.persistent_workers is True:
+        if hasattr(train_loader, 'persistent_workers'
+                   ) and train_loader.persistent_workers is True:
             train_loader._DataLoader__initialized = False
             train_loader._iterator = None
             self._restart_dataloader = True
