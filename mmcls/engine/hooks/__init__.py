521e1db9dc12ad7d6245145a339a7d80a1f9075c
Ezra-Yu <18586273+Ezra-Yu@users.noreply.github.com>
2022-10-18 17:28:02 +0800

    add switch hook and UTs

--- mmcls/engine/hooks/__init__.py
+++ mmcls/engine/hooks/__init__.py
@@ -1,10 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .class_num_check_hook import ClassNumCheckHook
 from .precise_bn_hook import PreciseBNHook
+from .switch_aug_hook import SwitchTrainAugHook
 from .visualization_hook import VisualizationHook
 
 __all__ = [
-    'ClassNumCheckHook',
-    'PreciseBNHook',
-    'VisualizationHook',
+    'ClassNumCheckHook', 'PreciseBNHook', 'VisualizationHook',
+    'SwitchTrainAugHook'
 ]
