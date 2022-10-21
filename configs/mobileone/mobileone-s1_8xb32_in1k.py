89094502a057eb2e7144ad7fc2b84e6a39a05158
Ezra-Yu <18586273+Ezra-Yu@users.noreply.github.com>
2022-10-21 18:24:19 +0800

    update configs

--- configs/mobileone/mobileone-s1_8xb32_in1k.py
+++ configs/mobileone/mobileone-s1_8xb32_in1k.py
@@ -11,10 +11,19 @@ optim_wrapper = dict(paramwise_cfg=dict(norm_decay_mult=0.))
 val_dataloader = dict(batch_size=256)
 test_dataloader = dict(batch_size=256)
 
+bgr_mean = _base_.data_preprocessor['mean'][::-1]
 base_train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='RandomResizedCrop', scale=224, backend='pillow'),
     dict(type='RandomFlip', prob=0.5, direction='horizontal'),
+    dict(
+        type='RandAugment',
+        policies='timm_increasing',
+        num_policies=2,
+        total_level=10,
+        magnitude_level=7,
+        magnitude_std=0.5,
+        hparams=dict(pad_val=[round(x) for x in bgr_mean])),
     dict(type='PackClsInputs')
 ]
 
@@ -23,15 +32,18 @@ import copy  # noqa: E402
 # modify start epoch's RandomResizedCrop.scale to 160
 train_pipeline_1e = copy.deepcopy(base_train_pipeline)
 train_pipeline_1e[1]['scale'] = 160
+train_pipeline_1e[3]['magnitude_level'] *= 0.1
 _base_.train_dataloader.dataset.pipeline = train_pipeline_1e
 
 # modify 37 epoch's RandomResizedCrop.scale to 192
 train_pipeline_37e = copy.deepcopy(base_train_pipeline)
 train_pipeline_37e[1]['scale'] = 192
+train_pipeline_1e[3]['magnitude_level'] *= 0.2
 
 # modify 112 epoch's RandomResizedCrop.scale to 224
 train_pipeline_112e = copy.deepcopy(base_train_pipeline)
 train_pipeline_112e[1]['scale'] = 224
+train_pipeline_1e[3]['magnitude_level'] *= 0.3
 
 custom_hooks = [
     dict(
