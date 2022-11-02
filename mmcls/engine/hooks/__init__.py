693596bc2f24681c8b0d61ae1c827526a3fed2bf
zzc98 <40905160+zzc98@users.noreply.github.com>
2022-11-02 17:43:56 +0800

    [Feature] Add Base Retriever and Image2Image Retriever for retrieval tasks. (#1055)
    * feat: add image retriever
    
    * feat: add image retriever
    
    * feat: add image retriever
    
    * feat: add image retriever
    
    * feat: add image retriever
    
    * feat: add image retriever
    
    * feat: add image retriever
    
    * feat: add image retriever
    
    * feat: add image retriever
    
    * update retriever
    
    * fix lint
    
    * add hook unit test
    
    * Use `register_buffer` to save prototype vectors and add a progress bar
    during preparing prototype.
    
    * update UTs
    
    * update UTs
    
    * fix typo
    
    * modify the hook
    
    Co-authored-by: Ezra-Yu <18586273+Ezra-Yu@users.noreply.github.com>
    Co-authored-by: mzr1996 <mzr1996@163.com>

--- mmcls/engine/hooks/__init__.py
+++ mmcls/engine/hooks/__init__.py
@@ -1,10 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .class_num_check_hook import ClassNumCheckHook
 from .precise_bn_hook import PreciseBNHook
+from .retriever_hooks import PrepareProtoBeforeValLoopHook
 from .visualization_hook import VisualizationHook
 
 __all__ = [
-    'ClassNumCheckHook',
-    'PreciseBNHook',
-    'VisualizationHook',
+    'ClassNumCheckHook', 'PreciseBNHook', 'VisualizationHook',
+    'PrepareProtoBeforeValLoopHook'
 ]
