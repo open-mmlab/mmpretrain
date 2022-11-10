b259dc37a5e615c44e26afa99cc506b503f3f475
marouaneamz <maroineamil99@gmail.com>
2022-11-10 10:27:26 +0100

    correct test

--- tests/test_structures/test_datasample.py
+++ tests/test_structures/test_datasample.py
@@ -12,7 +12,6 @@ from mmcls.structures import ClsDataSample, MultiTaskDataSample
 class TestClsDataSample(TestCase):
 
     def _test_set_label(self, key):
-
         data_sample = ClsDataSample()
         method = getattr(data_sample, 'set_' + key)
         # Test number
