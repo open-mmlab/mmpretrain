49d399ba2ab4bd5ecf2c1f8fc5a732803bdf29a8
marouaneamz <maroineamil99@gmail.com>
2022-11-14 13:14:21 +0100

    fix linter

--- mmcls/evaluation/metrics/multi_task.py
+++ mmcls/evaluation/metrics/multi_task.py
@@ -9,7 +9,7 @@ from mmcls.registry import METRICS
 
 @METRICS.register_module()
 class MultiTasks(BaseMetric):
-    """"""
+    """a."""
 
     def __init__(self,
                  task_metrics: Dict,
@@ -23,13 +23,11 @@ class MultiTasks(BaseMetric):
 
     def process(self, data_batch, data_samples: Sequence[dict]):
         """Process one batch of data samples.
-
         The processed results should be stored in ``self.results``, which will
         be used to computed the metrics when all batches have been processed.
-
         Args:
-                data_batch: A batch of data from the dataloader.
-                data_samples (Sequence[dict]): A batch of outputs from the model.
+            data_batch: A batch of data from the dataloader.
+            data_samples (Sequence[dict]): A batch of outputs from the model.
         """
         for task_name in self.task_metrics.keys():
             index = list(self.task_metrics.keys()).index(task_name)
@@ -49,13 +47,11 @@ class MultiTasks(BaseMetric):
 
     def compute_metrics(self, results: List):
         """Compute the metrics from processed results.
-
         Args:
-                results (dict): The processed results of each batch.
-
+            results (dict): The processed results of each batch.
         Returns:
-                Dict: The computed metrics. The keys are the names of the metrics,
-                and the values are corresponding results.
+            Dict: The computed metrics. The keys are the names of the metrics,
+            and the values are corresponding results.
         """
         # NOTICE: don't access `self.results` from the method.
         Output = {}
