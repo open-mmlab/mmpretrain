2392287078816b1b973912e221d3b15dee22a763
Ezra-Yu <18586273+Ezra-Yu@users.noreply.github.com>
2022-11-16 15:44:06 +0800

    fix lint

--- mmcls/engine/hooks/__init__.py
+++ mmcls/engine/hooks/__init__.py
@@ -1,8 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .class_num_check_hook import ClassNumCheckHook
 from .precise_bn_hook import PreciseBNHook
-from .switch_recipe_hook import SwitchRecipeHook
 from .retriever_hooks import PrepareProtoBeforeValLoopHook
+from .switch_recipe_hook import SwitchRecipeHook
 from .visualization_hook import VisualizationHook
 
 __all__ = [
