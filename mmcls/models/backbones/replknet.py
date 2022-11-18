d17b6f666ad21defdcb8da0327d1d5ac6bb2cd83
techmonsterwang <2959688260@qq.com>
2022-11-18 18:16:10 +0800

    update replknet model

--- mmcls/models/backbones/replknet.py
+++ mmcls/models/backbones/replknet.py
@@ -666,34 +666,3 @@ class RepLKNet(BaseBackbone):
             if hasattr(m, 'merge_kernel'):
                 m.merge_kernel()
         self.small_kernel_merged = True
-
-    #   If your framework cannot automatically fuse BN for inference,
-    #   you may do it manually.
-    #   The BNs after and before conv layers can be removed.
-    #   No need to call this if your framework support automatic BN fusion.
-    def deep_fuse_BN(self):
-        for m in self.modules():
-            if not isinstance(m, nn.Sequential):
-                continue
-            if not len(m) in [2, 3]:  # Only handle conv-BN or conv-BN-relu
-                continue
-            #   If you use a custom Conv2d impl,
-            #   assume it also has 'kernel_size' and 'weight'
-            if hasattr(m[0], 'kernel_size') and hasattr(
-                    m[0], 'weight') and isinstance(m[1], nn.BatchNorm2d):
-                conv = m[0]
-                bn = m[1]
-                fused_kernel, fused_bias = fuse_bn(conv, bn)
-                fused_conv = nn.Conv2d(
-                    conv.in_channels,
-                    conv.out_channels,
-                    kernel_size=conv.kernel_size,
-                    stride=conv.stride,
-                    padding=conv.padding,
-                    dilation=conv.dilation,
-                    groups=conv.groups,
-                    bias=True)
-                fused_conv.weight.data = fused_kernel
-                fused_conv.bias.data = fused_bias
-                m[0] = fused_conv
-                m[1] = nn.Identity()
