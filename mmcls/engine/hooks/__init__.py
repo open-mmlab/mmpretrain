c3c1cb93aade549911aedb1c1eda5cc28180c08e
Ezra-Yu <18586273+Ezra-Yu@users.noreply.github.com>
2022-11-18 18:12:03 +0800

    [Feature] Add Switch Recipe Hook. (#1101)
    * add switch hook and UTs
    
    * update doc
    
    * update doc
    
    * fix lint
    
    * fix ci
    
    * fix ci
    
    * fix typo
    
    * fix ci
    
    * switchTrainAugHook to switchRecipeHook
    
    * fix lint
    
    * Refactor the `SwitchRecipeHook`.
    
    * Fix windows CI
    
    * Fix windows CI
    
    * Fix windows CI.
    
    Co-authored-by: mzr1996 <mzr1996@163.com>

--- mmcls/engine/hooks/__init__.py
+++ mmcls/engine/hooks/__init__.py
@@ -2,9 +2,10 @@
 from .class_num_check_hook import ClassNumCheckHook
 from .precise_bn_hook import PreciseBNHook
 from .retriever_hooks import PrepareProtoBeforeValLoopHook
+from .switch_recipe_hook import SwitchRecipeHook
 from .visualization_hook import VisualizationHook
 
 __all__ = [
     'ClassNumCheckHook', 'PreciseBNHook', 'VisualizationHook',
-    'PrepareProtoBeforeValLoopHook'
+    'SwitchRecipeHook', 'PrepareProtoBeforeValLoopHook'
 ]
