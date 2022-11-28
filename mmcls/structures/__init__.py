404d15dd011f138ad8dba35e3952b1aad55f0892
marouaneamz <maroineamil99@gmail.com>
2022-11-28 10:06:25 +0100

    Fix cannot import name MultiTaskDataSample

--- mmcls/structures/__init__.py
+++ mmcls/structures/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .cls_data_sample import ClsDataSample
+from .multi_task_data_sample import MultiTaskDataSample
 from .utils import (batch_label_to_onehot, cat_batch_labels,
                     stack_batch_scores, tensor_split)
 
