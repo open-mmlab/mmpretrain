2e3e61f435a8623a1bbb29f208059fd02949d9c9
mzr1996 <mzr1996@163.com>
2022-11-28 17:46:11 +0800

    Fix lint

--- mmcls/engine/hooks/__init__.py
+++ mmcls/engine/hooks/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .class_num_check_hook import ClassNumCheckHook
-from .margin_head_hooks import SetAdaptiveMarginsHook
 from .ema_hook import EMAHook
+from .margin_head_hooks import SetAdaptiveMarginsHook
 from .precise_bn_hook import PreciseBNHook
 from .retriever_hooks import PrepareProtoBeforeValLoopHook
 from .switch_recipe_hook import SwitchRecipeHook
