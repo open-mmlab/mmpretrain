1712e3d791ff2c4e25d0b6236955a325ddb685d1
Ezra-Yu <18586273+Ezra-Yu@users.noreply.github.com>
2022-12-14 11:47:37 +0800

    update code

--- mmcls/models/backbones/repvgg.py
+++ mmcls/models/backbones/repvgg.py
@@ -102,7 +102,6 @@ class RepVGGBlock(BaseModule):
             self.branch_1x1 = self.create_conv_bn(kernel_size=1)
 
         if se_cfg is not None:
-
             self.se_layer = SELayer(channels=out_channels, **se_cfg)
         else:
             self.se_layer = None
