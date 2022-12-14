3d4ce1153d7dab0ab1874058cb781eb4ff46eca5
qingtian <459291290@qq.com>
2022-12-14 12:47:46 +0800

    update b0 and s train pipeline

--- configs/efficientnet_v2/efficientnet_v2-s_8xb32_in1k.py
+++ configs/efficientnet_v2/efficientnet_v2-s_8xb32_in1k.py
@@ -5,6 +5,9 @@ _base_ = [
     '../_base_/default_runtime.py',
 ]
 
+bgr_mean = data_preprocessor['mean'][::-1]
+bgr_std = data_preprocessor['std'][::-1]
+
 # dataset settings
 dataset_type = 'ImageNet'
 data_preprocessor = dict(
@@ -18,8 +21,29 @@ data_preprocessor = dict(
 
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='EfficientNetRandomCrop', scale=300, crop_padding=0),
+    dict(
+        type='RandomResizedCrop',
+        scale=300,
+        backend='pillow',
+        interpolation='bicubic'),
     dict(type='RandomFlip', prob=0.5, direction='horizontal'),
+    dict(
+        type='RandAugment',
+        policies='timm_increasing',
+        num_policies=2,
+        total_level=10,
+        magnitude_level=9,
+        magnitude_std=0.5,
+        hparams=dict(
+            pad_val=[round(x) for x in bgr_mean], interpolation='bicubic')),
+    dict(
+        type='RandomErasing',
+        erase_prob=0.25,
+        mode='rand',
+        min_area_ratio=0.02,
+        max_area_ratio=1 / 3,
+        fill_color=bgr_mean,
+        fill_std=bgr_std),
     dict(type='PackClsInputs'),
 ]
 
