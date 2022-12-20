6587c372e309785ca9ad0ec7490f42fd8a4e6782
QINGTIAN <102738505+suibe-qingtian@users.noreply.github.com>
2022-12-20 20:18:09 +0800

    Update efficientnetv2-b3_8xb32_in1k.py

--- configs/efficientnet_v2/efficientnetv2-b3_8xb32_in1k.py
+++ configs/efficientnet_v2/efficientnetv2-b3_8xb32_in1k.py
@@ -12,7 +12,7 @@ train_pipeline = [
 
 test_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='EfficientNetCenterCrop', crop_size=300),
+    dict(type='EfficientNetCenterCrop', crop_size=300, crop_padding=0),
     dict(type='PackClsInputs'),
 ]
 
