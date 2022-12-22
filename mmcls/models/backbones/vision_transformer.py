5b266d9e7c49044d1fcf15d046df3c06af3ff505
Rongjie Li <limo97@163.com>
2022-12-22 16:33:57 +0800

    [Feature] Add clip backbone. (#1258)
    * clip backbone added
    
    * passed precommit
    
    * update readme
    
    * update according to PR review
    
    * add missing file
    
    * add unittest
    
    * refine metafile
    
    * refine metafile and readme for readdocs
    
    * refine metafile
    
    * refine metafile
    
    * Update metafile
    
    Co-authored-by: mzr1996 <mzr1996@163.com>

--- mmcls/models/backbones/vision_transformer.py
+++ mmcls/models/backbones/vision_transformer.py
@@ -236,6 +236,7 @@ class VisionTransformer(BaseBackbone):
                  interpolate_mode='bicubic',
                  patch_cfg=dict(),
                  layer_cfgs=dict(),
+                 pre_norm=False,
                  init_cfg=None):
         super(VisionTransformer, self).__init__(init_cfg)
 
@@ -264,6 +265,7 @@ class VisionTransformer(BaseBackbone):
             conv_type='Conv2d',
             kernel_size=patch_size,
             stride=patch_size,
+            bias=not pre_norm,  # disable bias if pre_norm is used(e.g., CLIP)
         )
         _patch_cfg.update(patch_cfg)
         self.patch_embed = PatchEmbed(**_patch_cfg)
@@ -319,6 +321,13 @@ class VisionTransformer(BaseBackbone):
             self.layers.append(TransformerEncoderLayer(**_layer_cfg))
 
         self.frozen_stages = frozen_stages
+        if pre_norm:
+            _, norm_layer = build_norm_layer(
+                norm_cfg, self.embed_dims, postfix=1)
+        else:
+            norm_layer = nn.Identity()
+        self.add_module('pre_norm', norm_layer)
+
         self.final_norm = final_norm
         if final_norm:
             self.norm1_name, norm1 = build_norm_layer(
@@ -417,6 +426,7 @@ class VisionTransformer(BaseBackbone):
             num_extra_tokens=self.num_extra_tokens)
         x = self.drop_after_pos(x)
 
+        x = self.pre_norm(x)
         if not self.with_cls_token:
             # Remove class token for transformer encoder input
             x = x[:, 1:]
