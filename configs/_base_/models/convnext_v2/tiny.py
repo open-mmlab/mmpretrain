060b0ed3b5cd47e3196c2be922cdaa6a927e8ce2
Ma Zerun <mzr1996@163.com>
2023-01-06 16:13:41 +0800

    [Feature] Support ConvNeXt-V2 backbone. (#1294)
    * [Feature] Support ConvNeXt-V2.
    
    * Use registry of mmcls instead of mmengine.
    
    * Add README.
    
    * Add unit tests and docs.

--- configs/_base_/models/convnext_v2/tiny.py
+++ configs/_base_/models/convnext_v2/tiny.py
@@ -1,13 +1,18 @@
 # Model settings
 model = dict(
     type='ImageClassifier',
-    backbone=dict(type='ConvNeXt', arch='tiny', drop_path_rate=0.1),
+    backbone=dict(
+        type='ConvNeXt',
+        arch='tiny',
+        drop_path_rate=0.2,
+        layer_scale_init_value=0.,
+        use_grn=True,
+    ),
     head=dict(
         type='LinearClsHead',
         num_classes=1000,
         in_channels=768,
-        loss=dict(
-            type='LabelSmoothLoss', label_smooth_val=0.1, mode='original'),
+        loss=dict(type='LabelSmoothLoss', label_smooth_val=0.2),
         init_cfg=None,
     ),
     init_cfg=dict(
