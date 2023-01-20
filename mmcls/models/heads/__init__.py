952463db68d286da7a9901156a0643bad4282bac
jts <jts250@126.com>
2023-01-20 18:31:46 +0800

    resolve some bugs

--- mmcls/models/heads/__init__.py
+++ mmcls/models/heads/__init__.py
@@ -4,6 +4,7 @@ from .conformer_head import ConformerHead
 from .deit_head import DeiTClsHead
 from .disalign_linear_head import DisAlignLinearClsHead
 from .efficientformer_head import EfficientFormerClsHead
+from .levit_head import LeViTClsHead
 from .linear_head import LinearClsHead
 from .margin_head import ArcFaceClsHead
 from .multi_label_cls_head import MultiLabelClsHead
@@ -11,11 +12,23 @@ from .multi_label_csra_head import CSRAClsHead
 from .multi_label_linear_head import MultiLabelLinearClsHead
 from .multi_task_head import MultiTaskHead
 from .stacked_head import StackedLinearClsHead
+from .vig_head import VigClsHead
 from .vision_transformer_head import VisionTransformerClsHead
 
 __all__ = [
-    'ClsHead', 'LinearClsHead', 'StackedLinearClsHead', 'MultiLabelClsHead',
-    'MultiLabelLinearClsHead', 'VisionTransformerClsHead', 'DeiTClsHead',
-    'ConformerHead', 'EfficientFormerClsHead', 'ArcFaceClsHead', 'CSRAClsHead',
-    'MultiTaskHead', 'LeViTClsHead', 'VigClsHead', 'DisAlignLinearClsHead'
+    'ClsHead',
+    'LinearClsHead',
+    'StackedLinearClsHead',
+    'MultiLabelClsHead',
+    'MultiLabelLinearClsHead',
+    'VisionTransformerClsHead',
+    'DeiTClsHead',
+    'ConformerHead',
+    'EfficientFormerClsHead',
+    'ArcFaceClsHead',
+    'CSRAClsHead',
+    'MultiTaskHead',
+    'LeViTClsHead',
+    'VigClsHead',
+    'DisAlignLinearClsHead',
 ]
