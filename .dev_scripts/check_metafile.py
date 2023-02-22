e0993cd68e29ea5c300eaef470aaef00032b9684
mzr1996 <mzr1996@163.com>
2023-02-22 16:29:01 +0800

    Update pre-commit and metafile check script

--- .dev_scripts/check_metafile.py
+++ .dev_scripts/check_metafile.py
@@ -1,4 +1,6 @@
 import argparse
+import logging
+import sys
 from pathlib import Path
 
 import yaml
@@ -6,6 +8,31 @@ from modelindex.load_model_index import load
 from modelindex.models.Collection import Collection
 from modelindex.models.Model import Model
 from modelindex.models.ModelIndex import ModelIndex
+
+
+class ContextFilter(logging.Filter):
+    metafile = None
+    name = None
+    failed = True
+
+    def filter(self, record: logging.LogRecord):
+        record.color = {
+            logging.WARNING: '\x1b[33;20m',
+            logging.ERROR: '\x1b[31;1m',
+        }.get(record.levelno, '')
+        self.failed = self.failed or (record.levelno >= logging.ERROR)
+        record.metafile = self.metafile or ''
+        record.name = ('' if self.name is None else '\x1b[32m' + self.name +
+                       '\x1b[0m: ')
+        return True
+
+
+context = ContextFilter()
+logging.basicConfig(
+    format='[%(metafile)s] %(color)s%(levelname)s\x1b[0m - %(name)s%(message)s'
+)
+logger = logging.getLogger()
+logger.addFilter(context)
 
 prog_description = """\
 Check the format of metafile.
@@ -23,61 +50,83 @@ def parse_args():
         '-w',
         action='store_true',
         help='Whether to enable all warnings.')
+    parser.add_argument('--skip', action='append', help='Rules to skip check.')
     args = parser.parse_args()
+    args.skip = args.skip or []
     return args
 
 
-def check_collection(modelindex: ModelIndex):
-    if len(modelindex.collections) != 1:
-        return 'One metafile should have only one collection.'
+def check_collection(modelindex: ModelIndex, skip=[]):
+
+    if len(modelindex.collections) == 0:
+        return ['No collection field.']
+    elif len(modelindex.collections) > 1:
+        logger.error('One metafile should have only one collection.')
+
     collection: Collection = modelindex.collections[0]
+
     if collection.name is None:
-        return 'The collection should have `Name` field.'
+        logger.error('The collection should have `Name` field.')
     if collection.readme is None:
-        return 'The collection should have `README` field.'
+        logger.error('The collection should have `README` field.')
     if not (MMCLS_ROOT / collection.readme).exists():
-        return f'The README {collection.readme} is not found.'
+        logger.error(f'The README {collection.readme} is not found.')
     if not isinstance(collection.paper, dict):
-        return ('The collection should have `Paper` field with '
-                '`Title` and `URL`.')
-    if 'Title' not in collection.paper:
+        logger.error('The collection should have `Paper` field with '
+                     '`Title` and `URL`.')
+    elif 'Title' not in collection.paper:
         # URL is not necessary.
-        return "The collection's paper should have `Paper` field."
+        logger.error("The collection's paper should have `Paper` field.")
 
 
-def check_model(model: Model, wall=True):
+def check_model(model: Model, wall=True, skip=[]):
+
+    context.name = None
     if model.name is None:
-        return "A model doesn't have `Name` field."
-    if model.metadata is None:
-        return f'{model.name}: No `Metadata` field.'
-    if model.metadata.parameters is None or model.metadata.flops is None:
-        return (
-            f'{model.name}: Metadata should have `Parameters` and '
-            '`FLOPs` fields. You can use `tools/analysis_tools/get_flops.py` '
-            'to calculate them.')
-    if model.results is not None:
+        logger.error("A model doesn't have `Name` field.")
+        return
+    context.name = model.name
+
+    if model.name.endswith('.py'):
+        logger.error("Don't add `.py` suffix in model name.")
+
+    if model.metadata is None and 'metadata' not in skip:
+        logger.error('No `Metadata` field.')
+
+    if (model.metadata.parameters is None
+            or model.metadata.flops is None) and 'flops-param' not in skip:
+        logger.error('Metadata should have `Parameters` and `FLOPs` fields. '
+                     'You can use `tools/analysis_tools/get_flops.py` '
+                     'to calculate them.')
+
+    if model.results is not None and 'result' not in skip:
         result = model.results[0]
         if not isinstance(result.dataset, str):
-            return (
-                f'{model.name}: Dataset field of Results should be a string. '
-                'If you want to specify the training dataset, please use '
-                '`Metadata.Training Data` field.')
-    if model.config is None:
-        return f'{model.name}: No `Config` field.'
-    if not (MMCLS_ROOT / model.config).exists():
-        return f'{model.name}: The config {model.config} is not found.'
+            logger.error('Dataset field of Results should be a string. '
+                         'If you want to specify the training dataset, '
+                         'please use `Metadata.Training Data` field.')
+
+    if 'config' not in skip:
+        if model.config is None:
+            logger.error('No `Config` field.')
+        elif not (MMCLS_ROOT / model.config).exists():
+            logger.error(f'The config {model.config} is not found.')
+
     if model.in_collection is None:
-        return f'{model.name}: No `In Collection` field.'
+        logger.error('No `In Collection` field.')
 
     if wall and model.data.get(
             'Converted From') is not None and '3rdparty' not in model.name:
-        print(f'WARN: The model name {model.name} should include '
-              "'3rdparty' since it's converted from other repository.")
+        logger.warning("The model name should include '3rdparty' "
+                       "since it's converted from other repository.")
+
     if wall and model.weights is not None and model.weights.endswith('.pth'):
         basename = model.weights.rsplit('/', 1)[-1]
         if not basename.startswith(model.name):
-            print(f'WARN: The checkpoint name {basename} is not the '
-                  f'same as the model name {model.name}.')
+            logger.warning(f'The checkpoint name {basename} is not the '
+                           'same as the model name.')
+
+    context.name = None
 
 
 def main(metafile: Path, args):
@@ -87,28 +136,34 @@ def main(metafile: Path, args):
     elif metafile.samefile(MMCLS_ROOT / 'model-index.yml'):
         return
 
+    context.metafile = metafile
+
     with open(MMCLS_ROOT / 'model-index.yml', 'r') as f:
         metafile_list = yaml.load(f, yaml.Loader)['Import']
         if not any(
                 metafile.samefile(MMCLS_ROOT / file)
                 for file in metafile_list):
-            raise ValueError(f'The metafile {metafile} is not imported in '
-                             'the `model-index.yml`.')
+            logger.error(
+                'The metafile is not imported in the `model-index.yml`.')
 
     modelindex = load(str(metafile))
     modelindex.build_models_with_collections()
-    collection_err = check_collection(modelindex)
-    if collection_err is not None:
-        raise ValueError(f'The `Collections` in the {metafile} is wrong:'
-                         f'\n\t{collection_err}')
+    check_collection(modelindex, args.skip)
+
+    names = {model.name for model in modelindex.models}
+
     for model in modelindex.models:
-        model_err = check_model(model, args.Wall)
-        if model_err is not None:
-            raise ValueError(
-                f'The `Models` in the {metafile} is wrong:\n\t{model_err}')
+        check_model(model, args.Wall, args.skip)
+
+        for downstream in model.data.get('Downstream', []):
+            if downstream not in names:
+                context.name = model.name
+                logger.error(
+                    f"The downstream model {downstream} doesn't exist.")
 
 
 if __name__ == '__main__':
     args = parse_args()
     for metafile in args.metafile:
         main(metafile, args)
+    sys.exit(int(context.failed))
