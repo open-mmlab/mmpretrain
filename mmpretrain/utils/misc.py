914297461651bb164299276ccab591d54bfc788f
fangyixiao18 <fangyx18@hotmail.com>
2023-02-28 16:37:51 +0800

    update

--- mmpretrain/utils/misc.py
+++ mmpretrain/utils/misc.py
@@ -1,10 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import torch.nn as nn
-from mmengine.model import BaseModel, is_model_wrapper
+from mmengine.model import is_model_wrapper
 
 
-def get_ori_model(model: nn.Module) -> BaseModel:
-    """Get model if the input model is a model wrapper.
+def get_ori_model(model: nn.Module) -> nn.Module:
+    """Get original model if the input model is a model wrapper.
 
     Args:
         model (nn.Module): A model may be a model wrapper.
