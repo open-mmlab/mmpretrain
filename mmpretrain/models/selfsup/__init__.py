362c85256496f627685158e86d686196f814311e
fangyixiao18 <fangyx18@hotmail.com>
2023-02-28 19:43:10 +0800

    add byol simclr simsiam

--- mmpretrain/models/selfsup/__init__.py
+++ mmpretrain/models/selfsup/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .base import BaseSelfSupervisor
 from .beit import VQKD, BEiTPretrainViT
+from .byol import BYOL
 from .cae import CAEViT, Encoder
 from .mae import MAEViT
 from .maskfeat import HOGGenerator, MaskFeatViT
@@ -8,7 +9,9 @@ from .milan import CLIPGenerator, MILANViT
 from .mixmim import MixMIMPretrainTransformer
 from .moco import MoCo
 from .mocov3 import MoCoV3ViT, MoCoV3
+from .simclr import SimCLR
 from .simmim import SimMIMSwinTransformer
+from .simsiam import SimSiam
 
 __all__ = [
     'BaseSelfSupervisor',
@@ -26,4 +29,7 @@ __all__ = [
     'SimMIMSwinTransformer',
     'MoCo',
     'MoCoV3',
+    'BYOL',
+    'SimCLR',
+    'SimSiam',
 ]
---
+++ mmpretrain/models/selfsup/simsiam.py
@@ -0,0 +1,54 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from typing import Dict, List, Tuple
+
+import torch
+
+from mmpretrain.registry import MODELS
+from mmpretrain.structures import DataSample
+from .base import BaseSelfSupervisor
+
+
+@MODELS.register_module()
+class SimSiam(BaseSelfSupervisor):
+    """SimSiam.
+
+    Implementation of `Exploring Simple Siamese Representation Learning
+    <https://arxiv.org/abs/2011.10566>`_. The operation of fixing learning rate
+    of predictor is in `engine/hooks/simsiam_hook.py`.
+    """
+
+    def extract_feat(self, inputs: List[torch.Tensor],
+                     **kwarg) -> Tuple[torch.Tensor]:
+        """Function to extract features from backbone.
+
+        Args:
+            inputs (List[torch.Tensor]): The input images.
+
+        Returns:
+            Tuple[torch.Tensor]: Backbone outputs.
+        """
+        return self.backbone(inputs[0])
+
+    def loss(self, inputs: List[torch.Tensor], data_samples: List[DataSample],
+             **kwargs) -> Dict[str, torch.Tensor]:
+        """The forward function in training.
+
+        Args:
+            inputs (List[torch.Tensor]): The input images.
+            data_samples (List[DataSample]): All elements required
+                during the forward function.
+
+        Returns:
+            Dict[str, Tensor]: A dictionary of loss components.
+        """
+        img_v1 = inputs[0]
+        img_v2 = inputs[1]
+
+        z1 = self.neck(self.backbone(img_v1))[0]  # NxC
+        z2 = self.neck(self.backbone(img_v2))[0]  # NxC
+
+        loss_1 = self.head.loss(z1, z2)
+        loss_2 = self.head.loss(z2, z1)
+
+        losses = dict(loss=0.5 * (loss_1 + loss_2))
+        return losses
