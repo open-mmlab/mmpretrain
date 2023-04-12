08c01f112e82dd600612f693c238e3ccd185277d
fangyixiao18 <fangyx18@hotmail.com>
2023-04-12 15:01:34 +0800

    remove num_classes

--- configs/sam/vit-huge-p16_sam_headless.py
+++ configs/sam/vit-huge-p16_sam_headless.py
@@ -16,7 +16,6 @@ model = dict(
 )
 
 data_preprocessor = dict(
-    num_classes=1000,
     # RGB format normalization parameters
     mean=[123.675, 116.28, 103.53],
     std=[58.395, 57.12, 57.375],
