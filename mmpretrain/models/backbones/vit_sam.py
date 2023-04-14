35bd80b017adc9b5f65adabde539d794fdfabf0c
fangyixiao18 <fangyx18@hotmail.com>
2023-04-14 22:06:16 +0800

    add layer decay setting function

--- mmpretrain/models/backbones/vit_sam.py
+++ mmpretrain/models/backbones/vit_sam.py
@@ -660,3 +660,38 @@ class ViTSAM(BaseBackbone):
                                 f'{state_dict[ckpt_key].shape} to '
                                 f'{new_rel_pos.shape}')
                     state_dict[ckpt_key] = new_rel_pos
+
+    def get_layer_depth(self, param_name: str, prefix: str = ''):
+        """Get the layer-wise depth of a parameter.
+
+        Args:
+            param_name (str): The name of the parameter.
+            prefix (str): The prefix for the parameter.
+                Defaults to an empty string.
+
+        Returns:
+            Tuple[int, int]: The layer-wise depth and the num of layers.
+
+        Note:
+            The first depth is the stem module (``layer_depth=0``), and the
+            last depth is the subsequent module (``layer_depth=num_layers-1``)
+        """
+        num_layers = self.num_layers + 2
+
+        if not param_name.startswith(prefix):
+            # For subsequent module like head
+            return num_layers - 1, num_layers
+
+        param_name = param_name[len(prefix):]
+
+        if param_name in ('cls_token', 'pos_embed'):
+            layer_depth = 0
+        elif param_name.startswith('patch_embed'):
+            layer_depth = 0
+        elif param_name.startswith('layers'):
+            layer_id = int(param_name.split('.')[1])
+            layer_depth = layer_id + 1
+        else:
+            layer_depth = num_layers - 1
+
+        return layer_depth, num_layers
