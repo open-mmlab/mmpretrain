fd5ea84e88c48bd171e3e26ef55ad623bddbc6dc
fangyixiao18 <fangyx18@hotmail.com>
2023-04-27 16:35:21 +0800

    fix lint

--- tests/test_models/test_utils/test_swiglu_ffn.py
+++ tests/test_models/test_utils/test_swiglu_ffn.py
@@ -1,7 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from unittest import TestCase
+
 import torch
 import torch.nn as nn
-from unittest import TestCase
 
 from mmpretrain.models.utils import LayerScale, SwiGLUFFN, SwiGLUFFNFused
 
