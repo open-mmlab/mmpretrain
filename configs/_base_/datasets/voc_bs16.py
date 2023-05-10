11be53964e752ddb2875d72306e636905489f4e2
zzc98 <785386364@qq.com>
2023-05-10 14:55:36 +0800

    [Refactor] update voc dataset

--- configs/_base_/datasets/voc_bs16.py
+++ configs/_base_/datasets/voc_bs16.py
@@ -61,6 +61,5 @@ val_evaluator = [
     dict(type='VOCAveragePrecision')
 ]
 
-# If you want standard test, please manually configure the test dataset
 test_dataloader = val_dataloader
 test_evaluator = val_evaluator
