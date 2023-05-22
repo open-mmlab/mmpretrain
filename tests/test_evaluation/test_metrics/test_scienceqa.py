f26d8696f65ee21e57e29e593b493b5597bf81dc
liuyuan <3463423099@qq.com>
2023-05-22 15:51:04 +0800

    [Fix]: Fix scienceqa

--- tests/test_evaluation/test_metrics/test_scienceqa.py
+++ tests/test_evaluation/test_metrics/test_scienceqa.py
@@ -24,3 +24,21 @@ class TestScienceQAMetric:
         assert res['acc_grade_1_6'] == 0.0
         assert res['acc_language'] == 0.0
         assert res['all_acc'] == 0.0
+
+        meta_info = {
+            'choices': ['A', 'B', 'C', 'D'],
+            'pred_answer': 'A',
+            'grade': 'grade1',
+            'subject': 'language science',
+            'gt_answer': 0,
+            'hint': 'hint',
+            'has_image': True
+        }
+        data_sample = DataSample(metainfo=meta_info)
+        data_samples = [data_sample for _ in range(10)]
+        evaluator = Evaluator(dict(type='mmpretrain.ScienceQAMetric'))
+        evaluator.process(data_samples)
+        res = evaluator.evaluate(4)
+        assert res['acc_grade_1_6'] == 1.0
+        assert res['acc_language'] == 1.0
+        assert res['all_acc'] == 1.0
