6bc4b8c1d92097ef36d05a45428ebca2d031c4e4
Ezra-Yu <18586273+Ezra-Yu@users.noreply.github.com>
2023-05-24 14:34:53 +0800

    rebase

--- mmpretrain/evaluation/metrics/__init__.py
+++ mmpretrain/evaluation/metrics/__init__.py
@@ -3,10 +3,14 @@ from .caption import COCOCaption
 from .gqa import GQAAcc
 from .multi_label import AveragePrecision, MultiLabelMetric
 from .multi_task import MultiTasksMetric
+<<<<<<< HEAD
 from .nocaps import NocapsSave
 from .retrieval import RetrievalRecall
 from .scienceqa import ScienceQAMetric
+=======
+>>>>>>> 8636016c... fix lint
 from .retrieval import RetrievalAveragePrecision, RetrievalRecall
+from .scienceqa import ScienceQAMetric
 from .single_label import Accuracy, ConfusionMatrix, SingleLabelMetric
 from .visual_grounding_eval import VisualGroundingMetric
 from .voc_multi_label import VOCAveragePrecision, VOCMultiLabelMetric
