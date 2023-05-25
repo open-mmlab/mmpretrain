8f92c6a87c0f39c7cf9f5a0cf9d1c0b762e3b710
Christian Holland <christian.holland@logivations.com>
2023-05-25 11:34:44 +0200

    add docker run file

--- run_docker.sh
+++ run_docker.sh
@@ -3,8 +3,6 @@ docker run \
     --shm-size=8g \
     --rm \
     -it \
-    -v /data/FST/221130_person_crops:/mmclassification/data \
-    -v /data/mmclassification/:/mmclassification \
     -v /data:/data \
-    -w /mmclassification \
-    mmclassification
+    -w /data/mmpretrain \
+    mmpretrain
