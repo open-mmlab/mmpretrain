50be97fbbecffbe9d6206517d5cfd7a04ca2862f
Ezra-Yu <18586273+Ezra-Yu@users.noreply.github.com>
2023-06-09 15:59:23 +0800

    update preprocess shell

--- tools/dataset_converters/odl_cub_preprocess.sh
+++ tools/dataset_converters/odl_cub_preprocess.sh
@@ -5,6 +5,11 @@ set -x
 DOWNLOAD_DIR=$1
 DATA_ROOT=$2
 
-cat $DOWNLOAD_DIR/CUB-200-2011/raw/*.tar.gz | tar -xvz -C $(dirname $DATA_ROOT)
-mv -f $DATA_ROOT/CUB-200-2011/* $DATA_ROOT/
-rm -R $DATA_ROOT/CUB-200-2011/ $DATA_ROOT/raw/
+# unzip all of data
+cat $DOWNLOAD_DIR/CUB-200-2011/raw/*.tar.gz | tar -xvz -C $DOWNLOAD_DIR
+
+# move data into DATA_ROOT
+mv -f $DOWNLOAD_DIR/CUB-200-2011/CUB-200-2011/* $DATA_ROOT/
+
+remove useless data file
+rm -R $DOWNLOAD_DIR/CUB-200-2011/
--- tools/dataset_converters/odl_imagenet1k_preprocess.sh
+++ tools/dataset_converters/odl_imagenet1k_preprocess.sh
@@ -5,6 +5,18 @@ set -x
 DOWNLOAD_DIR=$1
 DATA_ROOT=$2
 
-cat $DOWNLOAD_DIR/raw/*.tar.gz.* | tar -xvz -C $(dirname $DATA_ROOT)
-mv -f $DATA_ROOT/imagenet/* $DATA_ROOT/
-rm -R $DATA_ROOT/raw/
+# unzip all of data
+cat $DOWNLOAD_DIR/ImageNet-1K/raw/*.tar.gz.* | tar -xvz -C $DOWNLOAD_DIR
+
+# move images into data/imagenet
+mv $DOWNLOAD_DIR/ImageNet-1K/{train,val,test} $DATA_ROOT
+
+# download the mate ann_files file
+wget -P $DATA_ROOT  https://download.openmmlab.com/mmclassification/datasets/imagenet/meta/caffe_ilsvrc12.tar.gz
+
+# unzip mate ann_files file and put it into 'meta' folder
+mkdir $DATA_ROOT/meta
+tar -xzvf $DATA_ROOT/caffe_ilsvrc12.tar.gz -C $DATA_ROOT/meta
+
+# remove useless data files
+rm -R $DOWNLOAD_DIR/ImageNet-1K
