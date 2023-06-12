d24044cac9a5ee4d9cd3e91453024ea78922f235
Mashiro <57566630+HAOCHENYE@users.noreply.github.com>
2023-06-12 20:04:26 +0800

    [Feature] Support new configs (#1638)
    * add new config of mae and simclr
    
    * update
    
    * update setup.cfg
    
    * update eva
    
    * update
    
    * update new config
    
    * Add new config
    
    * remove __init__.py
    
    * 1. remove ; 2. remove mmpretrain/configs/_base_/models/convnext
    
    * remove model_wrapper_cfg and add out type
    
    * Add comment for setting default_scope to NOne
    
    * update if '_base_' order
    
    * update
    
    * revert changes
    
    ---------
    
    Co-authored-by: fangyixiao18 <fangyx18@hotmail.com>

---
+++ mmpretrain/configs/resnet/resnet18_8xb32_in1k.py
@@ -0,0 +1,6 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+if '_base_':
+    from .._base_.datasets.imagenet_bs32 import *
+    from .._base_.default_runtime import *
+    from .._base_.models.resnet18 import *
+    from .._base_.schedules.imagenet_bs256 import *
