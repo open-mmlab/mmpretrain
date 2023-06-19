73cbe7f39ce9bd509770e2ee89d97bf264a570fe
fangyixiao18 <fangyx18@hotmail.com>
2023-06-19 20:25:05 +0800

    update multimodal floating range

--- .dev_scripts/benchmark_options.py
+++ .dev_scripts/benchmark_options.py
@@ -3,3 +3,8 @@ third_part_libs = [
 ]
 
 default_floating_range = 0.2
+model_floating_ranges = {
+    'blip/blip-base_8xb32_retrieval.py': 1.0,
+    'blip2/blip2-opt2.7b_8xb32_caption.py': 1.0,
+    'ofa/ofa-base_finetuned_caption.py': 1.0,
+}
