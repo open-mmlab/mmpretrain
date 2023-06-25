5960f0a5f9db09b50c3b5e76f6083bdc6f6ad888
fangyixiao18 <fangyx18@hotmail.com>
2023-06-25 15:50:50 +0800

    replace if '_base_'

--- mmpretrain/configs/mae/mae_vit_base_p16_8xb512_amp_coslr_300e_in1k.py
+++ mmpretrain/configs/mae/mae_vit_base_p16_8xb512_amp_coslr_300e_in1k.py
@@ -1,6 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 # This is a BETA new format config file, and the usage may change recently.
-if '_base_':
+from mmengine.config import read_base
+
+with read_base():
     from .._base_.models.mae_vit_base_p16 import *
     from .._base_.datasets.imagenet_bs512_mae import *
     from .._base_.default_runtime import *
