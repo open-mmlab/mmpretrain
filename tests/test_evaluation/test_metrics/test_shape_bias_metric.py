fed9180a570ea1a3ad2305d9180d987861b27481
Wang Xiang <wx--168@163.com>
2023-06-28 14:52:22 +0800

    fix unit test bug

--- tests/test_evaluation/test_metrics/test_shape_bias_metric.py
+++ tests/test_evaluation/test_metrics/test_shape_bias_metric.py
@@ -6,9 +6,9 @@ from mmpretrain.evaluation import ShapeBiasMetric
 
 def test_shape_bias_metric():
     data_sample = dict()
-    data_sample['pred_label'] = dict(
-        score=torch.rand(1000, ), label=torch.tensor(1))
-    data_sample['gt_label'] = dict(label=torch.tensor(1))
+    data_sample['pred_score'] = torch.rand(1000, )
+    data_sample['pred_label'] = torch.tensor(1)
+    data_sample['gt_label'] = torch.tensor(1)
     data_sample['img_path'] = 'tests/airplane/test.JPEG'
     evaluator = ShapeBiasMetric(
         csv_dir='tests/data', dataset_name='test', model_name='test')
