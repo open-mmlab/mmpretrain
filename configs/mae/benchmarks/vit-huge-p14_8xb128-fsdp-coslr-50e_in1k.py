4c843d6bd359dce560db6a7c49718ecdd1a8e1ea
HAOCHENYE <21724054@zju.edu.cn>
2023-06-28 19:17:38 +0800

    Update fsdp vit-huge and vit-large config

---
+++ configs/mae/benchmarks/vit-huge-p14_8xb128-fsdp-coslr-50e_in1k.py
@@ -0,0 +1,14 @@
+_base_ = ['./vit-huge-p14_8xb128-coslr-50e_in1k.py']
+
+# training strategy
+# Deepspeed with ZeRO3 + fp16
+strategy = dict(
+    type='FSDPStrategy',
+    model_wrapper=dict(
+        auto_wrap_policy=dict(
+            type='torch.distributed.fsdp.wrap.size_based_auto_wrap_policy')))
+
+optim_wrapper = dict(type='AmpOptimWrapper')
+
+# runner which supports strategies
+runner_type = 'FlexibleRunner'
