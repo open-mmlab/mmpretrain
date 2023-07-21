be0bbe94eb54c283ec49507c514054a2a9134718
fanqiNO1 <1848839264@qq.com>
2023-07-21 18:14:59 +0800

    [Fix] Fix merge scripts

--- tools/model_converters/merge_lora_weight.py
+++ tools/model_converters/merge_lora_weight.py
@@ -3,50 +3,78 @@ import argparse
 from pathlib import Path
 
 import torch
+from mmengine.config import Config
+
+from mmpretrain.registry import MODELS
 
 
-def merge_lora_weight(base_weight, lora_weight):
+@torch.no_grad()
+def merge_lora_weight(cfg, lora_weight):
     """Merge base weight and lora weight.
 
     Args:
-        base_weight (dict): weight dict from LoRAModel.module.
+        cfg (dict): config for LoRAModel.
         lora_weight (dict): weight dict from LoRAModel.
     Returns:
         Merged weight.
     """
     temp = dict()
     mapping = dict()
+    for name, param in lora_weight['state_dict'].items():
+        # backbone.module.layers.11.attn.qkv.lora_down.weight
+        if '.lora_' in name:
+            lora_split = name.split('.')
+            prefix = '.'.join(lora_split[:-2])
+            if prefix not in mapping:
+                mapping[prefix] = dict()
+            lora_type = lora_split[-2]
+            mapping[prefix][lora_type] = param
+        else:
+            temp[name] = param
 
-    for lora_key, lora_value in lora_weight['state_dict'].items():
-        # backbone.module.layers.0.attn.qkv.lora_up.weight
-        lora_prefix = '.'.join(lora_key.split('.')[:-2])
-        mapping[lora_prefix] = f'{lora_prefix}.original_layer'
-        temp[lora_key] = lora_value
-
-    for base_key, base_value in base_weight.items():
-        # backbone.patch_embed.projection.weight
-        # add 'module' prefix
-        base_split = base_key.split('.')
-        base_split.insert(1, 'module')
-        base_prefix = '.'.join(base_split[:-1])
-        if base_prefix in mapping.keys():
-            base_prefix = mapping[base_prefix]
-        base_key = '.'.join([base_prefix, base_split[-1]])
-        temp[base_key] = base_value
+    model = MODELS.build(cfg['model'])
+    for name, param in model.named_parameters():
+        if name in temp or '.lora_' in name:
+            continue
+        else:
+            name_split = name.split('.')
+            prefix = prefix = '.'.join(name_split[:-2])
+            if prefix in mapping:
+                name_split.pop(-2)
+                if name_split[-1] == 'weight':
+                    scaling = get_scaling(model, prefix)
+                    lora_down = mapping[prefix]['lora_down']
+                    lora_up = mapping[prefix]['lora_up']
+                    param += lora_up @ lora_down * scaling
+            name_split.pop(1)
+            name = '.'.join(name_split)
+            temp[name] = param
 
     result = dict()
     result['state_dict'] = temp
-    for key in lora_weight.keys():
-        if key != 'state_dict':
-            result[key] = lora_weight[key]
-
+    result['meta'] = lora_weight['meta']
     return result
+
+
+def get_scaling(model, prefix):
+    """Get the scaling of speified layer.
+
+    Args:
+        model (LoRAModel): the LoRAModel.
+        prefix (str): the prefix of the layer.
+    Returns:
+        the scale of the LoRALinear.
+    """
+    prefix_split = prefix.split('.')
+    for i in prefix_split:
+        model = getattr(model, i)
+    return model.scaling
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Merge LoRA weight')
-    parser.add_argument('base_src', help='src detectron base model path')
-    parser.add_argument('lora_src', help='src detectron lora model path')
+    parser.add_argument('cfg', help='cfg path')
+    parser.add_argument('src', help='src detectron lora model path')
     parser.add_argument('dst', help='save path')
     args = parser.parse_args()
     dst = Path(args.dst)
@@ -55,8 +83,8 @@ if __name__ == '__main__':
         exit(1)
     dst.parent.mkdir(parents=True, exist_ok=True)
 
-    base_model = torch.load(args.base_src, map_location='cpu')
-    lora_model = torch.load(args.lora_src, map_location='cpu')
+    cfg = Config.fromfile(args.cfg)
+    lora_model = torch.load(args.src, map_location='cpu')
 
-    merged_model = merge_lora_weight(base_model, lora_model)
+    merged_model = merge_lora_weight(cfg, lora_model)
     torch.save(merged_model, args.dst)
