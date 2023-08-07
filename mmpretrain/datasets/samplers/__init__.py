cd71c12a946b58bf9ac9d76816476468094304b4
bobo0810 <1055271769@qq.com>
2023-08-07 21:14:11 +0800

    【Feature】Class balanced sampling at the Batch level

--- mmpretrain/datasets/samplers/__init__.py
+++ mmpretrain/datasets/samplers/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .repeat_aug import RepeatAugSampler
 from .sequential import SequentialSampler
+from .batch_balance import BatchBalanceSampler
 
-__all__ = ['RepeatAugSampler', 'SequentialSampler']
+__all__ = ['RepeatAugSampler', 'SequentialSampler', 'BatchBalanceSampler']
