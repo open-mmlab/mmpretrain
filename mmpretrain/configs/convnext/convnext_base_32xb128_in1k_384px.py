2655df7c9a15ab45569dad7c498cd29633d2ebbd
limafang <21009201106@stu.xidian.edu.cn>
2023-08-10 18:43:40 +0800

    update convnext_base_32xb128_in1k_384px.py

--- mmpretrain/configs/convnext/convnext_base_32xb128_in1k_384px.py
+++ mmpretrain/configs/convnext/convnext_base_32xb128_in1k_384px.py
@@ -25,4 +25,4 @@ custom_hooks = [dict(type=EMAHook, momentum=4e-5, priority='ABOVE_NORMAL')]
 # NOTE: `auto_scale_lr` is for automatically scaling LR
 # based on the actual training batch size.
 # base_batch_size = (32 GPUs) x (128 samples per GPU)
-auto_scale_lr = dict(base_batch_size=4096)
+auto_scale_lr.update(base_batch_size=4096)
