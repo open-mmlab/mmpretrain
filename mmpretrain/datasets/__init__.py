02d1c3f2e9d9c980e0a4fe64d6e1253e1244e157
hmtbgc <hmtbgc@163.com>
2023-09-12 21:47:16 +0800

    restore files deleted by mistake

--- mmpretrain/datasets/__init__.py
+++ mmpretrain/datasets/__init__.py
@@ -41,6 +41,8 @@ if WITH_MULTIMODAL:
     from .flickr30k_caption import Flickr30kCaption
     from .flickr30k_retrieval import Flickr30kRetrieval
     from .gqa_dataset import GQA
+    from .iconqa import IconQA
+    from .infographic_vqa import InfographicVQA
     from .minigpt4_dataset import MiniGPT4Dataset
     from .nocaps import NoCaps
     from .ocr_vqa import OCRVQA
@@ -55,5 +57,6 @@ if WITH_MULTIMODAL:
         'COCOCaption', 'COCORetrieval', 'COCOVQA', 'FlamingoEvalCOCOCaption',
         'FlamingoEvalCOCOVQA', 'Flickr30kCaption', 'Flickr30kRetrieval',
         'RefCOCO', 'VisualGenomeQA', 'ScienceQA', 'NoCaps', 'GQA', 'TextVQA',
-        'VSR', 'VizWiz', 'OCRVQA', 'MiniGPT4Dataset'
+        'VSR', 'VizWiz', 'OCRVQA', 'InfographicVQA', 'IconQA',
+        'MiniGPT4Dataset'
     ])
