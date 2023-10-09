f4efe8b78765185f38de32359953f3870dc72010
joaquincabezas <1833086+joaquincabezas@users.noreply.github.com>
2023-10-09 17:24:28 +0200

    fix torchserve version

--- tools/torchserve/mmpretrain2torchserve.py
+++ tools/torchserve/mmpretrain2torchserve.py
@@ -10,8 +10,9 @@ try:
     from model_archiver.model_packaging_utils import ModelExportUtils
 except ImportError:
     raise ImportError(
-        'Please run `pip install torchserve torch-model-archiver"` to '
-        'install required third-party libraries.')
+        'Please run `pip install torchserve==0.7.1 torch-model-archiver==0.7.1"` to '
+        "install required third-party libraries."
+    )
 
 
 def mmpretrain2torchserve(
@@ -19,7 +20,7 @@ def mmpretrain2torchserve(
     checkpoint_file: str,
     output_folder: str,
     model_name: str,
-    model_version: str = '1.0',
+    model_version: str = "1.0",
     force: bool = False,
 ):
     """Converts mmpretrain model (config + checkpoint) to TorchServe `.mar`.
@@ -49,64 +50,75 @@ def mmpretrain2torchserve(
     config = mmengine.Config.fromfile(config_file)
 
     with TemporaryDirectory() as tmpdir:
-        config.dump(f'{tmpdir}/config.py')
+        config.dump(f"{tmpdir}/config.py")
 
         args = Namespace(
             **{
-                'model_file': f'{tmpdir}/config.py',
-                'serialized_file': checkpoint_file,
-                'handler': f'{Path(__file__).parent}/mmpretrain_handler.py',
-                'model_name': model_name or Path(checkpoint_file).stem,
-                'version': model_version,
-                'export_path': output_folder,
-                'force': force,
-                'requirements_file': None,
-                'extra_files': None,
-                'runtime': 'python',
-                'archive_format': 'default'
-            })
+                "model_file": f"{tmpdir}/config.py",
+                "serialized_file": checkpoint_file,
+                "handler": f"{Path(__file__).parent}/mmpretrain_handler.py",
+                "model_name": model_name or Path(checkpoint_file).stem,
+                "version": model_version,
+                "export_path": output_folder,
+                "force": force,
+                "requirements_file": None,
+                "extra_files": None,
+                "runtime": "python",
+                "archive_format": "default",
+            }
+        )
         manifest = ModelExportUtils.generate_manifest_json(args)
         package_model(args, manifest)
 
 
 def parse_args():
     parser = ArgumentParser(
-        description='Convert mmpretrain models to TorchServe `.mar` format.')
-    parser.add_argument('config', type=str, help='config file path')
-    parser.add_argument('checkpoint', type=str, help='checkpoint file path')
+        description="Convert mmpretrain models to TorchServe `.mar` format."
+    )
+    parser.add_argument("config", type=str, help="config file path")
+    parser.add_argument("checkpoint", type=str, help="checkpoint file path")
     parser.add_argument(
-        '--output-folder',
+        "--output-folder",
         type=str,
         required=True,
-        help='Folder where `{model_name}.mar` will be created.')
+        help="Folder where `{model_name}.mar` will be created.",
+    )
     parser.add_argument(
-        '--model-name',
+        "--model-name",
         type=str,
         default=None,
-        help='If not None, used for naming the `{model_name}.mar`'
-        'file that will be created under `output_folder`.'
-        'If None, `{Path(checkpoint_file).stem}` will be used.')
+        help="If not None, used for naming the `{model_name}.mar`"
+        "file that will be created under `output_folder`."
+        "If None, `{Path(checkpoint_file).stem}` will be used.",
+    )
     parser.add_argument(
-        '--model-version',
-        type=str,
-        default='1.0',
-        help='Number used for versioning.')
+        "--model-version", type=str, default="1.0", help="Number used for versioning."
+    )
     parser.add_argument(
-        '-f',
-        '--force',
-        action='store_true',
-        help='overwrite the existing `{model_name}.mar`')
+        "-f",
+        "--force",
+        action="store_true",
+        help="overwrite the existing `{model_name}.mar`",
+    )
     args = parser.parse_args()
 
     return args
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parse_args()
 
     if package_model is None:
-        raise ImportError('`torch-model-archiver` is required.'
-                          'Try: pip install torch-model-archiver')
+        raise ImportError(
+            "`torch-model-archiver` is required."
+            "Try: pip install torch-model-archiver==0.7.1"
+        )
 
-    mmpretrain2torchserve(args.config, args.checkpoint, args.output_folder,
-                          args.model_name, args.model_version, args.force)
+    mmpretrain2torchserve(
+        args.config,
+        args.checkpoint,
+        args.output_folder,
+        args.model_name,
+        args.model_version,
+        args.force,
+    )
